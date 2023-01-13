fe225b6006d1973cb89c2eec13a4b4ab2b361f09
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-01-13 01:34:27 -0500

    refactor: replace mud project with react template

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,28 +1,31 @@
+import ReactDOM from "react-dom/client";
 import { setupMUDNetwork } from "@latticexyz/std-client";
 import { createWorld } from "@latticexyz/recs";
 import { SystemTypes } from "contracts/types/SystemTypes";
 import { SystemAbis } from "contracts/types/SystemAbis.mjs";
 import { defineNumberComponent } from "@latticexyz/std-client";
 import { config } from "./config";
+import { App } from "./App";
+import { GodID as SingletonID } from "@latticexyz/network";
+
+const rootElement = document.getElementById("react-root");
+if (!rootElement) throw new Error("React root not found");
+const root = ReactDOM.createRoot(rootElement);
 
 // The world contains references to all entities, all components and disposers.
 const world = createWorld();
+export const singletonIndex = world.registerEntity({ id: SingletonID });
 
 // Components contain the application state.
 // If a contractId is provided, MUD syncs the state with the corresponding
 // component contract (in this case `CounterComponent.sol`)
-const components = {
+export const components = {
   Counter: defineNumberComponent(world, {
     metadata: {
       contractId: "component.Counter",
     },
   }),
 };
-
-// Components expose a stream that triggers when the component is updated.
-components.Counter.update$.subscribe(({ value }) => {
-  document.getElementById("counter")!.innerHTML = String(value?.[0]?.value);
-});
 
 // This is where the magic happens
 setupMUDNetwork<typeof components, SystemTypes>(
@@ -34,10 +37,5 @@ setupMUDNetwork<typeof components, SystemTypes>(
   // After setting up the network, we can tell MUD to start the synchronization process.
   startSync();
 
-  console.log(config.privateKey);
-
-  // Just for demonstration purposes: we create a global function that can be
-  // called to invoke the Increment system contract. (See IncrementSystem.sol.)
-  (window as any).increment = () =>
-    systems["system.Increment"].executeTyped("0x00");
+  root.render(<App world={world} systems={systems} components={components} />);
 });
