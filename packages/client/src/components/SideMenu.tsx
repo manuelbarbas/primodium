cd9b87a8a6c64689ae089458d24b76d26e2a874b
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-02-12 21:35:41 -0500

    fix: hook within component

--- packages/client/src/components/SideMenu.tsx
+++ packages/client/src/components/SideMenu.tsx
@@ -6,22 +6,24 @@ import { TbBulldozer } from "react-icons/tb";
 import { TbSword } from "react-icons/tb";
 import { TbScale } from "react-icons/tb";
 
-const [menuItem, setMenuItem] = useState(false);
+function SideBarIcon({ icon, text }: { icon: any; text: string }) {
+  const [menuItem, setMenuItem] = useState(false);
 
-const SideBarIcon = ({ icon, text }: { icon: any; text: string }) => (
-  <div>
-    <button
-      className="sidebar-icon group"
-      onClick={() => setMenuItem(!menuItem)}
-    >
-      {icon}
-      {menuItem && (
-        <div className="fixed z-[1000]">building menu goes here</div>
-      )}
-      <div className="sidebar-tooltip group-hover:scale-100"> {text} </div>
-    </button>
-  </div>
-);
+  return (
+    <div>
+      <button
+        className="sidebar-icon group"
+        onClick={() => setMenuItem(!menuItem)}
+      >
+        {icon}
+        {menuItem && (
+          <div className="fixed z-[1000]">building menu goes here</div>
+        )}
+        <div className="sidebar-tooltip group-hover:scale-100"> {text} </div>
+      </button>
+    </div>
+  );
+}
 
 function SideMenu() {
   return (
