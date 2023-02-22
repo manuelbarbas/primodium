9fa5c1385f8935b3e4574e36720badb84f18b4c3
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-02-21 22:48:49 -0500

    fix: can not start and end path at same coord

--- packages/contracts/src/systems/BuildPathSystem.sol
+++ packages/contracts/src/systems/BuildPathSystem.sol
@@ -22,6 +22,8 @@ contract BuildPathSystem is System {
     PathComponent pathComponent = PathComponent(getAddressById(components, PathComponentID));
     OwnedByComponent ownedByComponent = OwnedByComponent(getAddressById(components, OwnedByComponentID));
 
+    require(!(coordStart.x == coordEnd.x && coordStart.y == coordEnd.y), "can not start and end path at same coord");
+
     // Check that the coordinates exist tiles
     uint256[] memory entitiesAtStartCoord = positionComponent.getEntitiesWithValue(coordStart);
     require(entitiesAtStartCoord.length == 1, "can not start path at empty coord");
