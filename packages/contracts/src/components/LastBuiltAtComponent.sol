2c4044c265b19ef32bcb6388a736a19bfb09e9bc
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-02-25 20:55:25 -0500

    refactor: rename LastBuiltComponent to LastBuiltAtComponent etc

--- packages/contracts/src/components/LastBuiltAtComponent.sol
+++ packages/contracts/src/components/LastBuiltAtComponent.sol
@@ -2,8 +2,8 @@
 pragma solidity >=0.8.0;
 import "std-contracts/components/Uint256Component.sol";
 
-uint256 constant ID = uint256(keccak256("component.LastBuilt"));
+uint256 constant ID = uint256(keccak256("component.LastBuiltAt"));
 
-contract LastBuiltComponent is Uint256Component {
+contract LastBuiltAtComponent is Uint256Component {
   constructor(address world) Uint256Component(world, ID) {}
 }
