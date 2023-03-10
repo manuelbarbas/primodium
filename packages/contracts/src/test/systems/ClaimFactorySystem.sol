7a870fd4a43d05450391d02aafe750d227279158
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-03-10 18:46:09 -0500

    test: no through claiming (factory 2 claim -> doesn't claim from mines connected to factory 1)

--- packages/contracts/src/test/systems/ClaimFactorySystem.sol
+++ packages/contracts/src/test/systems/ClaimFactorySystem.sol
@@ -171,4 +171,68 @@ contract ClaimFactorySystemTest is MudTest {
 
     vm.stopPrank();
   }
+
+  function testClaimFactoryToFactory() public {
+    vm.startPrank(alice);
+
+    BuildSystem buildSystem = BuildSystem(system(BuildSystemID));
+    BuildPathSystem buildPathSystem = BuildPathSystem(system(BuildPathSystemID));
+    ClaimSystem claimSystem = ClaimSystem(system(ClaimSystemID));
+    IronResourceComponent ironResourceComponent = IronResourceComponent(component(IronResourceComponentID));
+
+    // TEMP: tile -5, 2 has iron according to current generation seed
+    Coord memory coord = Coord({ x: -5, y: 2 });
+    assertEq(LibTerrain.getTopLayerKey(coord), IronID);
+
+    Coord memory bulletFactoryCoord = Coord({ x: 0, y: 0 });
+    Coord memory endPathCoord = Coord({ x: -1, y: 0 });
+    Coord memory startPathCoord = Coord({ x: -5, y: 1 });
+
+    bytes memory bulletFactoryEntity = buildSystem.executeTyped(BulletFactoryID, bulletFactoryCoord);
+    uint256 bulletFactoryID = abi.decode(bulletFactoryEntity, (uint256));
+    buildSystem.executeTyped(ConveyerID, endPathCoord);
+    buildSystem.executeTyped(ConveyerID, startPathCoord);
+    buildPathSystem.executeTyped(startPathCoord, endPathCoord);
+
+    // TEMP: MINE_COUNT_PER_BLOCK = 10 regardless of miner
+    // START CLAIMING
+    vm.roll(0);
+
+    buildSystem.executeTyped(MinerID, coord);
+    assertTrue(!ironResourceComponent.has(bulletFactoryID));
+
+    vm.roll(10);
+
+    claimSystem.executeTyped(bulletFactoryCoord);
+    assertTrue(ironResourceComponent.has(bulletFactoryID));
+    assertEq(ironResourceComponent.getValue(bulletFactoryID), 100);
+
+    // new bullet factory to transfer resources to
+    Coord memory bulletFactory2Coord = Coord({ x: 0, y: 10 });
+    Coord memory endPath2Coord = Coord({ x: 0, y: 9 });
+    Coord memory startPath2Coord = Coord({ x: 0, y: 1 });
+
+    bytes memory bulletFactory2Entity = buildSystem.executeTyped(BulletFactoryID, bulletFactory2Coord);
+    uint256 bulletFactory2ID = abi.decode(bulletFactory2Entity, (uint256));
+    buildSystem.executeTyped(ConveyerID, endPath2Coord);
+    buildSystem.executeTyped(ConveyerID, startPath2Coord);
+    buildPathSystem.executeTyped(startPath2Coord, endPath2Coord);
+
+    // transfer iron from factory 1 to factory 2
+    console.log("Block here? should be 10");
+    console.log(block.number);
+
+    claimSystem.executeTyped(bulletFactory2Coord);
+    assertTrue(ironResourceComponent.has(bulletFactory2ID));
+    assertEq(ironResourceComponent.getValue(bulletFactory2ID), 100);
+
+    vm.roll(20);
+
+    // no through claiming. factory 2 claim -> doesn't claim from mines connected to factory 1
+    claimSystem.executeTyped(bulletFactory2Coord);
+    assertTrue(ironResourceComponent.has(bulletFactory2ID));
+    assertEq(ironResourceComponent.getValue(bulletFactory2ID), 100);
+
+    vm.stopPrank();
+  }
 }
