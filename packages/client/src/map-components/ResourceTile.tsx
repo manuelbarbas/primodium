e352fdb64abad68fdb3489b04abf46d24a02083f
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-03-17 17:26:19 -0400

    refactor: display tile info

--- packages/client/src/map-components/ResourceTile.tsx
+++ packages/client/src/map-components/ResourceTile.tsx
@@ -3,13 +3,9 @@ import { memo, useMemo } from "react";
 import { Has, HasValue, EntityID, getComponentValue } from "@latticexyz/recs";
 import { useComponentValue, useEntityQuery } from "@latticexyz/react";
 
-import { Rectangle, Polyline, ImageOverlay } from "react-leaflet";
+import { ImageOverlay } from "react-leaflet";
 
-// import ReactDOMServer from "react-dom/server";
-// import L from "leaflet";
-// import { Marker } from "react-leaflet";
-
-import { BlockColors, BackgroundImage } from "../util/constants";
+import { BackgroundImage } from "../util/constants";
 import { useMud } from "../context/MudContext";
 import Path from "./Path";
 
@@ -95,42 +91,11 @@ function ResourceTile({
     }
   });
 
-  // // Debug to show tile info
-  // const DivElement = (
-  //   <p>
-  //     {x}, {y}
-  //     <br />
-  //     {BlockIdToKey[topLayerKey as EntityID]}
-  //   </p>
-  // );
-  // let icon = new L.DivIcon({
-  //   iconSize: [1, 1],
-  //   html: ReactDOMServer.renderToString(DivElement),
-  // });
-
   //!!Used for setting an image background!!
   let imagebackground = BackgroundImage.get(topLayerKey as EntityID);
 
   return (
     <>
-      {/* <Marker
-        key={JSON.stringify({ x, y, icon: "icon" })}
-        position={[y + 0.5, x + 0.5]}
-        icon={icon}
-      /> */}
-      {/* <Rectangle
-        key={JSON.stringify({ x, y })}
-        bounds={[
-          [y, x],
-          [y + 1, x + 1],
-        ]}
-        pathOptions={{
-          fillOpacity: 1,
-          weight: 1,
-          color: BlockColors.get(topLayerKey as EntityID),
-        }}
-        pane="mapPane"
-      /> */}
       {/* !!setting an image background!! */}
       <ImageOverlay
         className="pixel-images"
@@ -139,10 +104,8 @@ function ResourceTile({
           [y, x],
           [y + 1, x + 1],
         ]}
-        url={imagebackground}
-        layerOptions={{
-          pane: "overlayPane",
-        }}
+        url={imagebackground!}
+        zIndex={999}
       />
       {pathsToRender}
     </>
