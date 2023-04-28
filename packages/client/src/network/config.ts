0e088822951441255962fe077d7eae52501b5275
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-04-27 20:12:52 -0400

    chore: add mud chain config

--- packages/client/src/network/config.ts
+++ packages/client/src/network/config.ts
@@ -4,54 +4,15 @@ import { SetupContractConfig } from "@latticexyz/std-client";
 import { Wallet } from "ethers";
 const params = new URLSearchParams(window.location.search);
 
-export const devConfig = () => {
-  const randomWallet = Wallet.createRandom();
-  const config: SetupContractConfig = {
-    clock: {
-      period: 1000,
-      initialTime: 0,
-      syncInterval: 5000,
-    },
-    provider: {
-      jsonRpcUrl: params.get("rpc") ?? "http://localhost:8545",
-      wsRpcUrl: params.get("wsRpc") ?? "ws://localhost:8545",
-      chainId: Number(params.get("chainId")) || 31337,
-    },
-    privateKey: randomWallet.privateKey,
-    chainId: Number(params.get("chainId")) || 31337,
-    snapshotServiceUrl: params.get("snapshot") ?? undefined,
-    initialBlockNumber: Number(params.get("initialBlockNumber")) || 0,
-    worldAddress: params.get("worldAddress")!,
-    devMode: params.get("dev") === "true",
-  };
-  return { defaultWalletAddress: randomWallet.address, config: config };
-};
-
-export const getNetworkLayerConfig = (
-  externalProvider: ExternalProvider | undefined
-) => {
-  const config: SetupContractConfig = {
-    clock: {
-      period: 1000,
-      initialTime: 0,
-      syncInterval: 5000,
-    },
-    provider: {
-      jsonRpcUrl: params.get("rpc") ?? "http://localhost:8545",
-      wsRpcUrl: params.get("wsRpc") ?? "ws://localhost:8545",
-      chainId: Number(params.get("chainId")) || 31337,
-      externalProvider: externalProvider,
-    },
-    chainId: Number(params.get("chainId")) || 31337,
-    snapshotServiceUrl: params.get("snapshot") ?? undefined,
-    initialBlockNumber: Number(params.get("initialBlockNumber")) || 0,
-    worldAddress: params.get("worldAddress")!,
-    devMode: params.get("dev") === "true",
-  };
-  return { defaultWalletAddress: undefined, config: config };
-};
-
 // from opcraft, for deploying on mud testnet
+// rpc: https://follower.testnet-chain.linfra.xyz
+// wsRpc: wss://follower.testnet-chain.linfra.xyz
+// block explorer:  https://explorer.testnet-chain.linfra.xyz
+// stream service: https://ecs-stream.testnet-mud-services.linfra.xyz
+// snapshot service: https://ecs-snapshot.testnet-mud-services.linfra.xyz
+// relay service: https://ecs-relay.testnet-mud-services.linfra.xyz
+// faucet: https://faucet.testnet-mud-services.linfra.xyz
+
 export const defaultParams = {
   chainId: "4242",
   worldAddress: "0xc1c15CCE34E16684d36B0F76B9fa4f74b3a279f4",
@@ -65,4 +26,101 @@ export const defaultParams = {
   blockTime: "1000",
   blockExplorer: "https://explorer.testnet-chain.linfra.xyz",
   dev: "false",
+};
+
+// change flag before deployment
+const DEV = import.meta.env.VITE_DEV === "true";
+
+export const devConfig = () => {
+  const randomWallet = Wallet.createRandom();
+
+  if (!DEV) {
+    const config: SetupContractConfig = {
+      clock: {
+        period: 1000,
+        initialTime: 0,
+        syncInterval: 60_000,
+      },
+      provider: {
+        jsonRpcUrl: "https://follower.testnet-chain.linfra.xyz",
+        wsRpcUrl: "wss://follower.testnet-chain.linfra.xyz",
+        chainId: 4242,
+      },
+      chainId: 4242,
+      snapshotServiceUrl:
+        "https://ecs-snapshot.testnet-mud-services.linfra.xyz",
+      initialBlockNumber: 12947793,
+      worldAddress: params.get("worldAddress")!,
+      devMode: false,
+      privateKey: randomWallet.privateKey,
+    };
+    return { defaultWalletAddress: randomWallet.address, config: config };
+  } else {
+    const config: SetupContractConfig = {
+      clock: {
+        period: 1000,
+        initialTime: 0,
+        syncInterval: 5000,
+      },
+      provider: {
+        jsonRpcUrl: params.get("rpc") ?? "http://localhost:8545",
+        wsRpcUrl: params.get("wsRpc") ?? "ws://localhost:8545",
+        chainId: Number(params.get("chainId")) || 31337,
+      },
+      chainId: Number(params.get("chainId")) || 31337,
+      snapshotServiceUrl: params.get("snapshot") ?? undefined,
+      initialBlockNumber: Number(params.get("initialBlockNumber")) || 0,
+      worldAddress: params.get("worldAddress")!,
+      devMode: params.get("dev") === "true",
+      privateKey: randomWallet.privateKey,
+    };
+    return { defaultWalletAddress: randomWallet.address, config: config };
+  }
+};
+
+export const getNetworkLayerConfig = (
+  externalProvider: ExternalProvider | undefined
+) => {
+  if (!DEV) {
+    const config: SetupContractConfig = {
+      clock: {
+        period: 1000,
+        initialTime: 0,
+        syncInterval: 60_000,
+      },
+      provider: {
+        jsonRpcUrl: "https://follower.testnet-chain.linfra.xyz",
+        wsRpcUrl: "wss://follower.testnet-chain.linfra.xyz",
+        chainId: 4242,
+        externalProvider: externalProvider,
+      },
+      chainId: 4242,
+      snapshotServiceUrl:
+        "https://ecs-snapshot.testnet-mud-services.linfra.xyz",
+      initialBlockNumber: 12947793,
+      worldAddress: params.get("worldAddress")!,
+      devMode: false,
+    };
+    return { defaultWalletAddress: undefined, config: config };
+  } else {
+    const config: SetupContractConfig = {
+      clock: {
+        period: 1000,
+        initialTime: 0,
+        syncInterval: 5000,
+      },
+      provider: {
+        jsonRpcUrl: params.get("rpc") ?? "http://localhost:8545",
+        wsRpcUrl: params.get("wsRpc") ?? "ws://localhost:8545",
+        chainId: Number(params.get("chainId")) || 31337,
+        externalProvider: externalProvider,
+      },
+      chainId: Number(params.get("chainId")) || 31337,
+      snapshotServiceUrl: params.get("snapshot") ?? undefined,
+      initialBlockNumber: Number(params.get("initialBlockNumber")) || 0,
+      worldAddress: params.get("worldAddress")!,
+      devMode: params.get("dev") === "true",
+    };
+    return { defaultWalletAddress: undefined, config: config };
+  }
 };
