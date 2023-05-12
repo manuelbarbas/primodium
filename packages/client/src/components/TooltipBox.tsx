fa3d50addb5de5474f7137fffea4025d3b28cba6
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-05-11 22:26:23 -0700

    fix: move TooltipBox below ResearchModal

--- packages/client/src/components/TooltipBox.tsx
+++ packages/client/src/components/TooltipBox.tsx
@@ -144,7 +144,7 @@ function TooltipBox() {
 
   if (!minimized) {
     return (
-      <div className="z-[9999] fixed bottom-4 right-4 h-96 w-80  flex flex-col bg-gray-700 text-white shadow-xl font-mono rounded">
+      <div className="z-[1000] fixed bottom-4 right-4 h-96 w-80  flex flex-col bg-gray-700 text-white shadow-xl font-mono rounded">
         <div className=" mt-4 ml-5 flex flex-col overflow-y-scroll scrollbar h-64">
           <button onClick={minimizeBox} className="fixed right-9">
             <LinkIcon icon={<FaMinusSquare size="18" />} />
@@ -349,7 +349,7 @@ function TooltipBox() {
     );
   } else {
     return (
-      <div className="z-[9999] fixed bottom-4 right-4 h-14 w-64 flex flex-col bg-gray-700 text-white shadow-xl font-mono rounded">
+      <div className="z-[1000] fixed bottom-4 right-4 h-14 w-64 flex flex-col bg-gray-700 text-white shadow-xl font-mono rounded">
         <div className=" mt-4 ml-5 flex flex-col">
           <button onClick={minimizeBox} className="fixed right-9">
             <LinkIcon icon={<FaPlusSquare size="18" />} />
