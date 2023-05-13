c49ec76e5bc7b308ca1d11a81a892e71673180a4
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-05-12 19:58:46 -0700

    feat: reintroduce gas limit for out of gas errors

--- packages/client/src/components/action/ClaimButton.tsx
+++ packages/client/src/components/action/ClaimButton.tsx
@@ -11,17 +11,27 @@ export default function ClaimButton({ x, y }: DisplayTile) {
   const claimAction = useCallback(async () => {
     setTransactionLoading(true);
     await execute(
-      systems["system.ClaimFromMine"].executeTyped({
-        x: x,
-        y: y,
-      }),
+      systems["system.ClaimFromMine"].executeTyped(
+        {
+          x: x,
+          y: y,
+        },
+        {
+          gasLimit: 30_000_000,
+        }
+      ),
       providers
     );
     await execute(
-      systems["system.ClaimFromFactory"].executeTyped({
-        x: x,
-        y: y,
-      }),
+      systems["system.ClaimFromFactory"].executeTyped(
+        {
+          x: x,
+          y: y,
+        },
+        {
+          gasLimit: 30_000_000,
+        }
+      ),
       providers
     );
     setTransactionLoading(false);
