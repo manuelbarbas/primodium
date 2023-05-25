1d8cc64a929bfe5fb72329f046f0e36533218c4b
Nabs <43505437+NabsGit@users.noreply.github.com>
2023-05-25 17:50:57 -0500

    feat(destroy): destroy path is now in demolish menu

--- packages/client/src/components/destroy-menu/DestroyBuildingBox.tsx
+++ packages/client/src/components/destroy-menu/DestroyBuildingBox.tsx
@@ -8,8 +8,6 @@ import { useGameStore } from "../../store/GameStore";
 import { execute } from "../../network/actions";
 import { DisplayTile } from "../../util/constants";
 
-import DestroyTileButton from "./DestroyTileButton";
-
 function DestroyBuildingBox() {
   const { systems, providers } = useMud();
   // const { selectedTile } = useSelectedTile();
@@ -36,6 +34,18 @@ function DestroyBuildingBox() {
     setTransactionLoading(false);
   }, []);
 
+  // delete path
+  const destroyPath = useCallback(async () => {
+    setTransactionLoading(true);
+    await execute(
+      systems["system.DestroyPath"].executeTyped(selectedTile, {
+        gasLimit: 500_000,
+      }),
+      providers
+    );
+    setTransactionLoading(false);
+  }, [selectedTile]);
+
   // Helpers
   const destroyTileHelper = useCallback(() => {
     destroyTile(selectedTile);
@@ -46,10 +56,23 @@ function DestroyBuildingBox() {
       <div className="mt-4 mx-5 flex flex-col h-72">
         <p className="text-lg font-bold mb-3">Demolish</p>
         <p>
-          Select your building on the map and click <i>Demolish</i> to remove
-          it.
+          Select your building/path on the map and click <i>Demolish</i> to
+          remove it.
         </p>
-        <DestroyTileButton action={destroyTileHelper} />
+        <div className="absolute bottom-4 right-4 space-x-2">
+          <button
+            className="h-10 w-36 bg-orange-600 hover:bg-orange-700 font-bold rounded text-sm"
+            onClick={destroyPath}
+          >
+            <p className="inline-block ml-1">Demolish Path</p>
+          </button>
+          <button
+            className="h-10 w-36 bg-red-600 hover:bg-red-700 font-bold rounded text-sm"
+            onClick={destroyTileHelper}
+          >
+            <p className="inline-block ml-1">Demolish</p>
+          </button>
+        </div>
       </div>
     </div>
   );
