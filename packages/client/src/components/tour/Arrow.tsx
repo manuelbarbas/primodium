8f700c88d94646d82f7bbfb5695585887390af41
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-06-05 20:36:50 -0700

    fix: undo don't return tooltip if transaction is loading, because the arrow disappearing without motion is more confusing

--- packages/client/src/components/tour/Arrow.tsx
+++ packages/client/src/components/tour/Arrow.tsx
@@ -1,6 +1,5 @@
 import { FaArrowDown } from "react-icons/fa";
 import { SimpleCardinal } from "../../util/types";
-import { useGameStore } from "../../store/GameStore";
 
 const Arrow = ({
   direction,
@@ -11,10 +10,6 @@ const Arrow = ({
   bounce?: boolean;
   className?: string;
 }) => {
-  const [transactionLoading] = useGameStore((state) => [
-    state.transactionLoading,
-  ]);
-
   let rotation;
 
   switch (direction) {
@@ -32,22 +27,17 @@ const Arrow = ({
       break;
   }
 
-  // don't return tooltip if transaction is loading, because nothing to click on
-  if (transactionLoading) {
-    return <></>;
-  } else {
-    return (
-      <div className={`${rotation} ${className} pointer-events-none`}>
-        <div
-          className={`text-yellow-300 shadow-2xl text-4xl ${
-            bounce ? "animate-bounce" : ""
-          }`}
-        >
-          <FaArrowDown />
-        </div>
+  return (
+    <div className={`${rotation} ${className} pointer-events-none`}>
+      <div
+        className={`text-yellow-300 shadow-2xl text-4xl ${
+          bounce ? "animate-bounce" : ""
+        }`}
+      >
+        <FaArrowDown />
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Arrow;
