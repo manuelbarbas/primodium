1ab4e770a90c94f9c2c6cf446b778529d54fd0a4
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-06-14 23:02:38 -0700

    feat: update demolish box title

--- packages/client/src/components/demolish-menu/DemolishBuildingBox.tsx
+++ packages/client/src/components/demolish-menu/DemolishBuildingBox.tsx
@@ -25,12 +25,12 @@ function DemolishBuildingBox() {
     return (
       <div className="z-[1000] viewport-container fixed bottom-4 left-20 h-72 w-96 flex flex-col bg-gray-700 text-white drop-shadow-xl font-mono rounded">
         <div className="mt-4 mx-5 flex flex-col h-72">
-          <p className="text-lg font-bold mb-3">Demolish</p>
-          <p>Select a Node tile where your path starts to demolish the path.</p>
+          <p className="text-lg font-bold mb-3">Demolishing Path</p>
+          <p>Select a node where your path starts to demolish the path.</p>
           <div className="absolute bottom-4 right-4 space-x-2">
             <button
-              className="h-10 w-36 bg-orange-600 hover:bg-orange-700 font-bold rounded text-sm"
-              onClick={destroyPath}
+              className="h-10 w-36 bg-orange-700 hover:bg-orange-800 font-bold rounded text-sm"
+              onClick={resetSetSelectedBlock}
             >
               <p className="inline-block ml-1">Demolish Path</p>
             </button>
@@ -48,7 +48,7 @@ function DemolishBuildingBox() {
     return (
       <div className="z-[1000] viewport-container fixed bottom-4 left-20 h-72 w-96 flex flex-col bg-gray-700 text-white drop-shadow-xl font-mono rounded">
         <div className="mt-4 mx-5 flex flex-col h-72">
-          <p className="text-lg font-bold mb-3">Demolish</p>
+          <p className="text-lg font-bold mb-3">Demolishing Building</p>
           <p>Select your building on the map to remove it.</p>
           <div className="absolute bottom-4 right-4 space-x-2">
             <button
