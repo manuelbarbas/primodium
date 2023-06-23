60c1746a1860c16f8c926490a950f0b5d44b2e1e
smallbraingames <smallbraindev@gmail.com>
2023-06-23 11:52:20 -0700

    feat(contracts): coord -> entity encoding & decoding
    - takes key argument as string while creating entity

--- packages/contracts/src/libraries/LibEncode.sol
+++ packages/contracts/src/libraries/LibEncode.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import {Coord} from "std-contracts/components/CoordComponent.sol";
+import {split} from "solecs/utils.sol";
+
+
 library LibEncode {
   function hashFromStringAddress(string memory str, address addr) internal pure returns (uint256) {
     return uint256(keccak256(abi.encodePacked(str, addr)));
@@ -20,5 +24,18 @@ library LibEncode {
 
   function stringsEqual(string memory a, string memory b) internal pure returns (bool) {
     return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+  }
+
+  function encodeCoordEntity(Coord memory coord, string memory key) internal pure returns (uint256) {
+    return uint256(bytes32(bytes.concat(bytes4(uint32(coord.x)), bytes4(uint32(coord.y)), bytes24(bytes(key)))));
+  }
+
+  function decodeCoordEntity(uint256 entity) internal pure returns (Coord memory) {
+    bytes memory data = abi.encode(bytes32(entity));
+    uint8[] memory sizes = new uint8[](2);
+    sizes[0] = 4;
+    sizes[1] = 4;
+    bytes[] memory decoded = split(data, sizes);
+    return Coord(int32(uint32(bytes4(decoded[0]))), int32(uint32(bytes4(decoded[1]))));
   }
 }
