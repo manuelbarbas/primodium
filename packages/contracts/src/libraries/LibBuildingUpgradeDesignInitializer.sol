7b86c0e1595453cfc7f1ae074d26df8563e73bd4
alireza <najafi73alireza@gmail.com>
2023-06-24 14:23:03 +0300

    feat: initializer library which sets resource and research requirements for upgrading buildings

---
+++ packages/contracts/src/libraries/LibBuildingUpgradeDesignInitializer.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+
+import { IWorld } from "solecs/interfaces/IWorld.sol";
+import { getAddressById } from "solecs/utils.sol";
+// Production Buildings
+import { Uint256Component } from "std-contracts/components/Uint256Component.sol";
+import { Uint256ArrayComponent } from "std-contracts/components/Uint256ArrayComponent.sol";
+import { IUint256Component } from "solecs/interfaces/IUint256Component.sol";
+import { ItemComponent, ID as ItemComponentID } from "components/ItemComponent.sol";
+import { RequiredResourcesComponent, ID as RequiredResourcesComponentID } from "components/RequiredResourcesComponent.sol";
+import { RequiredResearchComponent, ID as RequiredResearchComponentID } from "components/RequiredResearchComponent.sol";
+
+import { MainBaseID, SiloID, BulletFactoryID, DebugPlatingFactoryID } from "../prototypes/Tiles.sol";
+import { LibEncode } from "../libraries/LibEncode.sol";
+
+// production buildings
+import { BasicMinerID, NodeID, PlatingFactoryID, BasicBatteryFactoryID, KineticMissileFactoryID, ProjectileLauncherID, HardenedDrillID, DenseMetalRefineryID, AdvancedBatteryFactoryID, HighTempFoundryID, PrecisionMachineryFactoryID, IridiumDrillbitFactoryID, PrecisionPneumaticDrillID, PenetratorFactoryID, PenetratingMissileFactoryID, MissileLaunchComplexID, HighEnergyLaserFactoryID, ThermobaricWarheadFactoryID, ThermobaricMissileFactoryID, KimberliteCatalystFactoryID } from "../prototypes/Tiles.sol";
+
+// Items
+import { BolutiteResourceItemID, CopperResourceItemID, IridiumResourceItemID, IronResourceItemID, KimberliteResourceItemID, LithiumResourceItemID, OsmiumResourceItemID, TitaniumResourceItemID, TungstenResourceItemID, UraniniteResourceItemID, IronPlateCraftedItemID, BasicPowerSourceCraftedItemID, KineticMissileCraftedItemID, RefinedOsmiumCraftedItemID, AdvancedPowerSourceCraftedItemID, PenetratingWarheadCraftedItemID, PenetratingMissileCraftedItemID, TungstenRodsCraftedItemID, IridiumCrystalCraftedItemID, IridiumDrillbitCraftedItemID, LaserPowerSourceCraftedItemID, ThermobaricWarheadCraftedItemID, ThermobaricMissileCraftedItemID, KimberliteCrystalCatalystCraftedItemID, BulletCraftedItemID } from "../prototypes/Keys.sol";
+
+// Research
+import { CopperResearchID, LithiumResearchID, TitaniumResearchID, OsmiumResearchID, TungstenResearchID, IridiumResearchID, KimberliteResearchID, PlatingFactoryResearchID, BasicBatteryFactoryResearchID, KineticMissileFactoryResearchID, ProjectileLauncherResearchID, HardenedDrillResearchID, DenseMetalRefineryResearchID, AdvancedBatteryFactoryResearchID, HighTempFoundryResearchID, PrecisionMachineryFactoryResearchID, IridiumDrillbitFactoryResearchID, PrecisionPneumaticDrillResearchID, PenetratorFactoryResearchID, PenetratingMissileFactoryResearchID, MissileLaunchComplexResearchID, HighEnergyLaserFactoryResearchID, ThermobaricWarheadFactoryResearchID, ThermobaricMissileFactoryResearchID, KimberliteCatalystFactoryResearchID, FastMinerResearchID } from "../prototypes/Keys.sol";
+
+library LibBuildingUpgradeDesignInitializer {
+
+    function init(IWorld world) internal
+    {
+        IUint256Component components = world.components();
+        ItemComponent itemComponent = ItemComponent(getAddressById(components,ItemComponentID));
+    
+        RequiredResourcesComponent requiredResources =  RequiredResourcesComponent(getAddressById(components,RequiredResourcesComponentID));
+
+
+
+        //MainBaseID Level 2 Upgrade
+        itemComponent.set(LibEncode.hashFromTwoKeys(IronResourceItemID, MainBaseID,2),500);
+        itemComponent.set(LibEncode.hashFromTwoKeys(CopperResourceItemID, MainBaseID,2),500);
+        uint256[] memory requiredResourceIDs = new uint256[](2);
+        requiredResourceIDs[0] = IronResourceItemID;
+        requiredResourceIDs[1] = CopperResourceItemID;
+        requiredResources.set(LibEncode.hashFromKey(MainBaseID,2),requiredResourceIDs);
+
+
+        //MainBaseID Level 3 Upgrade
+        itemComponent.set(LibEncode.hashFromTwoKeys(IronPlateCraftedItemID, MainBaseID,3),500);
+        itemComponent.set(LibEncode.hashFromTwoKeys(BasicPowerSourceCraftedItemID, MainBaseID,3),100);
+        itemComponent.set(LibEncode.hashFromTwoKeys(CopperResourceItemID, MainBaseID,3),2000);
+        requiredResourceIDs = new uint256[](3);
+        requiredResourceIDs[0] = IronPlateCraftedItemID;
+        requiredResourceIDs[1] = BasicPowerSourceCraftedItemID;
+        requiredResourceIDs[2] = CopperResourceItemID;
+        requiredResources.set(LibEncode.hashFromKey(MainBaseID,3),requiredResourceIDs);
+    }
+}
