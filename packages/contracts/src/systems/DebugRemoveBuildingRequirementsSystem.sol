63a011da5a638b955c7a5508814933eec70d94dc
alireza <najafi73alireza@gmail.com>
2023-06-24 21:10:09 +0300

    feat: implemented debug systems to use when writing tests. debug systems only work when IsDebug is true.

---
+++ packages/contracts/src/systems/DebugRemoveBuildingRequirementsSystem.sol
@@ -0,0 +1,34 @@
+pragma solidity >=0.8.0;
+import { System, IWorld } from "solecs/System.sol";
+import { getAddressById } from "solecs/utils.sol";
+import { RequiredResearchComponent, ID as RequiredResearchComponentID } from "components/RequiredResearchComponent.sol";
+import { RequiredResourcesComponent, ID as RequiredResourcesComponentID } from "components/RequiredResourcesComponent.sol";
+import { LibEncode } from "../libraries/LibEncode.sol";
+import { LibDebug } from "../libraries/LibDebug.sol";
+
+
+uint256 constant ID = uint256(keccak256("system.DebugRemoveBuildingRequirements"));
+
+contract DebugRemoveBuildingRequirementsSystem is System {
+  constructor(IWorld _world, address _components) System(_world, _components) {}
+
+  function execute(bytes memory args) public returns (bytes memory) {
+    if (!LibDebug.isDebug()) {
+      revert("Not in debug mode");
+    }
+    (uint256 buildingId) = abi.decode(args, (uint256));
+    RequiredResearchComponent requiredResearch = RequiredResearchComponent(
+      getAddressById(components, RequiredResearchComponentID)
+    );
+    RequiredResourcesComponent requiredResources = RequiredResourcesComponent(
+      getAddressById(components, RequiredResourcesComponentID)
+    );
+    requiredResearch.remove(buildingId);
+    requiredResources.remove(buildingId);
+  }
+
+  function executeTyped(uint256 buildingId) public returns (bytes memory) {
+    return execute(abi.encode(buildingId));
+  }
+
+}
