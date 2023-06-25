1e3a2a0ac017e4a73ad61e3aa46fbb526594813c
alireza <najafi73alireza@gmail.com>
2023-06-25 07:09:11 +0300

    fix: set build limit values to not impact current state of game untill the actual design regarding them has been implemented

--- packages/contracts/src/libraries/LibBuildingLimitDesignInitializer.sol
+++ packages/contracts/src/libraries/LibBuildingLimitDesignInitializer.sol
@@ -1,6 +1,5 @@
 pragma solidity >=0.8.0;
 
-
 import { IWorld } from "solecs/interfaces/IWorld.sol";
 import { getAddressById } from "solecs/utils.sol";
 // Production Buildings
@@ -9,23 +8,20 @@ import { Uint256ArrayComponent } from "std-contracts/components/Uint256ArrayComp
 import { IUint256Component } from "solecs/interfaces/IUint256Component.sol";
 import { BuildingLimitComponent, ID as BuildingLimitComponentID } from "components/BuildingLimitComponent.sol";
 
-
 import { LibEncode } from "../libraries/LibEncode.sol";
 
-
 library LibBuildingLimitDesignInitializer {
+  function init(IWorld world) internal {
+    IUint256Component components = world.components();
+    BuildingLimitComponent buildingLimitComponent = BuildingLimitComponent(
+      getAddressById(components, BuildingLimitComponentID)
+    );
 
-    function init(IWorld world) internal
-    {
-        IUint256Component components = world.components();
-        BuildingLimitComponent buildingLimitComponent = BuildingLimitComponent(getAddressById(components,BuildingLimitComponentID));
-
-        buildingLimitComponent.set(uint256(1), 5);
-        buildingLimitComponent.set(uint256(2), 10);
-        buildingLimitComponent.set(uint256(3), 15);
-        buildingLimitComponent.set(uint256(4), 20);
-        buildingLimitComponent.set(uint256(5), 25);
-        buildingLimitComponent.set(uint256(6), 30);
-
-    }
+    buildingLimitComponent.set(uint256(1), 10000000);
+    buildingLimitComponent.set(uint256(2), 10000005);
+    buildingLimitComponent.set(uint256(3), 10000010);
+    buildingLimitComponent.set(uint256(4), 10000015);
+    buildingLimitComponent.set(uint256(5), 10000020);
+    buildingLimitComponent.set(uint256(6), 10000025);
+  }
 }
