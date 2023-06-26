b016190877a66a5c274b904cb907764fb40997d4
alireza <najafi73alireza@gmail.com>
2023-06-26 16:06:12 +0300

    feat: wrote test to build building requireing resources, providing the resource with debug systems to test gas consumption

--- packages/contracts/src/test/systems/BuildSystem.sol
+++ packages/contracts/src/test/systems/BuildSystem.sol
@@ -1,22 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
-
+import "forge-std/console.sol";
 import { Deploy } from "../Deploy.sol";
 import { MudTest } from "std-contracts/test/MudTest.t.sol";
 import { addressToEntity } from "solecs/utils.sol";
 import { BuildSystem, ID as BuildSystemID } from "../../systems/BuildSystem.sol";
 import { BuildPathSystem, ID as BuildPathSystemID } from "../../systems/BuildPathSystem.sol";
-import { OwnedByComponent, ID as OwnedByComponentID } from "../../components/OwnedByComponent.sol";
+import { DebugAquireResourcesSystem, ID as DebugAquireResourcesSystemID } from "../../systems/DebugAquireResourcesSystem.sol";
 
+import { OwnedByComponent, ID as OwnedByComponentID } from "../../components/OwnedByComponent.sol";
+import { ItemComponent, ID as ItemComponentID } from "../../components/ItemComponent.sol";
 import { BuildingComponent, ID as BuildingComponentID } from "../../components/BuildingComponent.sol";
 import { PathComponent, ID as PathComponentID } from "../../components/PathComponent.sol";
 import { BuildingLimitComponent, ID as BuildingLimitComponentID } from "../../components/BuildingLimitComponent.sol";
-
+import { RequiredResourcesComponent, ID as RequiredResourcesComponentID } from "../../components/RequiredResourcesComponent.sol";
+//debug buildings
 import { MainBaseID, LithiumMinerID, DebugNodeID, MinerID, NodeID, DebugNodeID } from "../../prototypes/Tiles.sol";
+
+//main buildings
+import { BasicMinerID } from "../../prototypes/Tiles.sol";
 import { Coord } from "../../types.sol";
 
 import { LibBuilding } from "../../libraries/LibBuilding.sol";
 import { LibEncode } from "../../libraries/LibEncode.sol";
+import { LibMath } from "../../libraries/LibMath.sol";
 
 contract BuildSystemTest is MudTest {
   constructor() MudTest(new Deploy()) {}
@@ -37,6 +44,47 @@ contract BuildSystemTest is MudTest {
     OwnedByComponent ownedByComponent = OwnedByComponent(component(OwnedByComponentID));
 
     bytes memory blockEntity = buildSystem.executeTyped(LithiumMinerID, coord);
+
+    uint256 blockEntityID = abi.decode(blockEntity, (uint256));
+
+    Coord memory position = LibEncode.decodeCoordEntity(blockEntityID);
+    assertEq(position.x, coord.x);
+    assertEq(position.y, coord.y);
+
+    assertTrue(ownedByComponent.has(blockEntityID));
+    assertEq(ownedByComponent.getValue(blockEntityID), addressToEntity(alice));
+
+    vm.stopPrank();
+  }
+
+  function testBuildWithResourceRequirements() public {
+    vm.startPrank(alice);
+
+    Coord memory coord = Coord({ x: 0, y: 0 });
+
+    BuildSystem buildSystem = BuildSystem(system(BuildSystemID));
+    DebugAquireResourcesSystem debugAquireResourcesSystem = DebugAquireResourcesSystem(
+      system(DebugAquireResourcesSystemID)
+    );
+
+    ItemComponent itemComponent = ItemComponent(component(ItemComponentID));
+    OwnedByComponent ownedByComponent = OwnedByComponent(component(OwnedByComponentID));
+    RequiredResourcesComponent requiredResourcesComponent = RequiredResourcesComponent(
+      component(RequiredResourcesComponentID)
+    );
+
+    assertTrue(requiredResourcesComponent.has(BasicMinerID), "BasicMinerID should have resource requirements");
+    uint256[] memory resourceRequirements = requiredResourcesComponent.getValue(BasicMinerID);
+    assertEq(resourceRequirements.length, 1, "BasicMinerID should have 1 resource requirement");
+    for (uint256 i = 0; i < resourceRequirements.length; i++) {
+      uint256 resourceCost = LibMath.getSafeUint256Value(
+        itemComponent,
+        LibEncode.hashFromTwoKeys(resourceRequirements[i], MainBaseID, 2)
+      );
+      console.log("BasicMiner requires resource: %s of amount %s", resourceRequirements[i], resourceCost);
+      debugAquireResourcesSystem.executeTyped(resourceRequirements[i], resourceCost);
+    }
+    bytes memory blockEntity = buildSystem.executeTyped(BasicMinerID, coord);
 
     uint256 blockEntityID = abi.decode(blockEntity, (uint256));
 
