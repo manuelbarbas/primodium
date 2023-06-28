6fe70817bf99b7d7504964493e00c87781e853e1
alireza <najafi73alireza@gmail.com>
2023-06-28 20:46:32 +0300

    feat: updated BuildPathSystem and DestroyPathSystem to not care about transport nodes and call related functions on LibNewMine to update resource production

--- packages/contracts/src/systems/BuildPathSystem.sol
+++ packages/contracts/src/systems/BuildPathSystem.sol
@@ -5,27 +5,67 @@ import { getAddressById, addressToEntity } from "solecs/utils.sol";
 import { TileComponent, ID as TileComponentID } from "components/TileComponent.sol";
 import { PathComponent, ID as PathComponentID } from "components/PathComponent.sol";
 import { OwnedByComponent, ID as OwnedByComponentID } from "components/OwnedByComponent.sol";
-import { LastBuiltPathAtComponent, ID as LastBuiltPathAtComponentID } from "components/LastBuiltPathAtComponent.sol";
+import { UnclaimedResourceComponent, ID as UnclaimedResourceComponentID } from "components/UnclaimedResourceComponent.sol";
+import { LastClaimedAtComponent, ID as LastClaimedAtComponentID } from "components/LastClaimedAtComponent.sol";
+import { MineComponent, ID as MineComponentID } from "components/MineComponent.sol";
+import { StorageCapacityComponent, ID as StorageCapacityComponentID } from "components/StorageCapacityComponent.sol";
+import { ItemComponent, ID as ItemComponentID } from "components/ItemComponent.sol";
+import { BuildingComponent, ID as BuildingComponentID } from "components/BuildingComponent.sol";
+import { TileComponent, ID as TileComponentID } from "components/TileComponent.sol";
+
 import { DebugNodeID, NodeID } from "../prototypes/Tiles.sol";
 import { BuildingKey } from "../prototypes/Keys.sol";
 
 import { Coord } from "../types.sol";
 
 import { LibEncode } from "../libraries/LibEncode.sol";
-
+import { LibPath } from "../libraries/LibPath.sol";
+import { LibNewMine } from "../libraries/LibNewMine.sol";
+import { LibTerrain } from "../libraries/LibTerrain.sol";
 uint256 constant ID = uint256(keccak256("system.BuildPath"));
 
 contract BuildPathSystem is System {
   constructor(IWorld _world, address _components) System(_world, _components) {}
+
+  function updateUnclaimedForResource(
+    MineComponent mineComponent,
+    StorageCapacityComponent storageCapacityComponent,
+    uint256 playerEntity,
+    uint256 startCoordEntity
+  ) internal {
+    LibNewMine.updateUnclaimedForResource(
+      UnclaimedResourceComponent(getAddressById(components, UnclaimedResourceComponentID)),
+      LastClaimedAtComponent(getAddressById(components, LastClaimedAtComponentID)),
+      mineComponent,
+      storageCapacityComponent,
+      ItemComponent(getAddressById(components, ItemComponentID)),
+      playerEntity,
+      LibTerrain.getTopLayerKey(LibEncode.decodeCoordEntity(startCoordEntity))
+    );
+  }
+
+  function updateResourceProductionOnBuildPathFromMine(
+    TileComponent tileComponent,
+    MineComponent mineComponent,
+    uint256 fromEntity,
+    uint256 toEntity
+  ) internal {
+    BuildingComponent buildingComponent = BuildingComponent(getAddressById(components, BuildingComponentID));
+    LibNewMine.updateResourceProductionOnBuildPathFromMine(
+      mineComponent,
+      buildingComponent,
+      tileComponent,
+      fromEntity,
+      toEntity,
+      addressToEntity(msg.sender)
+    );
+  }
 
   function execute(bytes memory args) public returns (bytes memory) {
     (Coord memory coordStart, Coord memory coordEnd) = abi.decode(args, (Coord, Coord));
     TileComponent tileComponent = TileComponent(getAddressById(components, TileComponentID));
     PathComponent pathComponent = PathComponent(getAddressById(components, PathComponentID));
     OwnedByComponent ownedByComponent = OwnedByComponent(getAddressById(components, OwnedByComponentID));
-    LastBuiltPathAtComponent lastBuiltPathAtComponent = LastBuiltPathAtComponent(
-      getAddressById(components, LastBuiltPathAtComponentID)
-    );
     require(
       !(coordStart.x == coordEnd.x && coordStart.y == coordEnd.y),
       "[BuildPathSystem] Cannot start and end path at the same coordinate"
@@ -37,17 +77,17 @@ contract BuildPathSystem is System {
     uint256 endCoordEntity = LibEncode.encodeCoordEntity(coordEnd, BuildingKey);
     require(tileComponent.has(endCoordEntity), "[BuildPathSystem] Cannot end path at an empty coordinate");
 
-    // Check that the coordinates are both conveyor tiles
-    uint256 tileEntityAtStartCoord = tileComponent.getValue(startCoordEntity);
-    require(
-      tileEntityAtStartCoord == DebugNodeID || tileEntityAtStartCoord == NodeID,
-      "[BuildPathSystem] Cannot start path at a non-supported tile (Conveyor, Node)"
-    );
-    uint256 tileEntityAtEndCoord = tileComponent.getValue(endCoordEntity);
-    require(
-      tileEntityAtEndCoord == DebugNodeID || tileEntityAtEndCoord == NodeID,
-      "[BuildPathSystem] Cannot end path at a non-supported tile (Conveyor, Node)"
-    );
+    // // Check that the coordinates are both conveyor tiles
+    // uint256 tileEntityAtStartCoord = tileComponent.getValue(startCoordEntity);
+    // require(
+    //   tileEntityAtStartCoord == DebugNodeID || tileEntityAtStartCoord == NodeID,
+    //   "[BuildPathSystem] Cannot start path at a non-supported tile (Conveyor, Node)"
+    // );
+    // uint256 tileEntityAtEndCoord = tileComponent.getValue(endCoordEntity);
+    // require(
+    //   tileEntityAtEndCoord == DebugNodeID || tileEntityAtEndCoord == NodeID,
+    //   "[BuildPathSystem] Cannot end path at a non-supported tile (Conveyor, Node)"
+    // );
 
     // Check that the coordinates are both owned by the msg.sender
     uint256 ownedEntityAtStartCoord = ownedByComponent.getValue(startCoordEntity);
@@ -67,8 +107,26 @@ contract BuildPathSystem is System {
       "[BuildPathSystem] Cannot start more than one path from the same tile"
     );
 
+    MineComponent mineComponent = MineComponent(getAddressById(components, MineComponentID));
+    StorageCapacityComponent storageCapacityComponent = StorageCapacityComponent(
+      getAddressById(components, StorageCapacityComponentID)
+    );
+    require(
+      LibPath.checkCanBuildPath(
+        tileComponent,
+        mineComponent,
+        storageCapacityComponent,
+        startCoordEntity,
+        endCoordEntity,
+        addressToEntity(msg.sender)
+      ),
+      "[BuildPathSystem] Cannot build path"
+    );
+
     // Add key
     pathComponent.set(startCoordEntity, endCoordEntity);
+    updateUnclaimedForResource(mineComponent, storageCapacityComponent, addressToEntity(msg.sender), startCoordEntity);
+    updateResourceProductionOnBuildPathFromMine(tileComponent, mineComponent, startCoordEntity, endCoordEntity);
 
     return abi.encode(startCoordEntity);
   }
