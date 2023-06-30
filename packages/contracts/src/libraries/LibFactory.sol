16bb5259502bfa1ad784c3772a0b35333365cdd7
alireza <najafi73alireza@gmail.com>
2023-06-30 17:30:20 +0300

    feat: finalized core functions for new factory design to be used by BuildPathSystem, DestroyPathSystem, UpgradeSystem

--- packages/contracts/src/libraries/LibFactory.sol
+++ packages/contracts/src/libraries/LibFactory.sol
@@ -16,76 +16,116 @@ import { LibTerrain } from "./LibTerrain.sol";
 import { BolutiteResourceItemID, CopperResourceItemID, IridiumResourceItemID, IronResourceItemID, KimberliteResourceItemID, LithiumResourceItemID, OsmiumResourceItemID, TitaniumResourceItemID, TungstenResourceItemID, UraniniteResourceItemID, IronPlateCraftedItemID, BasicPowerSourceCraftedItemID, KineticMissileCraftedItemID, RefinedOsmiumCraftedItemID, AdvancedPowerSourceCraftedItemID, PenetratingWarheadCraftedItemID, PenetratingMissileCraftedItemID, TungstenRodsCraftedItemID, IridiumCrystalCraftedItemID, IridiumDrillbitCraftedItemID, LaserPowerSourceCraftedItemID, ThermobaricWarheadCraftedItemID, ThermobaricMissileCraftedItemID, KimberliteCrystalCatalystCraftedItemID, BulletCraftedItemID } from "../prototypes/Keys.sol";
 
 library LibFactory {
-  function checkCanConnectMineToFactoryAndUpdateFactoryConnectedMineCount(
-    FactoryMineBuildingsComponent factoryMineBuildingsComponent,
-    Uint256Component tileComponent,
-    uint256 mineEntity,
-    uint256 factoryEntity
-  ) internal returns (bool) {
-    FactoryMineBuildingsData memory factoryMineBuildingsData = factoryMineBuildingsComponent.getValue(factoryEntity);
-    for (uint256 i = 0; i < factoryMineBuildingsData.MineBuildingCount.length; i++) {
-      if (factoryMineBuildingsData.MineBuildingIDs[i] == tileComponent.getValue(mineEntity)) {
-        if (factoryMineBuildingsData.MineBuildingCount[i] > 0) {
-          factoryMineBuildingsData.MineBuildingCount[i]--;
-          factoryMineBuildingsComponent.set(factoryEntity, factoryMineBuildingsData);
-          return true;
-        }
-        return false;
-      }
-    }
-    return false;
-  }
+  //Mine changes to Factory Changes
 
-  function checkIsFactoryFunctional(
-    FactoryMineBuildingsComponent factoryMineBuildingsComponent,
+  //checks if path from mine to factory can be built, if yes updates factory is functional status
+  function checkOnBuildPathFromMineToFactory(
     BoolComponent factoryIsFunctionalComponent,
+    FactoryMineBuildingsComponent factoryMineBuildingsComponent,
     Uint256Component buildingComponent,
     Uint256Component tileComponent,
     Uint256Component pathComponent,
+    uint256 mineEntity,
     uint256 factoryEntity
   ) internal returns (bool) {
+    if (factoryIsFunctionalComponent.has(factoryEntity)) return false;
+    bool isFunctional = true;
+    bool isMineConnected = false;
+    FactoryMineBuildingsData memory factoryMineBuildingsData = factoryMineBuildingsComponent.getValue(factoryEntity);
+    for (uint256 i = 0; i < factoryMineBuildingsData.MineBuildingCount.length; i++) {
+      if (factoryMineBuildingsData.MineBuildingIDs[i] == tileComponent.getValue(mineEntity)) {
+        if (factoryMineBuildingsData.MineBuildingCount[i] <= 0) return false;
+        factoryMineBuildingsData.MineBuildingCount[i]--;
+        factoryMineBuildingsComponent.set(factoryEntity, factoryMineBuildingsData);
+        isMineConnected = true;
+        if (factoryMineBuildingsData.MineBuildingCount[i] > 0) isFunctional = false;
+      } else {
+        if (factoryMineBuildingsData.MineBuildingCount[i] > 0) isFunctional = false;
+      }
+    }
     uint256 factoryLevel = buildingComponent.getValue(factoryEntity);
-    uint256 factoryLevelEntity = LibEncode.hashFromKey(tileComponent.getValue(factoryEntity), factoryLevel);
     uint256[] memory connectedMineEntities = pathComponent.getEntitiesWithValue(factoryEntity);
-    FactoryMineBuildingsData memory factoryMineBuildingsData = factoryMineBuildingsComponent.getValue(
-      factoryLevelEntity
-    );
     for (uint256 i = 0; i < connectedMineEntities.length; i++) {
       if (buildingComponent.getValue(connectedMineEntities[i]) < factoryLevel) {
-        factoryIsFunctionalComponent.remove(factoryEntity);
-        return false;
-      }
-      for (uint256 j = 0; j < factoryMineBuildingsData.MineBuildingCount.length; j++) {
-        if (factoryMineBuildingsData.MineBuildingIDs[j] == tileComponent.getValue(connectedMineEntities[i])) {
-          factoryMineBuildingsData.MineBuildingCount[j]--;
-        }
+        isFunctional = false;
+        return isMineConnected;
       }
     }
+
+    if (isFunctional) {
+      factoryIsFunctionalComponent.set(factoryEntity);
+    }
+
+    return isMineConnected;
+  }
+
+  //when a path from mine to factory is destroyed, factory becomes non functional and required connected mine building count is increased
+  function onPathFromMineToFactoryDestroyed(
+    BoolComponent factoryIsFunctionalComponent, //writes
+    FactoryMineBuildingsComponent factoryMineBuildingsComponent, //writes
+    Uint256Component tileComponent,
+    uint256 mineEntity,
+    uint256 factoryEntity
+  ) internal {
+    factoryIsFunctionalComponent.remove(factoryEntity);
+    FactoryMineBuildingsData memory factoryMineBuildingsData = factoryMineBuildingsComponent.getValue(factoryEntity);
     for (uint256 i = 0; i < factoryMineBuildingsData.MineBuildingCount.length; i++) {
-      if (factoryMineBuildingsData.MineBuildingCount[i] > 0) {
-        factoryIsFunctionalComponent.remove(factoryEntity);
-        return false;
+      if (factoryMineBuildingsData.MineBuildingIDs[i] == tileComponent.getValue(mineEntity)) {
+        factoryMineBuildingsData.MineBuildingCount[i]++;
+        factoryMineBuildingsComponent.set(factoryEntity, factoryMineBuildingsData);
+        return;
       }
+    }
+  }
+
+  //when a mine connected to a factory is upgraded all mines connected to that factory are checked if they are at the required level and if the number of connected mines is enough
+  function onMineConnectedToFactoryUpgrade(
+    BoolComponent factoryIsFunctionalComponent, //writes
+    FactoryMineBuildingsComponent factoryMineBuildingsComponent, //writes
+    Uint256Component pathComponent,
+    Uint256Component buildingComponent,
+    uint256 factoryEntity
+  ) internal {
+    if (factoryIsFunctionalComponent.has(factoryEntity)) return;
+    uint256 factoryLevel = buildingComponent.getValue(factoryEntity);
+    uint256[] memory connectedMineEntities = pathComponent.getEntitiesWithValue(factoryEntity);
+    for (uint256 i = 0; i < connectedMineEntities.length; i++) {
+      if (buildingComponent.getValue(connectedMineEntities[i]) < factoryLevel) {
+        return;
+      }
+    }
+    FactoryMineBuildingsData memory factoryMineBuildingsData = factoryMineBuildingsComponent.getValue(factoryEntity);
+    for (uint256 i = 0; i < factoryMineBuildingsData.MineBuildingCount.length; i++) {
+      if (factoryMineBuildingsData.MineBuildingCount[i] > 0) return;
     }
     factoryIsFunctionalComponent.set(factoryEntity);
-    return true;
   }
+
+  function onFactoryUpgraded() internal {}
+
+  //checks all required conditions for a factory to be functional and updates factory is functional status
+  function updateFactoryIsFunctional(
+    BoolComponent factoryIsFunctionalComponent, //writes
+    FactoryMineBuildingsComponent factoryMineBuildingsComponent, //writes
+    Uint256Component pathComponent,
+    Uint256Component buildingComponent,
+    Uint256Component tileComponent,
+    uint256 mineEntity,
+    uint256 factoryEntity
+  ) internal {}
 
   function checkFactoryConnectedMinesLevelCondition(
     BoolComponent factoryIsFunctionalComponent,
     Uint256Component buildingComponent,
     Uint256Component pathComponent,
     uint256 factoryEntity
-  ) internal returns (bool) {
+  ) internal view returns (bool) {
+    if (!factoryIsFunctionalComponent.has(factoryEntity)) return false;
     uint256 factoryLevel = buildingComponent.getValue(factoryEntity);
     uint256[] memory connectedMineEntities = pathComponent.getEntitiesWithValue(factoryEntity);
     for (uint256 i = 0; i < connectedMineEntities.length; i++) {
-      if (buildingComponent.getValue(connectedMineEntities[i]) < factoryLevel) {
-        factoryIsFunctionalComponent.remove(factoryEntity);
-        return false;
-      }
+      if (buildingComponent.getValue(connectedMineEntities[i]) < factoryLevel) return false;
     }
-    factoryIsFunctionalComponent.set(factoryEntity);
     return true;
   }
 
@@ -124,6 +164,7 @@ library LibFactory {
     uint256 playerEntity,
     uint256 factoryEntity
   ) internal {
+    if (!factoryIsFunctionalComponent.has(factoryEntity)) return;
     uint256 buildingLevelEntityPreUpgrade = LibEncode.hashKeyEntity(
       tileComponent.getValue(factoryEntity),
       buildingComponent.getValue(factoryEntity) - 1
@@ -162,6 +203,8 @@ library LibFactory {
     }
   }
 
+  //Final
+  //call after upgrade has been done and level has been increased
   function updateResourceProductionOnDestroyPathFromFactoryToMainBase(
     FactoryProductionComponent factoryProductionComponent,
     BoolComponent factoryIsFunctionalComponent,
@@ -182,6 +225,8 @@ library LibFactory {
     );
   }
 
+  //Final
+  //call after upgrade has been done and level has been increased
   function updateResourceProductionOnBuildPathFromFactoryToMainBase(
     FactoryProductionComponent factoryProductionComponent,
     BoolComponent factoryIsFunctionalComponent,
@@ -200,5 +245,30 @@ library LibFactory {
       playerResourceEntity, //player resource production entity
       LibMath.getSafeUint256Value(mineComponent, playerResourceEntity) + factoryProductionData.ResourceProductionRate //current total resource production // resource production of factory
     );
+  }
+
+  function updateResourceProductionOnFactoryIsFunctionalChange(
+    FactoryProductionComponent factoryProductionComponent,
+    Uint256Component mineComponent,
+    Uint256Component tileComponent,
+    Uint256Component buildingComponent,
+    uint256 playerEntity,
+    uint256 factoryEntity,
+    bool isFunctional
+  ) internal {
+    uint256 buildingId = tileComponent.getValue(factoryEntity);
+    uint256 buildingLevelEntity = LibEncode.hashKeyEntity(buildingId, buildingComponent.getValue(factoryEntity));
+    FactoryProductionData memory factoryProductionData = factoryProductionComponent.getValue(buildingLevelEntity);
+    uint256 playerResourceEntity = LibEncode.hashKeyEntity(factoryProductionData.ResourceID, playerEntity);
+    if (isFunctional)
+      mineComponent.set(
+        playerResourceEntity, //player resource production entity
+        LibMath.getSafeUint256Value(mineComponent, playerResourceEntity) + factoryProductionData.ResourceProductionRate
+      );
+    else
+      mineComponent.set(
+        playerResourceEntity, //player resource production entity
+        LibMath.getSafeUint256Value(mineComponent, playerResourceEntity) - factoryProductionData.ResourceProductionRate
+      );
   }
 }
