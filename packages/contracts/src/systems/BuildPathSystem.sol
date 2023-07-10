43d1be3bb40f3de87e786479444793c0e9135a2c
0xhank <zeroxhank@gmail.com>
2023-07-10 11:58:59 -0400

    feat: make path use building tiles

--- packages/contracts/src/systems/BuildPathSystem.sol
+++ packages/contracts/src/systems/BuildPathSystem.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
-import { System, IWorld } from "solecs/System.sol";
-import { getAddressById, addressToEntity } from "solecs/utils.sol";
+
+import { PrimodiumSystem, IWorld, getAddressById, addressToEntity, entityToAddress } from "systems/internal/PrimodiumSystem.sol";
 import { TileComponent, ID as TileComponentID } from "components/TileComponent.sol";
 import { PathComponent, ID as PathComponentID } from "components/PathComponent.sol";
 import { OwnedByComponent, ID as OwnedByComponentID } from "components/OwnedByComponent.sol";
 import { DebugNodeID, NodeID } from "../prototypes/Tiles.sol";
-import { BuildingKey } from "../prototypes/Keys.sol";
+import { BuildingKey, BuildingTileKey } from "../prototypes/Keys.sol";
 
 import { Coord } from "../types.sol";
 
@@ -14,14 +14,16 @@ import { LibEncode } from "../libraries/LibEncode.sol";
 
 uint256 constant ID = uint256(keccak256("system.BuildPath"));
 
-contract BuildPathSystem is System {
-  constructor(IWorld _world, address _components) System(_world, _components) {}
+contract BuildPathSystem is PrimodiumSystem {
+  constructor(IWorld _world, address _components) PrimodiumSystem(_world, _components) {}
 
-  function execute(bytes memory args) public returns (bytes memory) {
+  function execute(bytes memory args) public override returns (bytes memory) {
     (Coord memory coordStart, Coord memory coordEnd) = abi.decode(args, (Coord, Coord));
     TileComponent tileComponent = TileComponent(getAddressById(components, TileComponentID));
     PathComponent pathComponent = PathComponent(getAddressById(components, PathComponentID));
     OwnedByComponent ownedByComponent = OwnedByComponent(getAddressById(components, OwnedByComponentID));
+
+    uint256 playerEntity = addressToEntity(msg.sender);
 
     require(
       !(coordStart.x == coordEnd.x && coordStart.y == coordEnd.y),
@@ -29,45 +31,41 @@ contract BuildPathSystem is System {
     );
 
     // Check that the coordinates exist tiles
-    uint256 startCoordEntity = LibEncode.encodeCoordEntity(coordStart, BuildingKey);
-    require(tileComponent.has(startCoordEntity), "[BuildPathSystem] Cannot start path at an empty coordinate");
-    uint256 endCoordEntity = LibEncode.encodeCoordEntity(coordEnd, BuildingKey);
-    require(tileComponent.has(endCoordEntity), "[BuildPathSystem] Cannot end path at an empty coordinate");
+    uint256 startBuilding = getBuildingFromCoord(coordStart);
+    uint256 endBuilding = getBuildingFromCoord(coordEnd);
 
     // Check that the coordinates are both conveyor tiles
-    uint256 tileEntityAtStartCoord = tileComponent.getValue(startCoordEntity);
+    uint256 tileEntityAtStartCoord = tileComponent.getValue(startBuilding);
     require(
       tileEntityAtStartCoord == DebugNodeID || tileEntityAtStartCoord == NodeID,
       "[BuildPathSystem] Cannot start path at a non-supported tile (Conveyor, Node)"
     );
-    uint256 tileEntityAtEndCoord = tileComponent.getValue(endCoordEntity);
+    uint256 tileEntityAtEndCoord = tileComponent.getValue(endBuilding);
     require(
       tileEntityAtEndCoord == DebugNodeID || tileEntityAtEndCoord == NodeID,
       "[BuildPathSystem] Cannot end path at a non-supported tile (Conveyor, Node)"
     );
 
     // Check that the coordinates are both owned by the msg.sender
-    uint256 ownedEntityAtStartCoord = ownedByComponent.getValue(startCoordEntity);
     require(
-      ownedEntityAtStartCoord == addressToEntity(msg.sender),
-      "[BuildPathSystem] Cannot start path at a tile you do not own"
+      ownedByComponent.getValue(startBuilding) == playerEntity,
+      "[BuildPathSystem] Cannot start path at a building you do not own"
     );
-    uint256 ownedEntityAtEndCoord = ownedByComponent.getValue(endCoordEntity);
     require(
-      ownedEntityAtEndCoord == addressToEntity(msg.sender),
-      "[BuildPathSystem] Cannot end path at a tile you do not own"
+      ownedByComponent.getValue(endBuilding) == playerEntity,
+      "[BuildPathSystem] Cannot end path at a building you do not own"
     );
 
     // Check that a path doesn't already start there (each tile can only be the start of one path)
     require(
-      !pathComponent.has(startCoordEntity),
-      "[BuildPathSystem] Cannot start more than one path from the same tile"
+      !pathComponent.has(startBuilding),
+      "[BuildPathSystem] Cannot start more than one path from the same building"
     );
 
     // Add key
-    pathComponent.set(startCoordEntity, endCoordEntity);
+    pathComponent.set(startBuilding, endBuilding);
 
-    return abi.encode(startCoordEntity);
+    return abi.encode(startBuilding);
   }
 
   function executeTyped(Coord memory coordStart, Coord memory coordEnd) public returns (bytes memory) {
--- packages/contracts/src/systems/internal/PrimodiumSystem.sol
+++ packages/contracts/src/systems/internal/PrimodiumSystem.sol
@@ -3,14 +3,25 @@ pragma solidity >=0.8.0;
 
 import { System, IWorld } from "solecs/System.sol";
 import { getAddressById, addressToEntity, entityToAddress } from "solecs/utils.sol";
+import { OwnedByComponent, ID as OwnedByComponentID } from "components/OwnedByComponent.sol";
 
-uint256 constant ID = uint256(keccak256("system.Blueprint"));
+import { Coord } from "../../types.sol";
+import { BuildingTileKey } from "../../prototypes/Keys.sol";
+
+import { LibEncode } from "../../libraries/LibEncode.sol";
 
 contract PrimodiumSystem is System {
   constructor(IWorld _world, address _components) System(_world, _components) {}
 
   function getC(uint256 id) internal view returns (address) {
     return getAddressById(components, id);
+  }
+
+  function getBuildingFromCoord(Coord memory coord) internal view returns (uint256) {
+    OwnedByComponent ownedByComponent = OwnedByComponent(getAddressById(components, OwnedByComponentID));
+    uint256 buildingTile = LibEncode.encodeCoordEntity(coord, BuildingTileKey);
+    require(ownedByComponent.has(buildingTile), "tile not owned");
+    return ownedByComponent.getValue(buildingTile);
   }
 
   function execute(bytes memory args) public virtual returns (bytes memory) {}
