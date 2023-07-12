1af69b82274cbbdf4aea85c61019d7399d810b40
alireza <najafi73alireza@gmail.com>
2023-07-13 00:36:11 +0300

    wip: left out upgrade.ts

--- packages/client/src/util/upgrade.ts
+++ packages/client/src/util/upgrade.ts
@@ -1,15 +1,16 @@
 import { EntityID, EntityIndex, World, hasComponent } from "@latticexyz/recs";
 import { NetworkComponents } from "@latticexyz/std-client";
-
+import { Coord } from "@latticexyz/utils";
 import { defineComponents } from "../network/components";
-
 export function canBeUpgraded(
   buildingEntity: EntityID,
   buildingType: EntityID,
+  coords: Coord,
   world: World,
   components: NetworkComponents<ReturnType<typeof defineComponents>>
 ): undefined | boolean {
   console.log("reached can be upgraded");
+
   if (
     !hasComponent(
       components.MaxLevel,
