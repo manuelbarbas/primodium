a169fd739d54765a60624f85c8fbd9c164baa833
alireza <najafi73alireza@gmail.com>
2023-07-13 12:49:08 +0300

    fix: raised gas limit for build main base

--- packages/client/src/components/action/NavigateMainBaseButton.tsx
+++ packages/client/src/components/action/NavigateMainBaseButton.tsx
@@ -58,7 +58,7 @@ export default function NavigateMainBaseButton() {
         BigNumber.from(BlockType.MainBase),
         selectedTile ?? cameraCoord,
         {
-          gasLimit: 1_800_000,
+          gasLimit: 2_800_000,
         }
       ),
       providers,
