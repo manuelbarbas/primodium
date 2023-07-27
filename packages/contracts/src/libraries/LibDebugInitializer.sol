21b5c3fce40e8f67985dd7006195963d46744a3e
0xhank <zeroxhank@gmail.com>
2023-07-27 13:58:15 -0400

    chore: move debug buildings to prototypes

--- packages/contracts/src/libraries/LibDebugInitializer.sol
+++ packages/contracts/src/libraries/LibDebugInitializer.sol
@@ -40,56 +40,6 @@ import { ResourceValue, ResourceValues } from "../types.sol";
 import { LibDebug } from "../libraries/LibDebug.sol";
 import { LibBlueprint } from "../libraries/LibBlueprint.sol";
 
-//buildings
-uint256 constant DebugSimpleBuildingNoReqsID = uint256(keccak256("block.DebugSimpleBuildingNoReqs"));
-uint256 constant DebugSimpleBuildingResourceReqsID = uint256(keccak256("block.DebugSimpleBuildingResourceReqs"));
-uint256 constant DebugSimpleBuildingResearchReqsID = uint256(keccak256("block.DebugSimpleBuildingResearchReqs"));
-uint256 constant DebugSimpleBuildingBuildLimitReq = uint256(keccak256("block.DebugSimpleBuildingBuildLimitReq"));
-uint256 constant DebugSimpleBuildingTileReqID = uint256(keccak256("block.DebugSimpleBuildingTileReq"));
-
-uint256 constant DebugSimpleBuildingPassiveResourceRequirement = uint256(
-  keccak256("block.DebugSimpleBuildingPassiveResourceRequirement")
-);
-
-uint256 constant DebugSimpleBuildingWithUpgradeResourceReqsID = uint256(
-  keccak256("block.DebugSimpleBuildingWithUpgradeResourceReqs")
-);
-uint256 constant DebugSimpleBuildingWithUpgradeResearchReqsID = uint256(
-  keccak256("block.DebugSimpleBuildingWithUpgradeResearchReqs")
-);
-
-// mines
-uint256 constant DebugIronMineID = uint256(keccak256("block.DebugIronMine"));
-uint256 constant DebugCopperMineID = uint256(keccak256("block.DebugCopperMine"));
-uint256 constant DebugLithiumMineID = uint256(keccak256("block.DebugLithiumMine"));
-
-uint256 constant DebugIronMineWithBuildLimitID = uint256(keccak256("block.DebugIronMineWithBuildLimit"));
-uint256 constant DebugIronMineNoTileReqID = uint256(keccak256("block.DebugIronMineNoTileReq"));
-
-//factories
-uint256 constant DebugIronPlateFactoryNoMineReqID = uint256(keccak256("block.DebugIronPlateFactoryNoMineReq"));
-uint256 constant DebugIronPlateFactoryID = uint256(keccak256("block.DebugIronPlateFactory"));
-uint256 constant DebugAlloyFactoryID = uint256(keccak256("block.DebugAlloyFactory"));
-uint256 constant DebugLithiumCopperOxideFactoryID = uint256(keccak256("block.DebugLithiumCopperOxideFactory"));
-
-uint256 constant DebugSolarPanelID = uint256(keccak256("block.DebugSolarPanel"));
-
-uint256 constant DebugPassiveProductionBuilding = uint256(keccak256("block.DebugPassiveProductionBuilding"));
-
-//super buildings
-uint256 constant DebugSuperIronMineID = uint256(keccak256("block.DebugSuperIronMine"));
-uint256 constant DebugSuperIronPlateFactoryID = uint256(keccak256("block.DebugSuperIronPlateFactory"));
-//technologies
-uint256 constant DebugSimpleTechnologyNoReqsID = uint256(keccak256("block.DebugSimpleTechnologyNoReqs"));
-uint256 constant DebugSimpleTechnologyResourceReqsID = uint256(keccak256("block.DebugSimpleTechnologyResourceReqs"));
-uint256 constant DebugSimpleTechnologyResearchReqsID = uint256(keccak256("block.DebugSimpleTechnologyResearchReqs"));
-uint256 constant DebugSimpleTechnologyMainBaseLevelReqsID = uint256(
-  keccak256("block.DebugSimpleTechnologyMainBaseLevelReqs")
-);
-
-//storage building
-uint256 constant DebugStorageBuildingID = uint256(keccak256("block.DebugStorageBuilding"));
-
 // the purpose of this lib is to define and initialize debug buildings that can be used for testing
 // so additions and removal of actual game design elements don't effect the already written tests
 library LibDebugInitializer {
@@ -278,11 +228,10 @@ library LibDebugInitializer {
 
     //DebugSimpleBuildingWithUpgradeResearchReqsID
     maxLevelComponent.set(DebugSimpleBuildingWithUpgradeResearchReqsID, 2);
-    LibSetUpgradeResearchRequirements.setRequiredResearchForEntityUpgradeToLevel(
-      requiredResearch,
-      DebugSimpleBuildingWithUpgradeResearchReqsID,
-      DebugSimpleTechnologyNoReqsID,
-      2
+    uint256 buildingIdLevel = LibEncode.hashKeyEntity(DebugSimpleBuildingWithUpgradeResearchReqsID, 2);
+    RequiredResearchComponent(world.getComponent(RequiredResearchComponentID)).set(
+      LibEncode.hashKeyEntity(DebugSimpleTechnologyNoReqsID, buildingIdLevel),
+      DebugSimpleTechnologyNoReqsID
     );
 
     //DebugSimpleBuildingPassiveResourceRequirement
