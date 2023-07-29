5859f6d1d9126c62d714dbb255606b52b9fa75d0
alireza <najafi73alireza@gmail.com>
2023-07-29 16:02:02 +0300

    fix: updated client to use building type and level for build action regarding resource requirements

--- packages/client/src/util/encode.ts
+++ packages/client/src/util/encode.ts
@@ -83,8 +83,8 @@ export function hashKeyEntity(
 // Remove leading zeros due to mudv1 hashing behavior
 // if there are leading zeroes, the key in world.entityToIndex will be trimmed
 export function hashKeyEntityAndTrim(
-  key: EntityID | number,
-  entity: EntityID | string
+  key: EntityID | string | number,
+  entity: EntityID | string | number
 ): EntityID {
   return BigNumber.from(hashKeyEntity(key, entity)).toHexString() as EntityID;
 }
