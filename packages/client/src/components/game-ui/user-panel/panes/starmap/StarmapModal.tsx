8cf469079ce80e319ab721249cdafe5cb0bb615c
Nabs <43505437+NAB5@users.noreply.github.com>
2023-07-31 08:49:15 -0500

    chore: unused vars

--- packages/client/src/components/game-ui/user-panel/panes/starmap/StarmapModal.tsx
+++ packages/client/src/components/game-ui/user-panel/panes/starmap/StarmapModal.tsx
@@ -1,13 +1,10 @@
 import PortalModal from "src/components/shared/PortalModal";
 import { Starmap } from "./Starmap";
-import { useState } from "react";
 
 export const FullStarmap: React.FC<{ show: boolean; onClose: () => void }> = ({
   show,
   onClose,
 }) => {
-  const [ready, setReady] = useState(false);
-
   return (
     <PortalModal show={show} onClose={onClose}>
       <div className="w-[40rem] h-96">{<Starmap gridSize={16} />}</div>
