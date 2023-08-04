b1cc65c2697b1fc1eda4020b74631c029a8fc527
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-08-04 17:39:48 +0100

    fix: register technologies in LibInitResearch

--- packages/contracts/src/libraries/LibInitResearch.sol
+++ packages/contracts/src/libraries/LibInitResearch.sol
@@ -6,6 +6,7 @@ import { IWorld } from "solecs/interfaces/IWorld.sol";
 // Components
 import { HasResearchedComponent, ID as HasResearchedComponentID } from "components/HasResearchedComponent.sol";
 import { LevelComponent, ID as LevelComponentID } from "components/LevelComponent.sol";
+import { IsTechComponent, ID as IsTechComponentID } from "components/IsTechComponent.sol";
 
 // Libraries
 import { LibEncode } from "../libraries/LibEncode.sol";
@@ -22,6 +23,7 @@ library LibInitResearch {
       world.getComponent(HasResearchedComponentID)
     );
     LevelComponent levelComponent = LevelComponent(world.getComponent(LevelComponentID));
+    IsTechComponent isTechComponent = IsTechComponent(world.getComponent(IsTechComponentID));
 
     ResourceValue[] memory requiredResources = new ResourceValue[](1);
 
@@ -31,18 +33,21 @@ library LibInitResearch {
     LibSetBuildingReqs.setResourceReqs(world, CopperMineResearchID, requiredResources);
     hasResearchedComponent.set(CopperMineResearchID);
     levelComponent.set(CopperMineResearchID, 2);
+    isTechComponent.set(CopperMineResearchID);
 
     hasResearchedComponent.set(CopperMine2ResearchID);
     levelComponent.set(CopperMine2ResearchID, 3);
     requiredResources = new ResourceValue[](1);
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 1500 });
     LibSetBuildingReqs.setResourceReqs(world, CopperMine2ResearchID, requiredResources);
+    isTechComponent.set(CopperMine2ResearchID);
 
     hasResearchedComponent.set(CopperMine3ResearchID);
     levelComponent.set(CopperMine3ResearchID, 5);
     requiredResources = new ResourceValue[](1);
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 4000 });
     LibSetBuildingReqs.setResourceReqs(world, CopperMine3ResearchID, requiredResources);
+    isTechComponent.set(CopperMine3ResearchID);
 
     // Iron Mine
     hasResearchedComponent.set(IronMine2ResearchID);
@@ -50,12 +55,14 @@ library LibInitResearch {
     requiredResources = new ResourceValue[](1);
     requiredResources[0] = ResourceValue({ resource: CopperResourceItemID, value: 300 });
     LibSetBuildingReqs.setResourceReqs(world, IronMine2ResearchID, requiredResources);
+    isTechComponent.set(IronMine2ResearchID);
 
     hasResearchedComponent.set(IronMine3ResearchID);
     levelComponent.set(IronMine3ResearchID, 3);
     requiredResources = new ResourceValue[](1);
     requiredResources[0] = ResourceValue({ resource: CopperResourceItemID, value: 3000 });
     LibSetBuildingReqs.setResourceReqs(world, IronMine3ResearchID, requiredResources);
+    isTechComponent.set(IronMine3ResearchID);
 
     // Iron Plate Factory
     hasResearchedComponent.set(IronPlateFactoryResearchID);
@@ -64,6 +71,7 @@ library LibInitResearch {
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 1200 });
     requiredResources[1] = ResourceValue({ resource: CopperResourceItemID, value: 1000 });
     LibSetBuildingReqs.setResourceReqs(world, IronPlateFactoryResearchID, requiredResources);
+    isTechComponent.set(IronPlateFactoryResearchID);
 
     hasResearchedComponent.set(IronPlateFactory2ResearchID);
     levelComponent.set(IronPlateFactory2ResearchID, 4);
@@ -71,12 +79,14 @@ library LibInitResearch {
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 3000 });
     requiredResources[1] = ResourceValue({ resource: CopperResourceItemID, value: 3000 });
     LibSetBuildingReqs.setResourceReqs(world, IronPlateFactory2ResearchID, requiredResources);
+    isTechComponent.set(IronPlateFactory2ResearchID);
 
     hasResearchedComponent.set(IronPlateFactory3ResearchID);
     levelComponent.set(IronPlateFactory3ResearchID, 5);
     requiredResources = new ResourceValue[](1);
     requiredResources[0] = ResourceValue({ resource: IronPlateCraftedItemID, value: 1200 });
     LibSetBuildingReqs.setResourceReqs(world, IronPlateFactory3ResearchID, requiredResources);
+    isTechComponent.set(IronPlateFactory3ResearchID);
 
     // Storage Unit
     hasResearchedComponent.set(StorageUnitResearchID);
@@ -85,6 +95,7 @@ library LibInitResearch {
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 1000 });
     requiredResources[1] = ResourceValue({ resource: CopperResourceItemID, value: 1000 });
     LibSetBuildingReqs.setResourceReqs(world, StorageUnitResearchID, requiredResources);
+    isTechComponent.set(StorageUnitResearchID);
 
     hasResearchedComponent.set(StorageUnit2ResearchID);
     levelComponent.set(StorageUnit2ResearchID, 4);
@@ -92,6 +103,7 @@ library LibInitResearch {
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 2000 });
     requiredResources[1] = ResourceValue({ resource: CopperResourceItemID, value: 2000 });
     LibSetBuildingReqs.setResourceReqs(world, StorageUnit2ResearchID, requiredResources);
+    isTechComponent.set(StorageUnit2ResearchID);
 
     hasResearchedComponent.set(StorageUnit3ResearchID);
     levelComponent.set(StorageUnit3ResearchID, 5);
@@ -99,6 +111,7 @@ library LibInitResearch {
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 4000 });
     requiredResources[1] = ResourceValue({ resource: CopperResourceItemID, value: 4000 });
     LibSetBuildingReqs.setResourceReqs(world, StorageUnit3ResearchID, requiredResources);
+    isTechComponent.set(StorageUnit3ResearchID);
 
     // Lithium Mine
     hasResearchedComponent.set(LithiumMineResearchID);
@@ -106,6 +119,7 @@ library LibInitResearch {
     requiredResources = new ResourceValue[](1);
     requiredResources[0] = ResourceValue({ resource: IronPlateCraftedItemID, value: 1000 });
     LibSetBuildingReqs.setResourceReqs(world, LithiumMineResearchID, requiredResources);
+    isTechComponent.set(LithiumMineResearchID);
 
     hasResearchedComponent.set(LithiumMine2ResearchID);
     levelComponent.set(LithiumMine2ResearchID, 5);
@@ -113,6 +127,7 @@ library LibInitResearch {
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 4000 });
     requiredResources[1] = ResourceValue({ resource: CopperResourceItemID, value: 4000 });
     LibSetBuildingReqs.setResourceReqs(world, CopperMine2ResearchID, requiredResources);
+    isTechComponent.set(LithiumMine2ResearchID);
 
     // Alloy Factory
     hasResearchedComponent.set(AlloyFactoryResearchID);
@@ -121,6 +136,7 @@ library LibInitResearch {
     requiredResources[0] = ResourceValue({ resource: IronPlateCraftedItemID, value: 3000 });
     requiredResources[1] = ResourceValue({ resource: LithiumResourceItemID, value: 1000 });
     LibSetBuildingReqs.setResourceReqs(world, AlloyFactoryResearchID, requiredResources);
+    isTechComponent.set(AlloyFactoryResearchID);
 
     // Lithium Copper Oxide Factory
     hasResearchedComponent.set(LithiumCopperOxideFactoryResearchID);
@@ -129,15 +145,16 @@ library LibInitResearch {
     requiredResources[0] = ResourceValue({ resource: IronPlateCraftedItemID, value: 3000 });
     requiredResources[1] = ResourceValue({ resource: CopperResourceItemID, value: 5000 });
     LibSetBuildingReqs.setResourceReqs(world, LithiumCopperOxideFactoryResearchID, requiredResources);
+    isTechComponent.set(LithiumCopperOxideFactoryResearchID);
 
     // Solar Panel
     hasResearchedComponent.set(SolarPanelResearchID);
     levelComponent.set(SolarPanelResearchID, 5);
-
     requiredResources = new ResourceValue[](3);
     requiredResources[0] = ResourceValue({ resource: IronResourceItemID, value: 2500 });
     requiredResources[1] = ResourceValue({ resource: CopperResourceItemID, value: 2500 });
     requiredResources[2] = ResourceValue({ resource: LithiumResourceItemID, value: 1500 });
     LibSetBuildingReqs.setResourceReqs(world, SolarPanelResearchID, requiredResources);
+    isTechComponent.set(SolarPanelResearchID);
   }
 }
