1fc6133d0f01d0c0e7115b072c2d708d8ce1b732
0xhank <zeroxhank@gmail.com>
2023-08-07 14:41:32 +0100

    feat: make the color green

--- packages/contracts/prod-json-gen.mjs
+++ packages/contracts/prod-json-gen.mjs
@@ -3,7 +3,8 @@ import fs from "fs";
 
 const isDevOrDebug = (string) => {
   const toRemove = /debug|dev/i.test(string);
-  if (toRemove) console.log("removing ", string);
+  if (toRemove) console.log("\x1b[32m%s\x1b[0m", `removing ${string}`);
+
   return !toRemove;
 };
 function removeDebugAndDev(data) {
