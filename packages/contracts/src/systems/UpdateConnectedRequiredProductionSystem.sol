1092b8d5d3c7b848b47eacbecd541e72812aad59
0xhank <zeroxhank@gmail.com>
2023-08-07 19:10:08 +0100

    chore: clean updateconnectedrequiredproductionsystem

--- packages/contracts/src/systems/UpdateConnectedRequiredProductionSystem.sol
+++ packages/contracts/src/systems/UpdateConnectedRequiredProductionSystem.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 import { PrimodiumSystem, IWorld, addressToEntity, getAddressById } from "./internal/PrimodiumSystem.sol";
 
@@ -7,17 +8,12 @@ import { ID as DestroyPathSystemID } from "./DestroyPathSystem.sol";
 
 import { IOnBuildingSubsystem, EActionType } from "../interfaces/IOnBuildingSubsystem.sol";
 import { RequiredConnectedProductionComponent, ID as RequiredConnectedProductionComponentID, ResourceValues } from "../components/RequiredConnectedProductionComponent.sol";
-import { ItemComponent, ID as ItemComponentID } from "../components/ItemComponent.sol";
-import { MaxStorageComponent, ID as MaxStorageComponentID } from "../components/MaxStorageComponent.sol";
-import { MaxResourceStorageComponent, ID as MaxResourceStorageComponentID } from "../components/MaxResourceStorageComponent.sol";
 import { BuildingTypeComponent, ID as BuildingTypeComponentID } from "../components/BuildingTypeComponent.sol";
 import { LevelComponent, ID as LevelComponentID } from "../components/LevelComponent.sol";
 import { BuildingProductionComponent, ID as BuildingProductionComponentID } from "../components/BuildingProductionComponent.sol";
 import { PathComponent, ID as PathComponentID } from "../components/PathComponent.sol";
 import { LibEncode } from "../libraries/LibEncode.sol";
-import { LibMath } from "../libraries/LibMath.sol";
-import { LibResource } from "../libraries/LibResource.sol";
-import { LibStorage } from "../libraries/LibStorage.sol";
+
 uint256 constant ID = uint256(keccak256("system.UpdateConnectedRequiredProduction"));
 
 contract UpdateConnectedRequiredProductionSystem is IOnBuildingSubsystem, PrimodiumSystem {
@@ -41,12 +37,10 @@ contract UpdateConnectedRequiredProductionSystem is IOnBuildingSubsystem, Primod
     uint32 buildingLevel = LevelComponent(getAddressById(world.components(), LevelComponentID)).getValue(
       buildingEntity
     );
-    uint256 playerEntity = addressToEntity(playerAddress);
 
     uint256 buildingIdNewLevel = LibEncode.hashKeyEntity(buildingType, buildingLevel);
 
-    PathComponent pathComponent = PathComponent(getAddressById(world.components(), PathComponentID));
-    uint256 toEntity = pathComponent.getValue(buildingEntity);
+    uint256 toEntity = PathComponent(getAddressById(world.components(), PathComponentID)).getValue(buildingEntity);
 
     RequiredConnectedProductionComponent requiredConnectedProductionComponent = RequiredConnectedProductionComponent(
       getC(RequiredConnectedProductionComponentID)
@@ -58,14 +52,13 @@ contract UpdateConnectedRequiredProductionSystem is IOnBuildingSubsystem, Primod
     ResourceValues memory requiredProduction = requiredConnectedProductionComponent.getValue(toEntity);
     uint256 productionResourceID = buildingProductionComponent.getValue(buildingIdNewLevel).resource;
     for (uint256 i = 0; i < requiredProduction.resources.length; i++) {
-      if (requiredProduction.resources[i] == productionResourceID) {
-        if (actionType == EActionType.Build) {
-          requiredProduction.values[i]--;
-        } else if (actionType == EActionType.Destroy) {
-          requiredProduction.values[i]++;
-        }
-        break;
+      if (requiredProduction.resources[i] != productionResourceID) continue;
+      if (actionType == EActionType.Build) {
+        requiredProduction.values[i]--;
+      } else if (actionType == EActionType.Destroy) {
+        requiredProduction.values[i]++;
       }
+      break;
     }
     requiredConnectedProductionComponent.set(toEntity, requiredProduction);
   }
