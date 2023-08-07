3b17542495f91664ff144d39f30c5167fca72c8b
0xhank <zeroxhank@gmail.com>
2023-08-07 14:33:41 +0100

    feat: add script to autogenerate deploy-live

---
+++ packages/contracts/prod-json-gen.mjs
@@ -0,0 +1,23 @@
+import jsonData from "./deploy.json" assert { type: "json" };
+import fs from "fs";
+
+const isDevOrDebug = (string) => {
+  const toRemove = /debug|dev/i.test(string);
+  if (toRemove) console.log("removing ", string);
+  return !toRemove;
+};
+function removeDebugAndDev(data) {
+  data.components = data.components.filter((component) => {
+    return isDevOrDebug(component);
+  });
+  data.initializers = data.initializers.filter((initializer) => {
+    return isDevOrDebug(initializer);
+  });
+  data.systems = data.systems.filter((system) => {
+    return isDevOrDebug(system.name);
+  });
+  return data;
+}
+
+const cleanedData = removeDebugAndDev(jsonData);
+fs.writeFileSync("deploy-live.json", JSON.stringify(cleanedData, null, 2));
