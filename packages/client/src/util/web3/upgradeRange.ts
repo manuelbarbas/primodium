e3dc096bba895abb10f00e3a3c170de00254537b
0xhank <zeroxhank@gmail.com>
2023-08-15 07:52:11 -0400

    feat: add new system

---
+++ packages/client/src/util/web3/upgradeRange.ts
@@ -0,0 +1,18 @@
+import { execute } from "src/network/actions";
+import { Network } from "src/network/layer";
+import { useGameStore } from "src/store/GameStore";
+import { useNotificationStore } from "src/store/NotificationStore";
+
+export const upgradeBuilding = async (network: Network) => {
+  const { providers, systems } = network;
+  const setTransactionLoading = useGameStore.getState().setTransactionLoading;
+  const setNotification = useNotificationStore.getState().setNotification;
+  setTransactionLoading(true);
+
+  await execute(
+    systems["system.UpgradeRange"].executeTyped(),
+    providers,
+    setNotification
+  );
+  setTransactionLoading(false);
+};
