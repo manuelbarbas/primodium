e7cd8d534b0d2634959c63947d2e5615aee52743
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-08-22 01:15:27 -0700

    feat: track system.UpgradeRange

--- packages/client/src/util/web3/upgradeRange.ts
+++ packages/client/src/util/web3/upgradeRange.ts
@@ -1,7 +1,11 @@
+import { BigNumber } from "ethers";
+import { ampli } from "src/ampli";
 import { execute } from "src/network/actions";
 import { Network } from "src/network/layer";
 import { useGameStore } from "src/store/GameStore";
 import { useNotificationStore } from "src/store/NotificationStore";
+import { parseReceipt } from "../analytics/parseReceipt";
+import { ActiveAsteroid } from "src/network/components/clientComponents";
 
 export const upgradeRange = async (network: Network) => {
   const { providers, systems } = network;
@@ -9,10 +13,20 @@ export const upgradeRange = async (network: Network) => {
   const setNotification = useNotificationStore.getState().setNotification;
   setTransactionLoading(true);
 
-  await execute(
+  const activeAsteroid = ActiveAsteroid.get()?.value;
+
+  if (!activeAsteroid) return;
+
+  const receipt = await execute(
     systems["system.UpgradeRange"].executeTyped(),
     providers,
     setNotification
   );
+
+  ampli.systemUpgradeRange({
+    asteroidCoord: BigNumber.from(activeAsteroid).toString(),
+    ...parseReceipt(receipt),
+  });
+
   setTransactionLoading(false);
 };
