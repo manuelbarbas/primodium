1963af8999c22585b8f558e3ff790dbc9c5faae0
Nabs <43505437+NAB5@users.noreply.github.com>
2023-08-23 14:05:39 -0500

    feat: combine web3 function into one func

--- packages/client/src/util/web3/units.ts
+++ packages/client/src/util/web3/units.ts
@@ -6,10 +6,12 @@ import { useNotificationStore } from "src/store/NotificationStore";
 import { ActiveAsteroid } from "src/network/components/clientComponents";
 import { ArrivalUnitStruct } from "../../../../contracts/types/ethers-contracts/SendUnitsSystem";
 import { Position } from "src/network/components/chainComponents";
+import { ESendType } from "./types";
 
-export const invadeAsteroid = async (
+export const sendUnits = async (
   destinationAsteroid: EntityID,
   arrivalUnits: ArrivalUnitStruct[],
+  sendType: ESendType,
   network: Network
 ) => {
   const { providers, systems } = network;
@@ -29,44 +31,7 @@ export const invadeAsteroid = async (
   await execute(
     systems["system.SendUnits"].executeTyped(
       arrivalUnits,
-      0,
-      originAsteroid,
-      destinationAsteroid,
-      to,
-      {
-        gasLimit: 8_000_000,
-      }
-    ),
-    providers,
-    setNotification
-  );
-
-  setTransactionLoading(false);
-};
-
-export const reinforceAsteroid = async (
-  destinationAsteroid: EntityID,
-  arrivalUnits: ArrivalUnitStruct[],
-  network: Network
-) => {
-  const { providers, systems } = network;
-  const setTransactionLoading = useGameStore.getState().setTransactionLoading;
-  const setNotification = useNotificationStore.getState().setNotification;
-
-  // todo: find a cleaner way to extract this value in all web3 functions
-  const originAsteroid = ActiveAsteroid.get()?.value;
-  if (!originAsteroid) return;
-
-  const to = Position.get(destinationAsteroid)?.parent;
-
-  if (!to) return;
-
-  setTransactionLoading(true);
-
-  await execute(
-    systems["system.SendUnits"].executeTyped(
-      arrivalUnits,
-      1,
+      sendType,
       originAsteroid,
       destinationAsteroid,
       to,
