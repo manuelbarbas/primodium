919862180ac968bb14bc991af44b18de0067fafd
0xhank <zeroxhank@gmail.com>
2023-08-28 16:58:39 -0400

    add chain components and systems

---
+++ packages/client/src/util/web3/recall.ts
@@ -0,0 +1,20 @@
+import { EntityID } from "@latticexyz/recs";
+import { execute } from "src/network/actions";
+import { Network } from "src/network/layer";
+import { useGameStore } from "src/store/GameStore";
+import { useNotificationStore } from "src/store/NotificationStore";
+
+export const recall = async (rockEntity: EntityID, network: Network) => {
+  const { providers, systems } = network;
+  const setTransactionLoading = useGameStore.getState().setTransactionLoading;
+  const setNotification = useNotificationStore.getState().setNotification;
+
+  setTransactionLoading(true);
+
+  await execute(
+    systems["system.RecallReinforcements"].executeTyped(rockEntity),
+    providers,
+    setNotification
+  );
+  setTransactionLoading(false);
+};
