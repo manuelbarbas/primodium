d04bfa034496aaaad1f04242558535911a9d48a3
Nabs <43505437+NAB5@users.noreply.github.com>
2023-08-29 13:28:45 -0500

    fix: use date.now instead of querying block timestamp

--- packages/client/src/network/systems/setupBlockNumber.ts
+++ packages/client/src/network/systems/setupBlockNumber.ts
@@ -1,28 +1,20 @@
-import { WebSocketProvider } from "@ethersproject/providers";
-import { IComputedValue } from "mobx";
 import { Observable } from "rxjs";
 import { BlockNumber } from "../components/clientComponents";
 import { world } from "../world";
 
-export const setupBlockNumber = (
-  blockNumber$: Observable<number>,
-  providers: IComputedValue<{
-    json: any;
-    ws: WebSocketProvider | undefined;
-  }>
-) => {
+export const setupBlockNumber = (blockNumber$: Observable<number>) => {
   const span = 100;
   const blockTimes: number[] = [];
+  let prevBlockTime: number | undefined = undefined;
 
   const blockListener = blockNumber$.subscribe(async (blockNumber) => {
-    const currentBlockTime = (await providers.get().ws?.getBlock(blockNumber))
-      ?.timestamp;
-    const prevBlockTime = (await providers.get().ws?.getBlock(blockNumber - 1))
-      ?.timestamp;
+    if (!prevBlockTime) {
+      prevBlockTime = Date.now();
+      return;
+    }
 
-    if (!currentBlockTime || !prevBlockTime) return;
-
-    const timeDifference = currentBlockTime - prevBlockTime;
+    const timeDifference = (Date.now() - prevBlockTime) / 1000;
+    prevBlockTime = Date.now();
 
     blockTimes.push(timeDifference); // Add the new block time to the queue
 
@@ -33,7 +25,7 @@ export const setupBlockNumber = (
 
     // Compute the average over the window (i.e., over the items in the queue)
     const total = blockTimes.reduce((sum, time) => sum + time, 0);
-    const avgBlockTime = total / blockTimes.length;
+    const avgBlockTime = Math.round(total / blockTimes.length);
 
     BlockNumber.set({ value: blockNumber, avgBlockTime: avgBlockTime });
   });
