9519e566b6f01674fe5dede768d9d724ed1292f4
Nabs <43505437+NAB5@users.noreply.github.com>
2023-08-30 00:01:34 -0500

    fix: fix utility resource scaling

--- packages/client/src/components/asteroid-ui/user-panel/panes/utilities/UtilityResourceLabel.tsx
+++ packages/client/src/components/asteroid-ui/user-panel/panes/utilities/UtilityResourceLabel.tsx
@@ -9,7 +9,7 @@ import {
 } from "src/network/components/chainComponents";
 import { BlockNumber } from "src/network/components/clientComponents";
 import { formatNumber } from "src/util/common";
-import { RESOURCE_SCALE, ResourceImage } from "src/util/constants";
+import { ResourceImage } from "src/util/constants";
 
 export const UtilityResourceLabel = ({
   name,
@@ -64,10 +64,7 @@ export const UtilityResourceLabel = ({
           <div className="flex flex-col gap-1">
             <div className="flex justify-between border rounded-md border-cyan-800">
               <p className="px-1 bg-cyan-700 rounded-md rounded-r-none">
-                {formatNumber(
-                  (maxStorage - resourceCount + resourcesToClaim) *
-                    RESOURCE_SCALE
-                )}
+                {formatNumber(maxStorage - resourceCount + resourcesToClaim)}
               </p>
               <b className="rounded-md rounded-l-none bg-slate-700 px-1">
                 REMAINING
