af98363bec0260cc04b0b9c8378c8fe5c5850adc
0xhank <zeroxhank@gmail.com>
2023-08-30 18:45:12 -0400

    add output and cost

--- packages/client/src/components/asteroid-ui/tile-info/BlueprintInfo.tsx
+++ packages/client/src/components/asteroid-ui/tile-info/BlueprintInfo.tsx
@@ -12,7 +12,11 @@ import ResourceIconTooltip from "src/components/shared/ResourceIconTooltip";
 import { hashAndTrimKeyEntity, hashKeyEntity } from "src/util/encode";
 import { formatNumber } from "src/util/common";
 import { Account, BlockNumber } from "src/network/components/clientComponents";
-import { Level, P_Production } from "src/network/components/chainComponents";
+import {
+  Level,
+  P_Production,
+  P_ProductionDependencies,
+} from "src/network/components/chainComponents";
 
 export const BlueprintInfo: React.FC<{
   buildingType: EntityID;
@@ -25,18 +29,29 @@ export const BlueprintInfo: React.FC<{
   const recipe = useMemo(() => {
     return getRecipe(buildingLevelEntity);
   }, [buildingType, level]);
-  const production = P_Production.use(buildingLevelEntity);
   const { avgBlockTime } = BlockNumber.use(undefined, {
     value: 0,
     avgBlockTime: 1,
   });
+
+  const prodDependencies = P_ProductionDependencies.use(buildingLevelEntity);
+  const dependencies =
+    prodDependencies?.resources.map((resource, index) => ({
+      resource,
+      value:
+        (prodDependencies.values[index] * RESOURCE_SCALE * 60) / avgBlockTime,
+    })) ?? [];
+  const production = P_Production.use(buildingLevelEntity);
   const productionRate =
     ((production?.resourceProductionRate ?? 0) * RESOURCE_SCALE * 60) /
     avgBlockTime;
+
+  console.log("production", production);
+  console.log("dependencies", dependencies);
   return (
     <div className="flex flex-col w-fit">
       <div className="flex flex-col justify-center items-center w-full border border-yellow-400 border-dashed ring ring-yellow-700/20 rounded-md bg-slate-900 p-2">
-        <div className="relative flex items-center gap-2">
+        <div className="relative flex gap-2">
           <div
             className={`relative flex flex-col text-sm items-center cursor-pointer w-16 h-12 border rounded border-cyan-400`}
           >
@@ -55,32 +70,49 @@ export const BlueprintInfo: React.FC<{
                 .replace(/([A-Z]+)/g, " $1")
                 .replace(/([A-Z][a-z])/g, " $1")}
             </p>
-            <div className="relative gap-2 px-2 flex gap-2">
-              {production && (
-                <div className="flex items-center gap-2 text-xs bg-green-800/60 p-1 border border-green-600 rounded-md">
-                  <img
-                    className="inline-block h-4"
-                    src={ResourceImage.get(production.resourceID)}
-                  ></img>
-                  {formatNumber(productionRate)}/MIN
+            <div className="flex gap-2">
+              {recipe.length > 0 && (
+                <div className="flex flex-col gap-1 w-18 text-xs">
+                  COST
+                  <div className="flex justify-center items-center text-sm bg-red-800/60 p-1 border border-red-600 rounded-md gap-2">
+                    {recipe.map((resource) => {
+                      const resourceImage = ResourceImage.get(resource.id)!;
+                      const resourceName = BlockIdToKey[resource.id];
+                      return (
+                        <ResourceIconTooltip
+                          key={resource.id}
+                          image={resourceImage}
+                          resourceId={resource.id}
+                          name={resourceName}
+                          amount={resource.amount}
+                          fontSize={"xs"}
+                        />
+                      );
+                    })}
+                  </div>
                 </div>
               )}
-              {recipe.length > 0 && (
-                <div className="flex justify-center items-center text-sm bg-red-800/60 p-1 border border-red-600 rounded-md gap-2">
-                  {recipe.map((resource) => {
-                    const resourceImage = ResourceImage.get(resource.id)!;
-                    const resourceName = BlockIdToKey[resource.id];
-                    return (
-                      <ResourceIconTooltip
-                        key={resource.id}
-                        image={resourceImage}
-                        resourceId={resource.id}
-                        name={resourceName}
-                        amount={resource.amount}
-                        fontSize={"xs"}
-                      />
-                    );
-                  })}
+              {(!!production || dependencies) && (
+                <div className="relative gap-1 flex flex-col w-18 gap-1 text-xs">
+                  OUTPUT
+                  {production && (
+                    <div className="flex items-center gap-2 text-xs bg-green-800/60 p-1 border border-green-600 rounded-md">
+                      <img
+                        className="inline-block h-4"
+                        src={ResourceImage.get(production.resourceID)}
+                      ></img>
+                      {formatNumber(productionRate)}/MIN
+                    </div>
+                  )}
+                  {dependencies.map((dep) => (
+                    <div className="flex items-center gap-2 text-xs bg-red-800/60 p-1 border border-red-600 rounded-md">
+                      <img
+                        className="inline-block h-4"
+                        src={ResourceImage.get(dep.resource)}
+                      ></img>
+                      {formatNumber(dep.value)}/MIN
+                    </div>
+                  ))}
                 </div>
               )}
             </div>
