0d27a8426bfadb6c5b6fed48ffc2cb680794e4bb
a-najafi <najafi73alireza@gmail.com>
2023-08-31 19:13:16 +0300

    Revert "wip: motherlode resource production"
    This reverts commit 4fdd2c4744258d88b8deffdc5c889dc9e7987d39.

--- packages/client/src/components/asteroid-ui/user-panel/panes/inventory/ResourceLabel.tsx
+++ packages/client/src/components/asteroid-ui/user-panel/panes/inventory/ResourceLabel.tsx
@@ -1,23 +1,16 @@
-import { useEntityQuery } from "@latticexyz/react";
-import { EntityID, EntityIndex, Has, HasValue } from "@latticexyz/recs";
+import { EntityID, EntityIndex } from "@latticexyz/recs";
 import { useMemo } from "react";
 import useResourceCount from "src/hooks/useResourceCount";
 import {
-  AsteroidType,
   Item,
   LastClaimedAt,
-  OwnedBy,
   P_MaxStorage,
-  P_MotherlodeResource,
   Production,
 } from "src/network/components/chainComponents";
-
-import { BlockNumber, Hangar } from "src/network/components/clientComponents";
+import { BlockNumber } from "src/network/components/clientComponents";
 import { formatNumber } from "src/util/common";
-import { BlockType, RESOURCE_SCALE, ResourceImage } from "src/util/constants";
-import { ESpaceRockType } from "src/util/web3/types";
-import { world } from "src/network/world";
-import { getUnitStats } from "src/util/trainUnits";
+import { RESOURCE_SCALE, ResourceImage } from "src/util/constants";
+
 export const ResourceLabel = ({
   name,
   resourceId,
@@ -31,36 +24,6 @@ export const ResourceLabel = ({
     value: 0,
     avgBlockTime: 1,
   });
-
-  const player = world.entities[entityIndex ?? 0];
-  const query = [
-    Has(AsteroidType),
-    HasValue(OwnedBy, { value: player }),
-    HasValue(AsteroidType, { value: ESpaceRockType.Motherlode }),
-    HasValue(P_MotherlodeResource, { resource: resourceId }),
-  ];
-  const motherlodes = useEntityQuery(query);
-
-  const motherlodeProduction = useMemo(() => {
-    if (
-      resourceId === BlockType.Titanium ||
-      resourceId === BlockType.Iridium ||
-      resourceId === BlockType.Platinum ||
-      resourceId === BlockType.Kimberlite
-    ) {
-      let mProduction = 0;
-      motherlodes.forEach((motherlode) => {
-        const hangar = Hangar.get(world.entities[motherlode]);
-        if (hangar)
-          for (let i = 0; i < hangar.units.length; i++) {
-            mProduction += getUnitStats(hangar.units[i]).MIN * hangar.counts[i];
-          }
-      });
-      return mProduction;
-    } else {
-      return 0;
-    }
-  }, [motherlodes]);
 
   const resourceCount = useResourceCount(Item, resourceId, entityIndex);
 
@@ -92,10 +55,9 @@ export const ResourceLabel = ({
             <p>{name}</p>
           </div>
           <p className="text-slate-400">
-            {production + motherlodeProduction
+            {production
               ? `+ ${formatNumber(
-                  ((production + motherlodeProduction) * RESOURCE_SCALE * 60) /
-                    avgBlockTime
+                  (production * RESOURCE_SCALE * 60) / avgBlockTime
                 )}/MIN`
               : "-"}
           </p>
