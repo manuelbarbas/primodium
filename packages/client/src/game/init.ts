760d5bc34ac7fef9f07058f472c439357ab1eaf5
Nabs <43505437+NAB5@users.noreply.github.com>
2023-09-07 08:23:24 -0700

    refactor(game): Remove unnecessary player parameter from init functions and adjust game scaling

--- packages/client/src/game/init.ts
+++ packages/client/src/game/init.ts
@@ -1,12 +1,11 @@
-import { EntityID } from "@latticexyz/recs";
 import { Network } from "src/network/layer";
 
 import { initAsteroidView } from "./lib/asteroid/init";
 import { initBeltView } from "./lib/belt/init";
 
-async function init(player: EntityID, network: Network) {
+async function init(network: Network) {
   await initAsteroidView(network);
-  await initBeltView(player, network);
+  await initBeltView(network);
 }
 
 export default init;
--- packages/client/src/game/lib/belt/systems/index.ts
+++ packages/client/src/game/lib/belt/systems/index.ts
@@ -4,10 +4,12 @@ import { focusAsteroid } from "./focusAsteroid";
 import { renderMotherlode } from "./renderMotherlode";
 import { renderUnitSend } from "./renderUnitSend";
 import { renderArrivalsInTransit } from "./renderArrivalsInTransit";
-import { EntityID } from "@latticexyz/recs";
 import { renderArrivalsInOrbit } from "./renderArrivalsInOrbit";
+import { Account } from "src/network/components/clientComponents";
 
-export const runSystems = (scene: Scene, player: EntityID) => {
+export const runSystems = (scene: Scene) => {
+  const player = Account.get()?.value!;
+
   focusAsteroid(scene);
 
   renderUnitSend(scene);
