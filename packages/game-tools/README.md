72dc22239db4091048bfa53a3ecf9c23fe55f103
0xhank <zeroxhank@gmail.com>
2023-09-08 13:27:54 -0700

    feat: remove local game tools

--- packages/game-tools/README.md
+++
@@ -1,5 +0,0 @@
-# Game Tools
-
-Unity style Game Tools sidebar for debugging / development
-
-Forked from @latticexyz's ecs-browser repo
--- packages/game-tools/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { Browser } from "./Browser";
-export { CheatcodesList } from "./CheatcodesList";
-export { Editor } from "./Editor";
-export * from "./types";
