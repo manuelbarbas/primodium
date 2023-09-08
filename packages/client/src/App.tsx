3a702a18ee4322d3775d559458a3bb634557d972
0xhank <zeroxhank@gmail.com>
2023-09-07 20:49:09 -0700

    feat: add mud dev tools

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 
+import mudConfig from "contracts/mud.config";
 import AppLoadingState from "./AppLoadingState";
 import { ampli } from "./ampli";
 import { MudProvider } from "./hooks/providers/MudProvider";
 import { setup } from "./network/setup";
 import { SetupResult } from "./network/types";
+import { world } from "./network/world";
 
 const DEV = import.meta.env.PRI_DEV === "true";
 
@@ -25,6 +27,25 @@ export default function App() {
   } else {
     ampli.load({ client: { apiKey: import.meta.env.PRI_AMPLI_API_KEY_PROD } });
   }
+  useEffect(() => {
+    if (!networkLayer) return;
+    // https://vitejs.dev/guide/env-and-mode.html
+    if (import.meta.env.DEV) {
+      import("@latticexyz/dev-tools").then(({ mount: mountDevTools }) =>
+        mountDevTools({
+          config: mudConfig,
+          publicClient: networkLayer.network.publicClient,
+          walletClient: networkLayer.network.walletClient,
+          latestBlock$: networkLayer.network.latestBlock$,
+          blockStorageOperations$: networkLayer.network.blockStorageOperations$,
+          worldAddress: networkLayer.network.worldContract.address,
+          worldAbi: networkLayer.network.worldContract.abi,
+          write$: networkLayer.network.write$,
+          recsWorld: world,
+        })
+      );
+    }
+  }, [networkLayer]);
 
   if (networkLayer === undefined) {
     return (
