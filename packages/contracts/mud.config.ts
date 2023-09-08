0743c95aa8cd39f0b7eb1f138f977247b7381324
0xhank <zeroxhank@gmail.com>
2023-09-08 15:51:32 -0700

    feat: add spawn tables

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -11,10 +11,60 @@ if (typeof process != undefined && typeof process != "undefined") {
 
 export default mudConfig({
   excludeSystems: [...dev],
+
+  enums: {
+    Rock: ["Null", "Asteroid", "Motherlode"],
+  },
   tables: {
+    /* -------------------------------------------------------------------------- */
+    /*                                     Dev                                    */
+    /* -------------------------------------------------------------------------- */
     Counter: {
       keySchema: {},
       schema: "uint32",
     },
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   Common                                   */
+    /* -------------------------------------------------------------------------- */
+    GameConfig: {
+      keySchema: {},
+      schema: {
+        spawnSpace: "uint32",
+      },
+    },
+
+    Position: {
+      keySchema: { entity: "bytes32" },
+      schema: {
+        x: "int32",
+        y: "int32",
+        parent: "bytes32",
+      },
+    },
+
+    ReversePosition: {
+      keySchema: { x: "int32", y: "int32" },
+      schema: {
+        entity: "bytes32",
+      },
+    },
+
+    /* -------------------------------------------------------------------------- */
+    /*                                    Rocks                                   */
+    /* -------------------------------------------------------------------------- */
+    AsteroidCount: {
+      keySchema: {},
+      schema: "uint32",
+    },
+
+    RockType: {
+      keySchema: { entity: "bytes32" },
+      schema: "uint8",
+    },
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  Buildings                                 */
+    /* -------------------------------------------------------------------------- */
   },
 });
