a04686cb9be0877622174517e9c63064d15a38ad
0xhank <zeroxhank@gmail.com>
2023-09-12 11:19:32 -0700

    chore: move prototypes to config folder

--- packages/contracts/ts/prototypes/prototypeConfig.ts
+++ packages/contracts/ts/prototypes/prototypeConfig.ts
@@ -1,26 +1,15 @@
 import { StaticAbiType } from "@latticexyz/schema-type/deprecated";
 import { ConfigFieldTypeToPrimitiveType, StoreConfig } from "@latticexyz/store";
+type Tables<C extends StoreConfig> = {
+  [Table in keyof C["tables"]]?: {
+    [Field in keyof C["tables"][Table]["schema"]]: ConfigFieldTypeToPrimitiveType<C["tables"][Table]["schema"][Field]>;
+  };
+};
 
 export type PrototypeConfig<C extends StoreConfig> = {
   keys?: Record<string, StaticAbiType>;
-} & {
-  tables: {
-    [Table in keyof C["tables"]]?: {
-      [Field in keyof C["tables"][Table]["schema"]]: ConfigFieldTypeToPrimitiveType<
-        C["tables"][Table]["schema"][Field]
-      >;
-    };
-  };
-  levels?: {
-    [Table in keyof C["tables"]]?: Record<
-      number,
-      {
-        [Field in keyof C["tables"][Table]["schema"]]: ConfigFieldTypeToPrimitiveType<
-          C["tables"][Table]["schema"][Field]
-        >;
-      }
-    >;
-  };
+  tables: Tables<C>;
+  levels?: Record<number, Tables<C>>;
 };
 
 export type PrototypesConfig<C extends StoreConfig> = Record<string, PrototypeConfig<C>>;
