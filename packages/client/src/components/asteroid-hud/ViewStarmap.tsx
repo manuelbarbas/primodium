70faedfb18f1caafc88bb7a4368ed84f19c4e1ae
Nabs <43505437+NAB5@users.noreply.github.com>
2023-09-22 21:07:50 -0500

    feat(client): Update icons and adjust UI elements in asteroid-hud component

--- packages/client/src/components/asteroid-hud/ViewStarmap.tsx
+++ packages/client/src/components/asteroid-hud/ViewStarmap.tsx
@@ -3,15 +3,13 @@ import { Button } from "../core/Button";
 export const ViewStarmap = () => {
   return (
     <Button
-      className="w-full flex gap-2 btn-warning bg-gradient-to-br from-rose-700 to-pink-600 border-2 ring-2 ring-error/30 border-rose-900 drop-shadow-2xl text-base-content pixel-images"
+      className="w-full flex gap-2 btn-warning bg-gradient-to-br from-rose-700 to-pink-600 border-2 ring-2 ring-error/30 border-rose-900 drop-shadow-2xl text-base-content pixel-images overflow-y-hidden group outline-none"
       onClick={() => {
         return;
       }}
     >
-      <img
-        src="img/icons/attackaircraft.png"
-        className="pixel-images w-8 h-8"
-      />
+      <span className="absolute bg-orange-400/50 -right-96 -bottom-0 group-hover:-right-16 group-hover:bottom-0 h-32 w-32 rounded-full mix-blend-overlay transition-all duration-200" />
+      <img src="img/icons/starmapicon.png" className="pixel-images w-8 h-8" />
       <span className="flex font-bold gap-1">OPEN STAR MAP</span>
     </Button>
   );
