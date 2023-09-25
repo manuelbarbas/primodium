990989849ce2d553ee3e6f1e292fd11afe3bb828
a-najafi <najafi73alireza@gmail.com>
2023-09-25 22:56:31 +0330

    fix: added BattleSpaceRockComponent to battle tests to account for Defensive Buildings

--- packages/contracts/src/test/systems/BattleSystem.t.sol
+++ packages/contracts/src/test/systems/BattleSystem.t.sol
@@ -9,6 +9,7 @@ import { S_ResolveBattleSystem, ID as S_ResolveBattleSystemID } from "../../syst
 import { ComponentDevSystem, ID as ComponentDevSystemID } from "../../systems/ComponentDevSystem.sol";
 import { BuildSystem as BuildSystem } from "../../systems/BuildSystem.sol";
 
+import { BattleSpaceRockComponent, ID as BattleSpaceRockComponentID } from "../../components/BattleSpaceRockComponent.sol";
 import { BattleParticipant } from "../../types.sol";
 import { ItemComponent, ID as ItemComponentID } from "../../components/ItemComponent.sol";
 import { LevelComponent, ID as LevelComponentID } from "../../components/LevelComponent.sol";
@@ -96,6 +97,7 @@ contract S_ResolveBattleSystemTest is PrimodiumTest {
       battleEntity,
       abi.encode(defender.participantEntity, defender.unitTypes, defender.unitLevels, defender.unitCounts)
     );
+    componentDevSystem.executeTyped(BattleSpaceRockComponentID, battleEntity, abi.encode(getHomeAsteroidEntity(bob)));
 
     battleSystem.executeTyped(alice, battleEntity);
     assertTrue(battleResultComponent.has(battleEntity), "battle result not found");
@@ -150,7 +152,7 @@ contract S_ResolveBattleSystemTest is PrimodiumTest {
       battleEntity,
       abi.encode(defender.participantEntity, defender.unitTypes, defender.unitLevels, defender.unitCounts)
     );
-
+    componentDevSystem.executeTyped(BattleSpaceRockComponentID, battleEntity, abi.encode(getHomeAsteroidEntity(bob)));
     battleSystem.executeTyped(alice, battleEntity);
     assertTrue(battleResultComponent.has(battleEntity), "battle result not found");
     BattleResult memory result = battleResultComponent.getValue(battleEntity);
@@ -213,7 +215,7 @@ contract S_ResolveBattleSystemTest is PrimodiumTest {
       battleEntity,
       abi.encode(defender.participantEntity, defender.unitTypes, defender.unitLevels, defender.unitCounts)
     );
-
+    componentDevSystem.executeTyped(BattleSpaceRockComponentID, battleEntity, abi.encode(getHomeAsteroidEntity(bob)));
     battleSystem.executeTyped(alice, battleEntity);
     assertTrue(battleResultComponent.has(battleEntity), "battle result not found");
 
