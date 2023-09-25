c33f1ebb72638057a3130029eee8f93311ca28af
Nabs <43505437+NAB5@users.noreply.github.com>
2023-09-25 11:40:45 -0500

    fix: fix vessel count calculation

--- packages/client/src/components/hud/building-menu/widgets/UpgradeMiningVessel.tsx
+++ packages/client/src/components/hud/building-menu/widgets/UpgradeMiningVessel.tsx
@@ -36,6 +36,7 @@ export const UpgradeMiningVessel: React.FC = () => {
   );
 
   const hasEnough = useHasEnoughResources(recipe);
+
   const canUpgrade =
     hasEnough && mainBaseLevel >= mainBaseLvlReq && level < maxLevel;
 
