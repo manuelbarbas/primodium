f1ea5beca00763669e3a18c306b9dbbc6f903187
0xhank <zeroxhank@gmail.com>
2023-09-26 14:24:04 -0400

    feat: S_battleSystem

---
+++ packages/contracts/src/systems/subsystems/S_BattleSystem.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { PrimodiumSystem } from "systems/internal/PrimodiumSystem.sol";
+
+import { LibBattle } from "libraries/LibBattle.sol";
+import { ESendType } from "src/Types.sol";
+import { BattleResultData } from "codegen/Tables.sol";
+
+contract S_BattleSystem is PrimodiumSystem {
+  function battle(
+    bytes32 attackerEntity,
+    bytes32 defenderEntity,
+    bytes32 rockEntity,
+    ESendType sendType
+  ) public returns (BattleResultData memory) {
+    return LibBattle.battle(attackerEntity, defenderEntity, rockEntity, sendType);
+  }
+}
