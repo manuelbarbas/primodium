3738ab6c5af717e97397088b0c62ec632f41ae30
0xhank <zeroxhank@gmail.com>
2023-10-03 11:17:30 +0100

    feat: more updates

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
           publicClient: networkLayer.network.publicClient,
           walletClient: networkLayer.network.walletClient,
           latestBlock$: networkLayer.network.latestBlock$,
-          blockStorageOperations$: networkLayer.network.blockStorageOperations$,
+          storedBlockLogs$: networkLayer.network.storedBlockLogs$,
           worldAddress: networkLayer.network.worldContract.address,
           worldAbi: networkLayer.network.worldContract.abi,
           write$: networkLayer.network.write$,
