30b86764c4abe520fd405dda7991b8507ddfdab7
a-najafi <najafi73alireza@gmail.com>
2023-10-20 01:48:11 +0300

    fix: removed extra imports in LibReward

--- packages/contracts/src/libraries/LibReward.sol
+++ packages/contracts/src/libraries/LibReward.sol
@@ -1,25 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-import { addressToEntity, entityToAddress, getSystemResourceId, bytes32ToString } from "src/utils.sol";
-import { SystemCall } from "@latticexyz/world/src/SystemCall.sol";
 // tables
-import { P_IsUtility, MaxResourceCount, ResourceCount, P_ResourceReward, P_ResourceRewardData, P_UnitReward, P_UnitRewardData, P_RequiredObjectives, CompletedObjective, P_EnumToPrototype, P_MaxLevel, Home, P_RequiredTile, P_RequiredBaseLevel, P_Terrain, P_AsteroidData, P_Asteroid, Spawned, DimensionsData, Dimensions, PositionData, Level, BuildingType, Position, LastClaimedAt, Children, OwnedBy, P_Blueprint, Children } from "codegen/index.sol";
+import { P_IsUtility, MaxResourceCount, ResourceCount, P_ResourceReward, P_ResourceRewardData, P_UnitReward, P_UnitRewardData, Home, Spawned, PositionData, Position } from "codegen/index.sol";
 
 // libraries
-import { LibEncode } from "libraries/LibEncode.sol";
-import { LibReduceProductionRate } from "libraries/LibReduceProductionRate.sol";
 import { LibProduction } from "libraries/LibProduction.sol";
 import { LibStorage } from "libraries/LibStorage.sol";
 import { LibUnit } from "libraries/LibUnit.sol";
-import { UnitFactorySet } from "libraries/UnitFactorySet.sol";
-import { LibBuilding } from "libraries/LibBuilding.sol";
 
 // types
-import { UnitKey, BuildingKey, BuildingTileKey, ExpansionKey } from "src/Keys.sol";
-import { Bounds, EBuilding, EResource } from "src/Types.sol";
-
-import { MainBasePrototypeId } from "codegen/Prototypes.sol";
+import { EResource } from "src/Types.sol";
 
 library LibReward {
   function receiveRewards(bytes32 playerEntity, bytes32 prototype) internal {
