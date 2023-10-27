e381ec758f0766772e0e370d71632dca5508d550
Nabs <43505437+NAB5@users.noreply.github.com>
2023-10-27 13:53:10 -0500

    feat: add leave alliance system

--- packages/client/src/util/web3/contractCalls/alliance.ts
+++ packages/client/src/util/web3/contractCalls/alliance.ts
@@ -3,8 +3,13 @@ import { toHex32 } from "src/util/constants";
 
 export const createAlliance = async (name: string, inviteOnly: boolean, network: SetupNetworkResult) => {
   const tx = await network.worldContract.write.create([
-    toHex32(name.substring(0, 5).toUpperCase()),
+    toHex32(name.substring(0, 6).toUpperCase()),
     Number(inviteOnly),
   ]);
+  await network.waitForTransaction(tx);
+};
+
+export const leaveAlliance = async (network: SetupNetworkResult) => {
+  const tx = await network.worldContract.write.leave();
   await network.waitForTransaction(tx);
 };
