821bb49c23dc0f0503fe3c2bf1c7d5173ad8c28c
0xhank <zeroxhank@gmail.com>
2023-11-01 11:31:29 -0400

    chore: document system hack

--- packages/client/src/network/systems/setupSend.ts
+++ packages/client/src/network/systems/setupSend.ts
@@ -9,6 +9,8 @@ export const setupSend = (player: Entity) => {
     // temp so we can test without the set origin ui
     components.Send.setOrigin(asteroid);
   });
+
+  // this is a hack because the component.use hook can't pick up deleteRecord events
   defineComponentSystem(world, components.GracePeriod, ({ entity, value }) => {
     if (!value[0])
       components.GracePeriod.set({ value: 0n, __dynamicData: "", __encodedLengths: "", __staticData: "" }, entity);
