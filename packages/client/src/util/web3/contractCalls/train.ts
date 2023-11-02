f9c0df7f2a3d610bad6d5e66cb85a07e9346cfe4
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-11-01 21:33:04 -0700

    fix: replace generated uuid with randomBytes(32) for BigNumber compatibility

--- packages/client/src/util/web3/contractCalls/train.ts
+++ packages/client/src/util/web3/contractCalls/train.ts
@@ -3,11 +3,11 @@ import { EUnit } from "contracts/config/enums";
 import { execute } from "src/network/actions";
 import { SetupNetworkResult } from "src/network/types";
 import { hashEntities } from "src/util/encode";
-import { uuid } from "@latticexyz/utils";
 import { Hex } from "viem";
+import { hexlify, randomBytes } from "ethers/lib/utils";
 
 export const train = async (buildingEntity: Entity, unit: EUnit, count: bigint, network: SetupNetworkResult) => {
   await execute(() => network.worldContract.write.trainUnits([buildingEntity as Hex, unit, count]), network, {
-    id: hashEntities(uuid()),
+    id: hashEntities(hexlify(randomBytes(32))),
   });
 };
