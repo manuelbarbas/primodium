8dd3da4bdb7e32da66ad2be57d88b81aed5e20bc
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-11-02 09:36:49 -0700

    refactor: new randomEntity function for generating new entities

--- packages/client/src/util/web3/contractCalls/train.ts
+++ packages/client/src/util/web3/contractCalls/train.ts
@@ -4,10 +4,10 @@ import { execute } from "src/network/actions";
 import { SetupNetworkResult } from "src/network/types";
 import { hashEntities } from "src/util/encode";
 import { Hex } from "viem";
-import { hexlify, randomBytes } from "ethers/lib/utils";
+import { randomEntity } from "src/util/common";
 
 export const train = async (buildingEntity: Entity, unit: EUnit, count: bigint, network: SetupNetworkResult) => {
   await execute(() => network.worldContract.write.trainUnits([buildingEntity as Hex, unit, count]), network, {
-    id: hashEntities(hexlify(randomBytes(32))),
+    id: hashEntities(randomEntity()),
   });
 };
