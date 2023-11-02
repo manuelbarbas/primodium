727e3db17bff89759a7593e42a94cf1330a0b94d
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-11-01 23:30:32 -0700

    feat: ampli.systemTrainUnits

--- packages/client/src/util/web3/contractCalls/train.ts
+++ packages/client/src/util/web3/contractCalls/train.ts
@@ -1,10 +1,13 @@
 import { Entity } from "@latticexyz/recs";
-import { EUnit } from "contracts/config/enums";
+import { EBuilding, EUnit, MUDEnums } from "contracts/config/enums";
 import { execute } from "src/network/actions";
 import { SetupNetworkResult } from "src/network/types";
 import { hashEntities } from "src/util/encode";
 import { uuid } from "@latticexyz/utils";
 import { Hex } from "viem";
+import { ampli } from "src/ampli";
+import { components } from "src/network/components";
+import { parseReceipt } from "../../analytics/parseReceipt";
 
 export const train = async (buildingEntity: Entity, unit: EUnit, count: bigint, network: SetupNetworkResult) => {
   await execute(
@@ -14,7 +17,14 @@ export const train = async (buildingEntity: Entity, unit: EUnit, count: bigint, 
       id: hashEntities(uuid()),
     },
     (receipt) => {
-      // handle amplitude here
+      const buildingType = components.BuildingType.get(buildingEntity)?.value as unknown as EBuilding;
+
+      ampli.systemTrainUnits({
+        buildingName: MUDEnums.EBuilding[buildingType],
+        unitName: MUDEnums.EUnit[unit],
+        unitCount: bigintToNumber(count),
+        ...parseReceipt(receipt),
+      });
     }
   );
 };
