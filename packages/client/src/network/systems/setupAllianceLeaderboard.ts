1f9b71df14b8636cc8c40f124a33244c0cd997c8
Nabs <43505437+NAB5@users.noreply.github.com>
2023-11-03 10:06:48 -0500

    fix: update leaderboard on alliance leave

--- packages/client/src/network/systems/setupAllianceLeaderboard.ts
+++ packages/client/src/network/systems/setupAllianceLeaderboard.ts
@@ -7,21 +7,30 @@ import { singletonEntity } from "@latticexyz/store-sync/recs";
 export const setupAllianceLeaderboard = (mud: SetupResult) => {
   const leaderboardMap = new Map<Entity, bigint>();
 
-  defineComponentSystem(world, components.Alliance, ({ entity }) => {
-    const player = mud.network.playerEntity;
-    const playerAlliance = components.PlayerAlliance.get(player)?.alliance as Entity | undefined;
+  defineComponentSystem(world, components.Alliance, (update) => {
+    const { entity } = update;
 
-    const score = components.Alliance.get(entity)?.score ?? 0n;
-
-    leaderboardMap.set(entity, score);
+    if (!update.value[0]) {
+      leaderboardMap.delete(entity);
+    } else {
+      const score = components.Alliance.get(entity)?.score ?? 0n;
+      leaderboardMap.set(entity, score);
+    }
 
     const leaderboardArray = [...leaderboardMap.entries()].sort((a, b) => Number(b[1]) - Number(a[1]));
-
     const scores = leaderboardArray.map((entry) => entry[1]);
     const alliances = leaderboardArray.map((entry) => entry[0]);
 
+    const player = mud.network.playerEntity;
+    const playerAlliance = components.PlayerAlliance.get(player)?.alliance as Entity | undefined;
+
     const playerAllianceIndex = alliances.indexOf(playerAlliance ?? singletonEntity);
     const playerAllianceRank = playerAllianceIndex !== -1 ? playerAllianceIndex + 1 : leaderboardArray.length + 1;
+
+    if (alliances.length === 0) {
+      components.AllianceLeaderboard.remove();
+      return;
+    }
 
     components.AllianceLeaderboard.set({
       scores,
