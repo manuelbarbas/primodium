f52e0d721b7cfa4172a4950751a8ee285c08d9fb
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-11-03 15:49:55 -0700

    refactor: check for valid player on leaderboard in setupLeaderboard

--- packages/client/src/network/systems/setupLeaderboard.ts
+++ packages/client/src/network/systems/setupLeaderboard.ts
@@ -3,6 +3,7 @@ import { world } from "src/network/world";
 import { Leaderboard } from "../components/clientComponents";
 import { SetupResult } from "../types";
 import { components } from "../components";
+import { entityToAddress } from "src/util/common";
 
 export const setupLeaderboard = (mud: SetupResult) => {
   const leaderboardMap = new Map<Entity, number>();
@@ -18,8 +19,19 @@ export const setupLeaderboard = (mud: SetupResult) => {
 
     const leaderboardArray = [...leaderboardMap.entries()].sort((a, b) => b[1] - a[1]);
 
-    const scores = leaderboardArray.map((entry) => entry[1]);
-    const players = leaderboardArray.map((entry) => entry[0]);
+    // TODO: more reliable way of checking whether "player" is a valid player entity that can result in an address
+    // currently filters on players (entry[0]) whether getAddress returns an error or not
+    const filteredLeaderboardArray = leaderboardArray.filter((entry) => {
+      try {
+        entityToAddress(entry[0]);
+        return true;
+      } catch (error) {
+        return false;
+      }
+    });
+
+    const players = filteredLeaderboardArray.map((entry) => entry[0]);
+    const scores = filteredLeaderboardArray.map((entry) => entry[1]);
 
     const playerIndex = players.indexOf(player);
     const playerRank = playerIndex !== -1 ? playerIndex + 1 : leaderboardArray.length + 1;
