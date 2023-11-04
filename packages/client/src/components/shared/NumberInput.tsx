4edbdb6db76b20706716ab399a26e772d5d077c5
0xhank <zeroxhank@gmail.com>
2023-11-04 13:41:50 -0400

    fix: make number input clearable

--- packages/client/src/components/shared/NumberInput.tsx
+++ packages/client/src/components/shared/NumberInput.tsx
@@ -5,22 +5,31 @@ export const NumberInput: React.FC<{
   max: number;
   onChange: (val: number) => void;
 }> = ({ min = 0, max, onChange }) => {
-  const [count, setCount] = useState(min);
+  const [count, setCount] = useState<number | "">(min);
 
   const handleUpdate = (newCount: number) => {
-    if (isNaN(newCount)) newCount = min;
-    else if (newCount > max) newCount = max;
+    if (isNaN(newCount) || newCount == 0) {
+      newCount = min;
+      setCount("");
+      onChange(newCount);
+      return;
+    }
+
+    if (newCount > max) newCount = max;
     else if (newCount < min) newCount = min;
     setCount(newCount);
     onChange(newCount);
   };
-
+  const removeLeadingZeroes = (str: string) => {
+    str = `${str}`.toString();
+    return parseInt(str, 10).toString();
+  };
   return (
     <div className={`flex gap-2 mt-4 mb-2 `}>
       <button
         className={`${count == min ? "opacity-50" : ""}`}
         disabled={count == min}
-        onClick={() => handleUpdate(Math.max(min, count - 1))}
+        onClick={() => handleUpdate(Math.max(min, count == "" ? 0 : count - 1))}
       >
         -
       </button>
@@ -31,8 +40,10 @@ export const NumberInput: React.FC<{
         placeholder="0"
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           e.preventDefault();
-          const value = Number(e.target.value);
-          handleUpdate(value);
+          const value = removeLeadingZeroes(e.target.value);
+
+          console.log("value:", value);
+          handleUpdate(Number(value));
         }}
         min={0}
         max={max}
@@ -40,7 +51,7 @@ export const NumberInput: React.FC<{
       <button
         className={`${count == max ? "opacity-50" : ""}`}
         disabled={count == max}
-        onClick={() => handleUpdate(Math.min(max, count + 1))}
+        onClick={() => handleUpdate(Math.min(max, count == "" ? 1 : count + 1))}
       >
         +
       </button>
