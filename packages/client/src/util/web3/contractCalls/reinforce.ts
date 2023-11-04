4d7dec9f5555fe899c4a997a20f62690851668d4
Nabs <43505437+NAB5@users.noreply.github.com>
2023-11-04 14:43:22 -0500

    fix: reinforce call queue id

--- packages/client/src/util/web3/contractCalls/reinforce.ts
+++ packages/client/src/util/web3/contractCalls/reinforce.ts
@@ -3,13 +3,14 @@ import { decodeEntity } from "@latticexyz/store-sync/recs";
 import { execute } from "src/network/actions";
 import { components } from "src/network/components";
 import { SetupNetworkResult } from "src/network/types";
-import { randomEntity } from "src/util/common";
+import { TransactionQueueType } from "src/util/constants";
+import { hashEntities } from "src/util/encode";
 import { Hex } from "viem";
 
 export const reinforce = async (rockEntity: Entity, arrivalEntity: Entity, network: SetupNetworkResult) => {
   const { key } = decodeEntity(components.MapItemArrivals.metadata.keySchema, arrivalEntity);
 
   await execute(() => network.worldContract.write.reinforce([rockEntity as Hex, key as Hex]), network, {
-    id: randomEntity(),
+    id: hashEntities(TransactionQueueType.Reinforce, key, rockEntity),
   });
 };
