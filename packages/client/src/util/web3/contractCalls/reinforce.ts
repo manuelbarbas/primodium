eb82e2bd4de98cfb8afabde241ebfcc565cd2889
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-11-03 20:33:05 -0700

    fix: replace hashed id for reinforce with randomEntity due to overflow

--- packages/client/src/util/web3/contractCalls/reinforce.ts
+++ packages/client/src/util/web3/contractCalls/reinforce.ts
@@ -3,14 +3,13 @@ import { decodeEntity } from "@latticexyz/store-sync/recs";
 import { execute } from "src/network/actions";
 import { components } from "src/network/components";
 import { SetupNetworkResult } from "src/network/types";
-import { TransactionQueueType } from "src/util/constants";
-import { hashEntities } from "src/util/encode";
+import { randomEntity } from "src/util/common";
 import { Hex } from "viem";
 
 export const reinforce = async (rockEntity: Entity, arrivalEntity: Entity, network: SetupNetworkResult) => {
   const { key } = decodeEntity(components.MapItemArrivals.metadata.keySchema, arrivalEntity);
 
   await execute(() => network.worldContract.write.reinforce([rockEntity as Hex, key as Hex]), network, {
-    id: hashEntities(TransactionQueueType.Reinforce, arrivalEntity, rockEntity),
+    id: randomEntity(),
   });
 };
