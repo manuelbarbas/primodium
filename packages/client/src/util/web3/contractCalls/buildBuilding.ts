ec75561aded0f0eef994ccc15656e7f542dec4b4
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-11-06 09:24:45 -0800

    fix: set build to 7m gas limit

--- packages/client/src/util/web3/contractCalls/buildBuilding.ts
+++ packages/client/src/util/web3/contractCalls/buildBuilding.ts
@@ -19,7 +19,7 @@ export const buildBuilding = async (network: SetupNetworkResult, building: EBuil
   const position = { ...coord, parent: activeAsteroid as Hex };
 
   await execute(
-    () => network.worldContract.write.build([building, position], { gas: 3500000n }),
+    () => network.worldContract.write.build([building, position], { gas: 7000000n }),
     network,
     {
       id: hashEntities(TransactionQueueType.Build, encodeCoord(coord)),
