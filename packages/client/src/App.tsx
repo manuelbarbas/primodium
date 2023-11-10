387f67686004b29425df1ecc7266c58214e9166a
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-11-09 17:23:53 -0800

    refactor: match initializing css with AppLoadingState

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -51,14 +51,18 @@ export default function App() {
 
   if (networkLayer === undefined) {
     return (
-      <div className="relative bg-black">
+      <div className="bg-black h-screen">
         <div className="absolute w-full h-full star-background opacity-50" />
-        <div className="flex flex-col items-center justify-center h-screen text-white font-mono gap-4">
-          <p className="text-lg text-white">
-            <span className="font-mono">Initializing</span>
-            <span>&hellip;</span>
-          </p>
-          <Progress value={100} max={100} className="animate-pulse" />
+        <div className="relative">
+          <div className="flex items-center justify-center h-screen">
+            <div className="flex flex-col items-center gap-4">
+              <p className="text-lg text-white">
+                <span className="font-mono">Initializing</span>
+                <span>&hellip;</span>
+              </p>
+              <Progress value={100} max={100} className="animate-pulse" />
+            </div>
+          </div>
         </div>
       </div>
     );
