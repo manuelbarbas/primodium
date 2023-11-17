6442c94e22f051c27422c6f0fb22ae19de286536
Nabs <43505437+NAB5@users.noreply.github.com>
2023-11-17 17:25:49 +0300

    feat: verify user chat message

--- packages/client/api/chat.ts
+++ packages/client/api/chat.ts
@@ -1,5 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import Pusher from "pusher";
+import { isPlayer, entityToAddress } from "../src/util/common";
+import { recoverMessageAddress } from "viem";
 
 export const pusher = new Pusher({
   appId: process.env.PRI_PUSHER_APP_ID!,
@@ -11,17 +13,28 @@ export const pusher = new Pusher({
 });
 
 export default async function handleChatRequest(req: VercelRequest, res: VercelResponse) {
-  const { user, message, uuid } = req.body;
+  const { user, message, signature, uuid, channel } = req.body;
 
-  if (message.length === 0) res.status(200).send("OK");
+  if (!isPlayer(user)) return res.status(400).send("Invalid user");
+
+  const signer = await recoverMessageAddress({
+    message,
+    signature,
+  });
+
+  if (signer !== entityToAddress(user)) return res.status(401).send("Invalid signature");
+
+  if (message.length === 0) return res.status(200).send("OK");
 
   try {
-    await pusher.trigger("chat", "message", {
+    await pusher.trigger(channel, "message", {
       user: user ?? "unknown",
       message: message.trim(128),
       time: Date.now(),
       uuid: uuid,
     });
+
+    // store message in kv by channel
   } catch (e) {
     console.log(e);
   }
