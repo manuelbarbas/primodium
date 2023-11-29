df949ce206c47e302142c8ca8f2b146ad1465d4a
Nabs <43505437+NAB5@users.noreply.github.com>
2023-11-29 11:37:44 -0600

    style: remove rounding

--- packages/client/tailwind.config.ts
+++ packages/client/tailwind.config.ts
@@ -34,9 +34,9 @@ export default {
           warning: colors.yellow[600],
           error: colors.rose[700],
 
-          "--rounded-box": ".4rem", // border radius rounded-box utility class, used in card and other large boxes
-          "--rounded-btn": ".4rem", // border radius rounded-btn utility class, used in buttons and similar element
-          "--rounded-badge": ".3rem", // border radius rounded-badge utility class, used in badges and similar
+          "--rounded-box": "0rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "0rem", // border radius rounded-badge utility class, used in badges and similar
           "--animation-btn": "0.25s", // duration of animation when you click on button
           "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
           "--btn-text-case": "uppercase", // set default text transform for buttons
