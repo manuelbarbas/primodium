f5404ceccaf0e42e38d7ff3424e0c48b79f4d87f
a-najafi <najafi73alireza@gmail.com>
2023-12-11 13:19:01 +0300

    fix: fixes client resource calculations for this update

--- packages/client/src/hooks/useFullResourceCount.ts
+++ packages/client/src/hooks/useFullResourceCount.ts
@@ -1,12 +1,7 @@
 import { Entity } from "@latticexyz/recs";
 import { useMemo } from "react";
 import { BlockNumber } from "src/network/components/clientComponents";
-import {
-  getFullResourceCount,
-  getFullResourceCounts,
-  getPlayerFullResourceCount,
-  getPlayerFullResourceCounts,
-} from "src/util/resource";
+import { getFullResourceCount, getFullResourceCounts } from "src/util/resource";
 
 export function useFullResourceCount(resource: Entity, spaceRockEntity?: Entity) {
   const { value: blockNumber } = BlockNumber.use(undefined, {
@@ -28,26 +23,4 @@ export function useFullResourceCounts(spaceRockEntity?: Entity) {
   return useMemo(() => {
     return getFullResourceCounts(spaceRockEntity);
   }, [blockNumber, spaceRockEntity]);
-}
-
-export function usePlayerFullResourceCounts(playerEntity: Entity) {
-  const { value: blockNumber } = BlockNumber.use(undefined, {
-    value: 0n,
-    avgBlockTime: 1,
-  });
-
-  return useMemo(() => {
-    return getPlayerFullResourceCounts(playerEntity);
-  }, [blockNumber, playerEntity]);
-}
-
-export function usePlayerFullResourceCount(resource: Entity, playerEntity: Entity) {
-  const { value: blockNumber } = BlockNumber.use(undefined, {
-    value: 0n,
-    avgBlockTime: 1,
-  });
-
-  return useMemo(() => {
-    return getPlayerFullResourceCount(resource, playerEntity);
-  }, [blockNumber, playerEntity]);
 }
