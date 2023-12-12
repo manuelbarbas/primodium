572a3d451d3620db65f553eb29fdf373f8c411f9
Nabs <43505437+NAB5@users.noreply.github.com>
2023-12-12 13:02:08 -0500

    style: make it how hank wants it

--- packages/client/src/screens/Landing.tsx
+++ packages/client/src/screens/Landing.tsx
@@ -38,34 +38,33 @@ export const Landing: React.FC = () => {
         animate={{ scale: 1, opacity: 1, y: 0, transition: { delay: 0.25, duration: 0.5 } }}
         className="flex items-center justify-center h-screen text-white font-mono"
       >
-        <div className="relative text-center border border-secondary/25 px-24 py-16 bg-neutral/50 flex flex-col items-center justify-around gap-2">
+        <div className="relative text-center border border-secondary/25 px-24 py-16 bg-neutral/50 flex flex-col items-center gap-2">
           <div className="absolute top-0 w-full h-full topographic-background2 opacity-25" />
           <h1 className="text-8xl font-bold uppercase stroke stroke-white stroke-4 z-10">Primodium</h1>
           <h1 className="text-8xl font-bold uppercase text-secondary z-10 -mt-24 opacity-75 z-0">Primodium</h1>
+          <div className="w-4/5 relative flex flex-col items-center gap-2 h-40">
+            <img
+              src={"/img/mainbase.png"}
+              className=" w-32 pixel-images opacity-75 scale-x-[-1] z-0 absolute bottom-6 margin-auto z-20"
+            />
+            <div className="absolute bg-gray-900 blur-[15px] w-56 h-32 margin-auto bottom-0 z-10" />
+          </div>
 
           {!message && (
-            <div className="w-4/5 relative flex flex-col items-center gap-2 h-40">
-              <img
-                src={"/img/mainbase.png"}
-                className=" w-32 pixel-images opacity-75 scale-x-[-1] z-0 drop-shadow-2xl absolute bottom-6 margin-auto z-20"
-              />
-              <div className="absolute bg-gray-800/60 blur-[5px] w-40 h-32 margin-auto bottom-0 z-10" />
-            </div>
-          )}
-          {message ? (
-            <p className="text-lg">{message}</p>
-          ) : (
             <Button
               onClick={async () => {
                 await handlePlay();
               }}
-              className="btn-secondary w-4/5 star-background hover:scale-125 relative z-10 drop-shadow-2xl mt-4"
+              className="btn-secondary w-4/5 star-background hover:scale-125 relative z-10 mt-4"
             >
               enter
             </Button>
           )}
+          {message && (
+            <div className="btn disabled btn-secondary w-4/5 star-background relative z-10 mt-4">{message}</div>
+          )}
           <div className="absolute bottom-0 right-0 p-2 font-bold opacity-50">
-            {params.get("version") ?? ""}{" "}
+            {params.get("version") ?? ""}
             {import.meta.env.PRI_VERCEL_GIT_COMMIT_SHA ? import.meta.env.PRI_VERCEL_GIT_COMMIT_SHA.slice(0, 7) : ""}
           </div>
           {/* SHIPS */}
@@ -114,7 +113,6 @@ export const Landing: React.FC = () => {
             src={ResourceImage.get(EntityType.HammerDrone)}
             className="absolute top-10 -right-24 translate-x-full -translate-y-1/2 p-0 w-16 pixel-images opacity-50"
           />
-
           <motion.img
             initial={{ x: "-100%", y: "-50%", scaleX: "-100%" }}
             animate={{
