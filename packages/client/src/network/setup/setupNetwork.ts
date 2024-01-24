11e68b203a5096b451ccbcf4659a7358f01ab54d
Nabs <43505437+NAB5@users.noreply.github.com>
2024-01-23 20:31:07 -0500

    feat: replace syncToRecs with our custom setup

--- packages/client/src/network/setup/setupNetwork.ts
+++ packages/client/src/network/setup/setupNetwork.ts
@@ -1,12 +1,11 @@
 import { transportObserver } from "@latticexyz/common";
-import { syncToRecs } from "@latticexyz/store-sync/recs";
 import mudConfig from "contracts/mud.config";
 import { Hex, createPublicClient, fallback, http } from "viem";
 import { getNetworkConfig } from "../config/getNetworkConfig";
 import { createClock } from "../createClock";
-import { otherTables } from "../otherTables";
 import { world } from "../world";
-import { resolveConfig } from "@latticexyz/store";
+import { setupRecs } from "./setupRecs";
+import { otherTables } from "../otherTables";
 
 export async function setupNetwork() {
   const networkConfig = getNetworkConfig();
@@ -17,17 +16,13 @@ export async function setupNetwork() {
   };
 
   const publicClient = createPublicClient(clientOptions);
-  const tables = resolveConfig(mudConfig).tables;
 
-  const { components, latestBlock$, latestBlockNumber$, storedBlockLogs$, waitForTransaction } = await syncToRecs({
+  const { components, latestBlock$, latestBlockNumber$, tables, storedBlockLogs$, waitForTransaction } = setupRecs({
+    mudConfig,
     world,
-    config: mudConfig,
-    address: networkConfig.worldAddress as Hex,
     publicClient,
-    startBlock: BigInt(networkConfig.initialBlockNumber),
-    indexerUrl: networkConfig.indexerUrl,
-    tables: otherTables,
-    initialBlockLogs: [],
+    address: networkConfig.worldAddress as Hex,
+    otherTables: otherTables,
   });
 
   const clock = createClock(latestBlock$, {
