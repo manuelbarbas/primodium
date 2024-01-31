e0a6b82927c9bf57babbfc0f81885fd619a66ba1
0xhank <zeroxhank@gmail.com>
2024-01-31 09:56:53 -0500

    feat: proper prototype config

--- packages/client/src/network/setup/contractCalls/claimObjective.ts
+++ packages/client/src/network/setup/contractCalls/claimObjective.ts
@@ -1,22 +1,25 @@
 import { Entity } from "@latticexyz/recs";
 import { ampli } from "src/ampli";
 import { execute } from "src/network/actions";
+import { components } from "src/network/components";
 import { MUD } from "src/network/types";
 import { getBlockTypeName } from "src/util/common";
 import { ObjectiveEntityLookup, ObjectiveEnumLookup, TransactionQueueType } from "src/util/constants";
 import { getSystemId, hashEntities } from "src/util/encode";
+import { Hex } from "viem";
 import { parseReceipt } from "../../../util/analytics/parseReceipt";
 
 export const claimObjective = async (mud: MUD, rawObjective: Entity) => {
+  const selectedRock = components.SelectedRock.get()?.value;
   const objective = ObjectiveEnumLookup[rawObjective];
-  if (!objective) throw new Error(`Objective ${rawObjective} not found in ObjectiveEnumLookup`);
+  if (!selectedRock || !objective) throw new Error(`Objective ${rawObjective} not found in ObjectiveEnumLookup`);
 
   await execute(
     {
       mud,
       functionName: "claimObjective",
       systemId: getSystemId("ObjectiveSystem"),
-      args: [objective],
+      args: [selectedRock as Hex, objective],
       delegate: true,
     },
     {
