664cb982f4f86b8699c47cacd3782d49365cd031
0xhank <zeroxhank@gmail.com>
2024-02-02 11:44:58 -0500

    fix: move notifications

--- packages/client/src/network/systems/setupBattleNotifications.ts
+++ packages/client/src/network/systems/setupBattleNotifications.ts
@@ -53,28 +53,33 @@ export function setupBattleNotifications(mud: MUD) {
     }
   });
 
-  const orbitingQueue = new Map<Entity, bigint>();
+  const fleetTransitQueue = new Map<Entity, bigint>();
   defineComponentSystem(systemWorld, FleetMovement, (update) => {
-    const selectedRock = components.SelectedRock.get()?.value;
+    const ownerRock = components.OwnedBy.get(update.entity)?.value as Entity | undefined;
+    const ownerRockOwner = components.OwnedBy.get(ownerRock)?.value;
+    const player = components.Account.get()?.value;
     const now = components.Time.get()?.value ?? 0n;
     const entity = update.entity;
 
     const arrival = update.value[0];
     if (!arrival) return;
 
-    if (arrival.origin !== selectedRock && arrival.destination !== selectedRock) return;
+    if (!ownerRockOwner || ownerRockOwner !== player) return;
 
     //it has arrived
     if (arrival.sendTime + 30n < now) return;
+    const minutes = (arrival.arrivalTime - now) / 60n;
+    const seconds = (arrival.arrivalTime - now) % 60n;
+    const output = minutes > 0 ? `${minutes} minute(s)` : `${seconds} seconds`;
 
-    toast.info(`Your fleet is en route and will arrive in ${(arrival.arrivalTime - now) / 60n} minute(s).`);
-    orbitingQueue.set(entity, arrival.arrivalTime);
+    toast.info(`Your fleet is en route and will arrive in ${output}.`);
+    fleetTransitQueue.set(entity, arrival.arrivalTime);
   });
 
   defineComponentSystem(systemWorld, BlockNumber, () => {
     const now = components.Time.get()?.value ?? 0n;
 
-    orbitingQueue.forEach((arrivalTime, entityId) => {
+    fleetTransitQueue.forEach((arrivalTime, entityId) => {
       const arrival = FleetMovement.get(entityId);
 
       if (!arrival) return;
@@ -83,7 +88,7 @@ export function setupBattleNotifications(mud: MUD) {
       if (now > arrivalTime) {
         toast.info(`Your fleet has arrived at [${destination?.x ?? 0}, ${destination?.y ?? 0}].`);
 
-        orbitingQueue.delete(entityId);
+        fleetTransitQueue.delete(entityId);
       }
     });
   });
