7c22149ccd9a568997a175e9b40fd2a7258c70aa
Nabs <43505437+NAB5@users.noreply.github.com>
2024-03-04 17:46:25 -0500

    fix: use time instead of block number

--- packages/client/src/hooks/useHasEnoughResources.ts
+++ packages/client/src/hooks/useHasEnoughResources.ts
@@ -1,10 +1,10 @@
 import { Entity } from "@latticexyz/recs";
 import { useMemo } from "react";
-import { BlockNumber } from "src/network/components/clientComponents";
+import { Time } from "src/network/components/clientComponents";
 import { getRecipe, hasEnoughResources } from "src/util/recipe";
 
 export const useHasEnoughResources = (recipe: ReturnType<typeof getRecipe>, spaceRock: Entity, count = 1n) => {
-  const time = BlockNumber.use()?.value ?? 0n;
+  const time = Time.use()?.value ?? 0n;
 
   return useMemo(() => hasEnoughResources(recipe, spaceRock, count), [time, recipe, count, spaceRock]);
 };
