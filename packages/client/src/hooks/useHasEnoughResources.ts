8198576a6c062b1d7a87345e4b06920c46d89cc0
0xhank <zeroxhank@gmail.com>
2024-03-04 16:11:27 -0500

    fix: show floor of integers in formatNumber

--- packages/client/src/hooks/useHasEnoughResources.ts
+++ packages/client/src/hooks/useHasEnoughResources.ts
@@ -1,18 +1,10 @@
 import { Entity } from "@latticexyz/recs";
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { BlockNumber } from "src/network/components/clientComponents";
 import { getRecipe, hasEnoughResources } from "src/util/recipe";
 
 export const useHasEnoughResources = (recipe: ReturnType<typeof getRecipe>, spaceRock: Entity, count = 1n) => {
-  const [enoughResources, setEnoughResources] = useState(false);
-  const { value: blockNumber } = BlockNumber.use(undefined, {
-    value: 0n,
-    avgBlockTime: 1,
-  });
+  const time = BlockNumber.use()?.value ?? 0n;
 
-  useEffect(() => {
-    setEnoughResources(hasEnoughResources(recipe, spaceRock, count));
-  }, [blockNumber, recipe, count, spaceRock]);
-
-  return enoughResources;
+  return useMemo(() => hasEnoughResources(recipe, spaceRock, count), [time, recipe, count, spaceRock]);
 };
