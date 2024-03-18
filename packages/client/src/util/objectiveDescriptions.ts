1e34b43f0a2c8208dab2aa3a155dc37449a852aa
CometShock <cometshockcrypto@gmail.com>
2024-03-18 14:54:08 -0500

    chore: remove multilevel objectiveDescriptions

--- packages/client/src/util/objectiveDescriptions.ts
+++ packages/client/src/util/objectiveDescriptions.ts
@@ -33,7 +33,7 @@ export const ObjectiveDescriptions = new Map<EObjectives, string>([
   ],
   [
     EObjectives.BuildGarage,
-    "Select the garage from the building menu and place it on an empty tile. Garages provide housing for units. ",
+    "Select the garage from the building menu and place it on an empty tile. Garages provide housing for units.",
   ],
   [
     EObjectives.BuildWorkshop,
@@ -55,135 +55,38 @@ export const ObjectiveDescriptions = new Map<EObjectives, string>([
     EObjectives.TrainMinutemanMarine1,
     "Select the workshop you placed on the map to train Minuteman marines. Minutemen are basic defensive marines.",
   ],
-  [
-    EObjectives.TrainMinutemanMarine2,
-    "Select the workshop you placed on the map to train Minuteman marines. Minutemen are basic defensive marines.",
-  ],
-  [
-    EObjectives.TrainMinutemanMarine3,
-    "Select the workshop you placed on the map to train Minuteman marines. Minutemen are basic defensive marines.",
-  ],
 
   [
     EObjectives.TrainTridentMarine1,
     "Select the workshop you placed on the map to train Trident marines. Trident marines are basic offensive units.",
   ],
-  [
-    EObjectives.TrainTridentMarine2,
-    "Select the workshop you placed on the map to train Trident marines. Trident marines are basic offensive units.",
-  ],
-  [
-    EObjectives.TrainTridentMarine3,
-    "Select the workshop you placed on the map to train Trident marines. Trident marines are basic offensive units.",
-  ],
+
   [
     EObjectives.TrainAnvilDrone1,
     "Select the drone factory you placed on the map to train anvil drones. Anvil drones are basic defensive drones.",
   ],
-  [
-    EObjectives.TrainAnvilDrone2,
-    "Select the drone factory you placed on the map to train anvil drones. Anvil drones are basic defensive drones.",
-  ],
-  [
-    EObjectives.TrainAnvilDrone3,
-    "Select the drone factory you placed on the map to train anvil drones. Anvil drones are basic defensive drones.",
-  ],
+
   [
     EObjectives.DefeatPirateBase1,
     "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
   ],
-  [
-    EObjectives.DefeatPirateBase2,
-    "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
-  ],
-  [
-    EObjectives.DefeatPirateBase3,
-    "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
-  ],
-  [
-    EObjectives.DefeatPirateBase4,
-    "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
-  ],
-  [
-    EObjectives.DefeatPirateBase5,
-    "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
-  ],
-  [
-    EObjectives.DefeatPirateBase6,
-    "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
-  ],
-  [
-    EObjectives.DefeatPirateBase7,
-    "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
-  ],
-  [
-    EObjectives.DefeatPirateBase8,
-    "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
-  ],
-  [
-    EObjectives.DefeatPirateBase9,
-    "Select the starmap on the top of your screen, then choose the red tinted pirate asteroid and send units to attack and raid.",
-  ],
+
   [
     EObjectives.ExpandBase1,
-    "Select your main base and click on Expand base to expand your buildable zone and uncover more resource ores.",
-  ],
-  [
-    EObjectives.ExpandBase2,
-    "Select your main base and click on Expand base to expand your buildable zone and uncover more resource ores.",
-  ],
-  [
-    EObjectives.ExpandBase3,
-    "Select your main base and click on Expand base to expand your buildable zone and uncover more resource ores.",
-  ],
-  [
-    EObjectives.ExpandBase4,
-    "Select your main base and click on Expand base to expand your buildable zone and uncover more resource ores.",
-  ],
-  [
-    EObjectives.ExpandBase5,
-    "Select your main base and click on Expand base to expand your buildable zone and uncover more resource ores.",
-  ],
-  [
-    EObjectives.ExpandBase6,
     "Select your main base and click on Expand base to expand your buildable zone and uncover more resource ores.",
   ],
   [
     EObjectives.TrainHammerDrone1,
     "Select the drone factory you placed on the map to train hammer drones. Hammer drones are used for attacking.",
   ],
-  [
-    EObjectives.TrainHammerDrone2,
-    "Select the drone factory you placed on the map to train hammer drones. Hammer drones are used for attacking.",
-  ],
-  [
-    EObjectives.TrainHammerDrone3,
-    "Select the drone factory you placed on the map to train hammer drones. Hammer drones are used for attacking.",
-  ],
 
   [
-    EObjectives.TrainAegisDrone2,
-    "Select the drone factory you placed on the map to train aegis drones. Aegis drones are strong defensive units, but take up more housing.",
-  ],
-  [
-    EObjectives.TrainAegisDrone2,
-    "Select the drone factory you placed on the map to train aegis drones. Aegis drones are strong defensive units, but take up more housing.",
-  ],
-  [
-    EObjectives.TrainAegisDrone3,
+    EObjectives.TrainAegisDrone1,
     "Select the drone factory you placed on the map to train aegis drones. Aegis drones are strong defensive units, but take up more housing.",
   ],
 
   [
     EObjectives.TrainStingerDrone1,
-    "Select the drone factory you placed on the map to train aegis drones. Stinger drones are strong and fast offensive units, but take up more housing.",
-  ],
-  [
-    EObjectives.TrainStingerDrone2,
-    "Select the drone factory you placed on the map to train aegis drones. Stinger drones are strong and fast offensive units, but take up more housing.",
-  ],
-  [
-    EObjectives.TrainStingerDrone3,
     "Select the drone factory you placed on the map to train aegis drones. Stinger drones are strong and fast offensive units, but take up more housing.",
   ],
 
