3f60bff66c91671ade7a450937ebb4bceeba3fa6
0xhank <zeroxhank@gmail.com>
2024-04-03 13:44:21 -0400

    fix: relative wrapper

--- packages/client/src/components/shared/TransactionQueueMask.tsx
+++ packages/client/src/components/shared/TransactionQueueMask.tsx
@@ -13,20 +13,21 @@ export const TransactionQueueMask: React.FC<{
   if (queuePosition === -1) return <div className={className}>{children}</div>;
 
   return (
-    // @NAB5 - This allows an absolute div to retain its position. Is there a better way?
-    <div className={`${className} ${className?.includes("absolute") ? "" : "relative"}`}>
-      {children}
-      <div className="absolute inset-0 bg-black/75 rounded-box">
-        {queuePosition !== 0 && (
-          <div className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-black rounded-full px-2 border border-secondary text-xs">
-            {queuePosition}
-          </div>
-        )}
-        {queuePosition === 0 && (
-          <div className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
-            <Loader size={size} />
-          </div>
-        )}
+    <div className={className}>
+      <div className="relative">
+        {children}
+        <div className="absolute inset-0 bg-black/75 rounded-box">
+          {queuePosition !== 0 && (
+            <div className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-black rounded-full px-2 border border-secondary text-xs">
+              {queuePosition}
+            </div>
+          )}
+          {queuePosition === 0 && (
+            <div className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
+              <Loader size={size} />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
