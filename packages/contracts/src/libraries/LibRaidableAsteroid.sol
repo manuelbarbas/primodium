e7c20737fce9f98d56a53eae895a695d4a66b452
CometShock <cometshockcrypto@gmail.com>
2024-04-22 14:31:34 -0500

    feat: init raidable asteroid library

---
+++ packages/contracts/src/libraries/LibRaidableAsteroid.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { OwnedBy } from "codegen/index.sol";
+import { LibProduction } from "libraries/LibProduction.sol";
+import { LibAsteroid } from "libraries/LibAsteroid.sol";
+
+library LibRaidableAsteroid {
+  function createRaidableAsteroid() internal {}
+}
