d16e6fbd5c0139d6cb4cface3e9f456e2b5d5281
CometShock <cometshockcrypto@gmail.com>
2024-04-22 20:02:42 -0500

    feat: asteroid probability test

--- packages/contracts/test/libraries/LibRaidableAsteroid.t.sol
+++ packages/contracts/test/libraries/LibRaidableAsteroid.t.sol
@@ -4,11 +4,25 @@ pragma solidity >=0.8.24;
 import { console, PrimodiumTest } from "test/PrimodiumTest.t.sol";
 import { addressToEntity } from "src/utils.sol";
 
+import { P_AsteroidProbabilityConfig, P_AsteroidProbabilityConfigData } from "codegen/index.sol";
+
 import { EResource } from "src/Types.sol";
 import { MainBasePrototypeId, DroidPrototypeId } from "codegen/Prototypes.sol";
 
 import { LibRaidableAsteroid } from "libraries/LibRaidableAsteroid.sol";
 
 contract LibRaidableAsteroidTest is PrimodiumTest {
-  // todo: check P_AsteroidProbabilityConfig adds up to 100
+  function testAsteroidProbabilityConfig() public {
+    P_AsteroidProbabilityConfigData memory asteroidProb = P_AsteroidProbabilityConfig.get();
+    assertEq(
+      asteroidProb.common1 +
+        asteroidProb.common2 +
+        asteroidProb.eliteMicro +
+        asteroidProb.eliteSmall +
+        asteroidProb.eliteMedium +
+        asteroidProb.eliteLarge,
+      100,
+      "Secondary asteroid probabilities do not add up to 100"
+    );
+  }
 }
