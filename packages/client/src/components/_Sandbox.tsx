b2d37cadcb96d7c63cb825654fee20407a430a0d
0xhank <zeroxhank@gmail.com>
2024-04-25 11:05:10 -0400

    feat: fix dropdown

--- packages/client/src/components/_Sandbox.tsx
+++ packages/client/src/components/_Sandbox.tsx
@@ -1,22 +1,22 @@
-import { Button } from "@/components/core/Button";
-import { Card, SecondaryCard } from "@/components/core/Card";
-import { Range } from "@/components/core/Range";
-import { NumberInput } from "@/components/core/NumberInput";
-import { EntityType } from "@/util/constants";
 import { Accordion } from "@/components/core/Accordion";
 import { Badge } from "@/components/core/Badge";
+import { Button } from "@/components/core/Button";
 import { CapacityBar } from "@/components/core/CapacityBar";
+import { Card, SecondaryCard } from "@/components/core/Card";
+import { Dropdown } from "@/components/core/Dropdown";
 import { HUD } from "@/components/core/HUD";
+import { IconLabel } from "@/components/core/IconLabel";
 import { Join } from "@/components/core/Join";
+import { Loader } from "@/components/core/Loader";
 import { Modal } from "@/components/core/Modal";
+import { NumberInput } from "@/components/core/NumberInput";
 import { Progress } from "@/components/core/Progress";
+import { PushButton } from "@/components/core/PushButton";
+import { Range } from "@/components/core/Range";
 import { Tabs } from "@/components/core/Tabs";
 import { TextInput } from "@/components/core/TextInput";
 import { Toggle } from "@/components/core/Toggle";
-import { Loader } from "@/components/core/Loader";
-import { IconLabel } from "@/components/core/IconLabel";
-import { PushButton } from "@/components/core/PushButton";
-import { Dropdown } from "@/components/core/Dropdown";
+import { EntityType } from "@/util/constants";
 
 export const _Sandbox = () => {
   return (
@@ -203,9 +203,15 @@ export const _Sandbox = () => {
           <TextInput placeholder="test" topRightLabel="Text input" />
 
           <Dropdown variant={"bottomLeft"}>
-            <Dropdown.Item onSelect={() => console.log("hello")}>Test 1</Dropdown.Item>
-            <Dropdown.Item onSelect={() => console.log("hello")}>Test 2</Dropdown.Item>
-            <Dropdown.Item onSelect={() => console.log("hello")}>Test 3</Dropdown.Item>
+            <Dropdown.Item value={1} onSelect={() => console.log("hello")}>
+              Test 1
+            </Dropdown.Item>
+            <Dropdown.Item value={2} onSelect={() => console.log("hello")}>
+              Test 2
+            </Dropdown.Item>
+            <Dropdown.Item value={3} onSelect={() => console.log("hello")}>
+              Test 3
+            </Dropdown.Item>
           </Dropdown>
         </div>
       </div>
--- packages/client/src/components/core/Dropdown.tsx
+++ packages/client/src/components/core/Dropdown.tsx
@@ -1,12 +1,12 @@
-import React, { FC, ReactNode, useEffect, useRef, useState } from "react";
-import { cn } from "@/util/client";
 import { Button } from "@/components/core/Button";
-import { FaAngleDown } from "react-icons/fa";
 import { SecondaryCard } from "@/components/core/Card";
+import { cn } from "@/util/client";
 import { VariantProps, cva } from "class-variance-authority";
+import { FC, ReactNode, useEffect, useRef, useState } from "react";
+import { FaAngleDown } from "react-icons/fa";
 
 const dropdownVariants = cva(
-  "z-50 absolute mt-1 p-2 bg-neutral border border-secondary/25 w-44 pointer-events-auto data-[state=close]:pointer-events-none data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=close]:animate-out data-[state=close]:fade-out fill-mode-forwards",
+  "z-50 absolute mt-1 p-1 bg-neutral border border-secondary/25 w-44 pointer-events-auto data-[state=close]:pointer-events-none data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=close]:animate-out data-[state=close]:fade-out fill-mode-forwards",
   {
     variants: {
       variant: {
@@ -91,10 +91,11 @@ export const Dropdown: FC<DropdownProps> & {
 };
 
 const DropdownItem: FC<{
+  value: string | number;
   children: ReactNode;
-  onSelect?: () => void;
-}> = ({ children, onSelect }) => (
-  <div className="w-full" onClick={onSelect}>
+  onSelect?: (value?: string | number) => void;
+}> = ({ children, onSelect, value }) => (
+  <div className="w-full" onClick={() => onSelect && onSelect(value)}>
     {children}
   </div>
 );
