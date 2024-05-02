05b91f962dd211c3f658ad92e0456aa9c20f5d5f
0xhank <zeroxhank@gmail.com>
2024-05-02 14:33:02 -0400

    feat: new intro styling

--- packages/client/src/components/hud/modals/Intro.tsx
+++ packages/client/src/components/hud/modals/Intro.tsx
@@ -1,75 +1,69 @@
-import { Button } from "@/components/core/Button";
+import { SecondaryCard } from "@/components/core/Card";
 import { Modal } from "@/components/core/Modal";
 import { usePersistentStore } from "@/game/stores/PersistentStore";
-import { useState } from "react";
+import { InterfaceIcons } from "@primodiumxyz/assets";
 import { useShallow } from "zustand/react/shallow";
 
 export const Intro = () => {
   const { showIntro, setShowIntro } = usePersistentStore(
     useShallow((state) => ({ setShowIntro: state.setShowIntro, showIntro: state.showIntro }))
   );
-  const [page, setPage] = useState(0);
-
   return (
     <Modal title="Battle of the Shards" startOpen={showIntro} onClose={() => setShowIntro(false)}>
-      <Modal.Content className="w-[50rem] min-h-[25rem] font-mono p-6">
-        {page === 0 && <PageZero onContinue={() => setPage(1)} />}
-        {page === 1 && <PageOne final />}
+      <Modal.Content className="w-[50rem] min-h-[25rem] p-6">
+        <PageZero />
       </Modal.Content>
     </Modal>
   );
 };
 
-type PageProps = {
-  onContinue?: () => void;
-  final?: boolean;
-};
-const PageZero = (props: PageProps) => {
-  const Btn = props.final ? Modal.CloseButton : Button;
+const PageZero = () => {
   return (
-    <div className="flex flex-col gap-6 p-8 h-full">
-      <div className="text-center font-bold text-accent uppercase gap-6">Welcome to The Belt.</div>
-      <p className="text-sm">
-        For thousands of years, humanity has waged a galactic war across the stars. But over time, these warring
-        alliances have run out of resources in the Milky Way.
+    <div className="flex flex-col p-8 h-full">
+      <div className="text-center font-bold text-accent uppercase mb-2">
+        Welcome to The Belt: The last hope for humanity
+      </div>
+      <p className="text-xs text-center">
+        As mankind exhausts natural resources in the Milky Way, they turn to asteroid belts hidden within spacetime
+        rifts. You are an autonomous android, engineered to survive the journey to the Belt. Battle for resources to
+        ensure humanity&apos;s prosperity.
       </p>
-      <p className="text-sm">
-        Astrogeologists recently found rifts in spacetime that led to The Belt. No organic matter can pass through these
-        rifts, so Space Command sent you, an <span className="font-bold">autonomous astrogeologist android (AAA)</span>,
-        to mine resources and investigate markers of a mysterious mineral:{" "}
-        <span className="text-info font-bold">PRIMODIUM</span>.
-      </p>
-      <p className="font-bold">
-        <span className="text-warning uppercase text-center">Your mission:</span>{" "}
-        <ul className="gap-1 pl-4">
-          <li>1. Harvest resources and teleport them to Space Command through wormholes.</li>
-          <li>2. Seek out and harvest Primodium, a new mineral that unlocks vast quantities of renewable energy.</li>
-        </ul>
-      </p>
-      <Btn onClick={props.onContinue} variant="primary" className="self-end">
-        Continue
-      </Btn>
-    </div>
-  );
-};
-
-const PageOne = (props: PageProps) => {
-  const Btn = props.final ? Modal.CloseButton : Button;
-  return (
-    <div className="flex flex-col gap-6 p-8 h-full">
-      <p className="text-sm">
-        To send resources to Space Command, construct a <span className="text-success">Wormhole Generator</span> on a
-        Wormhole Asteroid. Beware -- the needs of Space Command are constantly changing!
-      </p>
-      <p className="text-sm">
-        Sensors have detected the presence of a valuable new energy source, <span className="text-info">Primodium</span>
-        , located on volatile shards in The Belt. Mine these shards and defend them from other alliances.
-      </p>
-      <p className="font-bold uppercase text-center text-warning">GOOD LUCK!</p>
-      <div className="w-full grid place-items-center">
-        <Btn onClick={props.onContinue} size="md" variant="secondary" className="w-fit">
+      <br className="w-full h-4" />
+      <div className="flex flex-col justify-center items-center gap-2">
+        <p className="text-warning">Mission Critical</p>
+        <SecondaryCard className="flex flex-row w-96 gap-2 items-center">
+          <img src={InterfaceIcons.Build} alt="build" className="w-10 h-10" />
+          <div className="flex flex-col">
+            <p>Harvest</p>
+            <p className="text-xs opacity-70">Mine resources from asteroids you control</p>
+          </div>
+        </SecondaryCard>
+        <SecondaryCard className="flex flex-row w-96 gap-2 items-center">
+          <img src={InterfaceIcons.Attack} alt="build" className="w-10 h-10" />
+          <div className="flex flex-col">
+            <p>Conquer</p>
+            <p className="text-xs opacity-70">Expand your empire by decrypting and conquering neighboring asteroids</p>
+          </div>
+        </SecondaryCard>
+        <SecondaryCard className="flex flex-row w-96 gap-2 items-center">
+          <img src={InterfaceIcons.Shard} alt="build" className="w-10 h-10" />
+          <div className="flex flex-col">
+            <p>Defend</p>
+            <p className="text-xs opacity-70">Capture and defend Volatile Shards to gain Primodium</p>
+          </div>
+        </SecondaryCard>
+        <SecondaryCard className="flex flex-row w-96 gap-2 items-center">
+          <img src={InterfaceIcons.Outgoing} alt="build" className="w-10 h-10" />
+          <div className="flex flex-col">
+            <p>Deliver</p>
+            <p className="text-xs opacity-70">Teleport your resources to Command through Wormhole Generators</p>
+          </div>
+        </SecondaryCard>
+        <br className="w-full h-4" />
+        Ready to command the stars?
+        <Modal.CloseButton variant="primary" size="md">
           Begin
-        </Btn>
+        </Modal.CloseButton>
       </div>
     </div>
   );
