5f1ce9e7750c831a55654c1a5b7eae05e399f743
kethcode <kethic@kethic.com>
2024-05-06 12:19:07 -0700

    fix: storage upgrade

--- packages/contracts/src/libraries/LibRaidableAsteroid.sol
+++ packages/contracts/src/libraries/LibRaidableAsteroid.sol
@@ -36,7 +36,7 @@ library LibRaidableAsteroid {
     // build and upgrade a storage building
     buildingEntity = LibBuilding.build(bytes32(0), StorageUnitPrototypeId, PositionData(21, 15, asteroidEntity), true);
     LibBuilding.upgradeBypassChecks(buildingEntity);
-    LibStorage.increaseMaxStorage(buildingEntity, 2);
+    LibStorage.increaseMaxStorage(buildingEntity, 1);
 
     // if common2, upgrade storage so it can hold adv resources, and build each factory type
     if (maxLevel >= 3) {
