3cb207785b4dc5e487992d3b02700d7643b77b7e
Nabs <43505437+NAB5@users.noreply.github.com>
2024-05-08 14:36:08 -0500

    fix: remove unused system

--- packages/client/src/game/scenes/starmap/systems/index.ts
+++ packages/client/src/game/scenes/starmap/systems/index.ts
@@ -3,12 +3,10 @@ import { renderAsteroids } from "@/game/scenes/starmap/systems/renderAsteroids";
 import { renderBattle } from "@/game/scenes/starmap/systems/renderBattle";
 import { renderFleets } from "@/game/scenes/starmap/systems/renderFleets";
 import { renderShardAsteroids } from "@/game/scenes/starmap/systems/renderShardAsteroids";
-import { renderTrajectory } from "@/game/scenes/starmap/systems/renderTrajectory";
 
 export const runSystems = (scene: Scene) => {
   renderAsteroids(scene);
   renderShardAsteroids(scene);
   renderFleets(scene);
   renderBattle(scene);
-  renderTrajectory(scene);
 };
