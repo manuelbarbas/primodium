b5a69a7cd2bfa22d965b88f9efaf1892c78a7f59
Emerson Hsieh <emersonhsieh@berkeley.edu>
2024-05-08 16:49:14 -0700

    feat: add amplitude analytics to FleetMergeSystem

--- packages/client/src/ampli/index.ts
+++ packages/client/src/ampli/index.ts
@@ -1077,6 +1077,56 @@ export interface SystemFleetLandSystemPrimodiumLandFleetProperties {
   transactionValid: boolean;
 }
 
+export interface SystemFleetMergeSystemPrimodiumMergeFleetsProperties {
+  /**
+   * An array of fleets represented by its Hex string.
+   *
+   * | Rule | Value |
+   * |---|---|
+   * | Item Type | string |
+   */
+  fleets: string[];
+  /**
+   * The address this transaction is from. On Amplitude, this is also tracked as the user's unique account address initilized with  `ampli.from()`.
+   */
+  transactionFrom?: string;
+  /**
+   * The amount of gas actually used by this transaction.
+   *
+   * | Rule | Value |
+   * |---|---|
+   * | Type | integer |
+   */
+  transactionGasUsed?: number;
+  /**
+   * The hash of the transaction.
+   */
+  transactionHash?: string;
+  /**
+   * The status of a transaction is 1 is successful or 0 if it was reverted. Direcrly read from `receipt.status`, as described in the ethers.js docs (https://docs.ethers.org/v5/api/providers/types/).
+   *
+   * | Rule | Value |
+   * |---|---|
+   * | Type | integer |
+   * | Min Value | 0 |
+   * | Max Value | 1 |
+   */
+  transactionStatus?: number;
+  /**
+   * The address this transaction is to. This is `null` if the transaction was an init transaction, used to deploy a contract.
+   *
+   * Since a user will only execute actions on a contract from the frontend, this value will never be null.
+   */
+  transactionTo?: string;
+  /**
+   * If the transaction is recorded on-chain and returns a valid receipt with a transaction hash, whether the transaction reverted or not, `transactionValid` will return `true`. Otherwise, it will return `false`.
+   *
+   *
+   * Note that if `transactionValid` is `true`, `transactionStatus` should be checked if a transaction is successful (status 1) or not (status 0).
+   */
+  transactionValid: boolean;
+}
+
 export interface SystemGrantRoleProperties {
   /**
    * A member of an alliance that has been granted a role. Currently only used by `system.GrantRole`.
@@ -2843,6 +2893,14 @@ export class SystemFleetLandSystemPrimodiumLandFleet implements BaseEvent {
   }
 }
 
+export class SystemFleetMergeSystemPrimodiumMergeFleets implements BaseEvent {
+  event_type = "system.FleetMergeSystem.Primodium__mergeFleets";
+
+  constructor(public event_properties: SystemFleetMergeSystemPrimodiumMergeFleetsProperties) {
+    this.event_properties = event_properties;
+  }
+}
+
 export class SystemGrantRole implements BaseEvent {
   event_type = "system.GrantRole";
 
@@ -3505,6 +3563,23 @@ export class Ampli {
     options?: EventOptions,
   ) {
     return this.track(new SystemFleetLandSystemPrimodiumLandFleet(properties), options);
+  }
+
+  /**
+   * system.FleetMergeSystem.Primodium__mergeFleets
+   *
+   * [View in Tracking Plan](https://data.amplitude.com/primodium/primodium-testnet2/events/main/latest/system.FleetMergeSystem.Primodium__mergeFleets)
+   *
+   * Event has no description in tracking plan.
+   *
+   * @param properties The event's properties (e.g. fleets)
+   * @param options Amplitude event options.
+   */
+  systemFleetMergeSystemPrimodiumMergeFleets(
+    properties: SystemFleetMergeSystemPrimodiumMergeFleetsProperties,
+    options?: EventOptions,
+  ) {
+    return this.track(new SystemFleetMergeSystemPrimodiumMergeFleets(properties), options);
   }
 
   /**
