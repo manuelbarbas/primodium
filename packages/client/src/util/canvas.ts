6c46b44353189160107b5e658f349eb4caf7a114
polarzero <0xpolarzero@gmail.com>
2024-05-08 13:16:36 +0200

    chore: remove empty line

--- packages/client/src/util/canvas.ts
+++ packages/client/src/util/canvas.ts
@@ -1,5 +1,4 @@
 export const isDomInteraction = (pointer: Phaser.Input.Pointer, type: "down" | "up") => {
   const element = type === "down" ? pointer.downElement : pointer.upElement;
-
   return element.tagName === "BUTTON" || Boolean(element.closest("button"));
 };
