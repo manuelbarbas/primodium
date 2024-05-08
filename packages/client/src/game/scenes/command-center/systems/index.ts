d3bcac77a6e05f93af9e1a72a16ff2b342c93b8a
Nabs <43505437+NAB5@users.noreply.github.com>
2024-05-08 04:56:53 -0500

    fix: remove unused systems for command

--- packages/client/src/game/scenes/command-center/systems/index.ts
+++ packages/client/src/game/scenes/command-center/systems/index.ts
@@ -1,12 +1,6 @@
 import { Scene } from "engine/types";
 import { renderOverview } from "@/game/scenes/command-center/systems/renderOverview";
-// import { renderBattle } from "@/game/scenes/starmap/systems/renderBattle";
-// import { renderFleets } from "@/game/scenes/starmap/systems/renderFleets";
-// import { renderTrajectory } from "@/game/scenes/starmap/systems/renderTrajectory";
 
 export const runSystems = (scene: Scene) => {
   renderOverview(scene);
-  // renderFleets(scene);
-  // renderBattle(scene);
-  // renderTrajectory(scene);
 };
