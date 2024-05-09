3c6482b7c15b11cc4321cf4c16215a1b7e1a0acc
Emerson Hsieh <emersonhsieh@berkeley.edu>
2024-05-08 21:30:38 -0700

    feat: add amplitude tracking to CombatSystem

--- packages/client/src/ampli/index.ts
+++ packages/client/src/ampli/index.ts
@@ -480,6 +480,56 @@ export interface SystemClaimObjectiveProperties {
   transactionValid: boolean;
 }
 
+export interface SystemCombatSystemPrimodiumAttackProperties {
+  /**
+   * A space rock entity represented by its Hex string.
+   */
+  spaceRock: string;
+  /**
+   * A space rock entity represented by its Hex string, paired with `spaceRock` as a target for a specific action.
+   */
+  spaceRockTo: string;
+  /**
+   * The address this transaction is from. On Amplitude, this is also tracked as the user's unique account address initilized with  `ampli.from()`.
+   */
+  transactionFrom?: string;
+  /**
+   * The amount of gas actually used by this transaction.
+   *
+   * | Rule | Value |
+   * |---|---|
+   * | Type | integer |
+   */
+  transactionGasUsed?: number;
+  /**
+   * The hash of the transaction.
+   */
+  transactionHash?: string;
+  /**
+   * The status of a transaction is 1 is successful or 0 if it was reverted. Direcrly read from `receipt.status`, as described in the ethers.js docs (https://docs.ethers.org/v5/api/providers/types/).
+   *
+   * | Rule | Value |
+   * |---|---|
+   * | Type | integer |
+   * | Min Value | 0 |
+   * | Max Value | 1 |
+   */
+  transactionStatus?: number;
+  /**
+   * The address this transaction is to. This is `null` if the transaction was an init transaction, used to deploy a contract.
+   *
+   * Since a user will only execute actions on a contract from the frontend, this value will never be null.
+   */
+  transactionTo?: string;
+  /**
+   * If the transaction is recorded on-chain and returns a valid receipt with a transaction hash, whether the transaction reverted or not, `transactionValid` will return `true`. Otherwise, it will return `false`.
+   *
+   *
+   * Note that if `transactionValid` is `true`, `transactionStatus` should be checked if a transaction is successful (status 1) or not (status 0).
+   */
+  transactionValid: boolean;
+}
+
 export interface SystemCraftProperties {
   /**
    * Location of an asteroid represented as the \[z\] element in the Position component. This is stored as a single string because the asteroid location is greater than the int32 number limit and has type BigNumber in the client.
@@ -3088,6 +3138,14 @@ export class SystemClaimObjective implements BaseEvent {
   }
 }
 
+export class SystemCombatSystemPrimodiumAttack implements BaseEvent {
+  event_type = "system.CombatSystem.Primodium__attack";
+
+  constructor(public event_properties: SystemCombatSystemPrimodiumAttackProperties) {
+    this.event_properties = event_properties;
+  }
+}
+
 export class SystemCraft implements BaseEvent {
   event_type = "system.Craft";
 
@@ -3699,6 +3757,23 @@ export class Ampli {
     options?: EventOptions,
   ) {
     return this.track(new SystemClaimObjective(properties), options);
+  }
+
+  /**
+   * system.CombatSystem.Primodium__attack
+   *
+   * [View in Tracking Plan](https://data.amplitude.com/primodium/primodium-testnet2/events/main/latest/system.CombatSystem.Primodium__attack)
+   *
+   * Event has no description in tracking plan.
+   *
+   * @param properties The event's properties (e.g. spaceRock)
+   * @param options Amplitude event options.
+   */
+  systemCombatSystemPrimodiumAttack(
+    properties: SystemCombatSystemPrimodiumAttackProperties,
+    options?: EventOptions,
+  ) {
+    return this.track(new SystemCombatSystemPrimodiumAttack(properties), options);
   }
 
   /**
