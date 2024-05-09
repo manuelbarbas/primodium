c795aefbcc930ce7e3cafb2923dff8fd6ccd3dea
Emerson Hsieh <emersonhsieh@berkeley.edu>
2024-05-09 00:41:43 -0700

    feat: add amplitude analytics for ClaimWormholeSystem

--- packages/client/src/ampli/index.ts
+++ packages/client/src/ampli/index.ts
@@ -668,6 +668,56 @@ export interface SystemClaimUnitsSystemPrimodiumClaimUnitsProperties {
   transactionValid: boolean;
 }
 
+export interface SystemClaimWormholeSystemPrimodiumWormholeDepositProperties {
+  /**
+   * The address this transaction is from. On Amplitude, this is also tracked as the user's unique account address initilized with  `ampli.from()`.
+   */
+  transactionFrom?: string;
+  /**
+   * The amount of gas actually used by this transaction.
+   *
+   * | Rule | Value |
+   * |---|---|
+   * | Type | integer |
+   */
+  transactionGasUsed?: number;
+  /**
+   * The hash of the transaction.
+   */
+  transactionHash?: string;
+  /**
+   * The status of a transaction is 1 is successful or 0 if it was reverted. Direcrly read from `receipt.status`, as described in the ethers.js docs (https://docs.ethers.org/v5/api/providers/types/).
+   *
+   * | Rule | Value |
+   * |---|---|
+   * | Type | integer |
+   * | Min Value | 0 |
+   * | Max Value | 1 |
+   */
+  transactionStatus?: number;
+  /**
+   * The address this transaction is to. This is `null` if the transaction was an init transaction, used to deploy a contract.
+   *
+   * Since a user will only execute actions on a contract from the frontend, this value will never be null.
+   */
+  transactionTo?: string;
+  /**
+   * If the transaction is recorded on-chain and returns a valid receipt with a transaction hash, whether the transaction reverted or not, `transactionValid` will return `true`. Otherwise, it will return `false`.
+   *
+   *
+   * Note that if `transactionValid` is `true`, `transactionStatus` should be checked if a transaction is successful (status 1) or not (status 0).
+   */
+  transactionValid: boolean;
+  /**
+   * A wormholeBase entity represented by its Hex string.
+   */
+  wormholeBase: string;
+  /**
+   * BigInt count of wormhole resource deposited into a wormhole base, represented as a string.
+   */
+  wormholeResourceCount: string;
+}
+
 export interface SystemColonySystemPrimodiumPayForMaxColonySlotsProperties {
   /**
    * Corresponds to the `resourceTypes` property in the same order.
@@ -3958,6 +4008,14 @@ export class SystemClaimUnitsSystemPrimodiumClaimUnits implements BaseEvent {
   }
 }
 
+export class SystemClaimWormholeSystemPrimodiumWormholeDeposit implements BaseEvent {
+  event_type = "system.ClaimWormholeSystem.Primodium__wormholeDeposit";
+
+  constructor(public event_properties: SystemClaimWormholeSystemPrimodiumWormholeDepositProperties) {
+    this.event_properties = event_properties;
+  }
+}
+
 export class SystemColonySystemPrimodiumPayForMaxColonySlots implements BaseEvent {
   event_type = "system.ColonySystem.Primodium__payForMaxColonySlots";
 
@@ -4731,6 +4789,23 @@ export class Ampli {
     options?: EventOptions,
   ) {
     return this.track(new SystemClaimUnitsSystemPrimodiumClaimUnits(properties), options);
+  }
+
+  /**
+   * system.ClaimWormholeSystem.Primodium__wormholeDeposit
+   *
+   * [View in Tracking Plan](https://data.amplitude.com/primodium/primodium-testnet2/events/main/latest/system.ClaimWormholeSystem.Primodium__wormholeDeposit)
+   *
+   * Event has no description in tracking plan.
+   *
+   * @param properties The event's properties (e.g. transactionFrom)
+   * @param options Amplitude event options.
+   */
+  systemClaimWormholeSystemPrimodiumWormholeDeposit(
+    properties: SystemClaimWormholeSystemPrimodiumWormholeDepositProperties,
+    options?: EventOptions,
+  ) {
+    return this.track(new SystemClaimWormholeSystemPrimodiumWormholeDeposit(properties), options);
   }
 
   /**
