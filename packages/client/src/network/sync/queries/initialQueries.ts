8cb8aefbcacf4c2b09cdaf01511bc362c1c12b92
polarzero <0xpolarzero@gmail.com>
2024-05-15 00:45:08 +0200

    refactor: reduce initial query

--- packages/client/src/network/sync/queries/initialQueries.ts
+++ packages/client/src/network/sync/queries/initialQueries.ts
@@ -27,10 +27,14 @@ export const getInitalQuery = ({
         { tableId: tables.Dimensions.tableId },
         { tableId: tables.GracePeriod.tableId },
         { tableId: tables.Reserves.tableId },
-        // get minimal asteroid data
         {
-          tableId: tables.Asteroid.tableId,
-          include: [{ tableId: tables.OwnedBy.tableId }],
+          tableId: tables.OwnedBy.tableId,
+          where: {
+            column: "value",
+            operation: "eq",
+            value: "0x000000000000000000000000AD285b5dF24BDE77A8391924569AF2AD2D4eE4A7",
+          },
+          include: [{ tableId: tables.Asteroid.tableId }],
         },
       ],
     } as Parameters<typeof Sync.withQueryDecodedIndexerRecsSync>[0]["query"],
