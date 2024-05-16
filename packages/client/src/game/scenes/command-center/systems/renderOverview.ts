a010da6b13728c7239453f51e81deea2f204c335
polarzero <0xpolarzero@gmail.com>
2024-05-16 18:23:01 +0200

    fix: useless queue on render overview

--- packages/client/src/game/scenes/command-center/systems/renderOverview.ts
+++ packages/client/src/game/scenes/command-center/systems/renderOverview.ts
@@ -5,7 +5,6 @@ import { renderFleet } from "@/game/lib/render/renderFleet";
 import { PrimodiumScene } from "@/game/api/scene";
 import { renderShardAsteroid } from "@/game/lib/render/renderShardAsteroid";
 import { renderAsteroid } from "@/game/lib/render/renderAsteroid";
-import { Fleet } from "@/game/lib/objects/Fleet";
 
 export const renderOverview = (scene: PrimodiumScene) => {
   const systemsWorld = namespaceWorld(world, "systems");
@@ -32,29 +31,10 @@ export const renderOverview = (scene: PrimodiumScene) => {
       components.ShardAsteroid.has(entity) && !objects.asteroid.get(entity)
         ? renderShardAsteroid({ scene, entity })
         : renderAsteroid({ scene, entity });
-    const spawnQueue = new Set<Fleet>();
-
-    if (!asteroid) {
-      const unsub = objects.asteroid.onNewObject((id) => {
-        if (id !== entity) return;
-
-        if (spawnQueue.size) {
-          const asteroid = objects.asteroid.get(entity);
-          for (const fleet of spawnQueue) {
-            asteroid!.getFleetsContainer().addFleet(fleet);
-          }
-        }
-      });
-      systemsWorld.registerDisposer(unsub);
-    }
 
     for (const fleet of runQuery([HasValue(components.FleetMovement, { destination: entity })])) {
       const fleetObject = renderFleet({ scene, entity: fleet });
-      if (asteroid) {
-        asteroid.getFleetsContainer().addFleet(fleetObject);
-      } else {
-        spawnQueue.add(fleetObject);
-      }
+      asteroid?.getFleetsContainer().addFleet(fleetObject);
     }
 
     //TODO: handle fleet orbit updates
