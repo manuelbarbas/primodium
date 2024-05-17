2f6874f3a5feca26701ec7188f7ec43f0d7a8ccb
polarzero <0xpolarzero@gmail.com>
2024-05-17 16:13:43 +0200

    fix: show coord for entire asteroid screen

--- packages/client/src/components/hud/global/modals/dev/CoordDebugger.tsx
+++ packages/client/src/components/hud/global/modals/dev/CoordDebugger.tsx
@@ -6,6 +6,8 @@ import { components } from "@/network/components";
 import { Button } from "@/components/core/Button";
 import { useGame } from "@/hooks/useGame";
 import { Mode } from "@/util/constants";
+import { Tile } from "@/game/lib/objects/Tile";
+import { DepthLayers } from "@/game/lib/constants/common";
 
 export const CoordDebugger = () => {
   const DEV = import.meta.env.PRI_DEV === "true";
@@ -28,13 +30,44 @@ export const CoordDebugger = () => {
 };
 
 export const CoordDebuggerAsteroid = () => {
-  const hoverTile = components.HoverTile.use();
+  const scene = useGame().ASTEROID;
+  const [tileCoord, setTileCoord] = useState<Coord | undefined>(undefined);
+
+  const originalTile = components.HoverTile.use();
+  let devTile: Tile | undefined;
+
+  useEffect(() => {
+    const pointerMoveSub = scene.input.pointermove$.pipe().subscribe((event) => {
+      const { x, y } = scene.utils.pixelCoordToTileCoord({ x: event.worldX, y: event.worldY });
+      const coord = { x, y: -y };
+      setTileCoord(coord);
+
+      // if the original tile is present destroy the dev one
+      if (originalTile && devTile) {
+        devTile.destroy();
+        devTile = undefined;
+      }
+
+      if (originalTile) return;
+      // create the tile or update its position
+      if (devTile) {
+        devTile.setCoordPosition(coord);
+      } else {
+        devTile = new Tile(scene, coord, 0xffffff, 0.05).setDepth(DepthLayers.Tile).spawn();
+      }
+    });
+
+    return () => {
+      pointerMoveSub.unsubscribe();
+      devTile?.destroy();
+    };
+  }, []);
 
   return (
     <div className="grid grid-cols-[12px_40px_48px] items-center gap-1 bg-black bg-opacity-50 p-2 rounded-sm">
       <FaSquareXmark />
       <CoordCaption caption="tile" />
-      <CoordDisplay coord={hoverTile} />
+      <CoordDisplay coord={tileCoord} />
     </div>
   );
 };
@@ -48,10 +81,9 @@ export const CoordDebuggerStarmap = () => {
 
   useEffect(() => {
     const pointerMoveSub = scene.input.pointermove$.pipe().subscribe((event) => {
-      const pixelCoord = { x: event.worldX, y: event.worldY };
-      console.log("pixelCoord", pixelCoord);
+      const pixelCoord = scene.utils.pixelCoordToTileCoord({ x: event.worldX, y: event.worldY });
 
-      setPixelCoord(Object.fromEntries(Object.entries(pixelCoord).map(([k, v]) => [k, Math.round(v)])) as Coord);
+      setPixelCoord({ x: Math.round(pixelCoord.x), y: Math.round(pixelCoord.y) });
       setTileCoord(scene.utils.pixelCoordToTileCoord({ x: pixelCoord.x, y: -pixelCoord.y }));
       setChunkCoord(scene.utils.pixelCoordToChunkCoord({ x: pixelCoord.x, y: -pixelCoord.y }));
     });
