f8d94f8764acfe37e1b50a1369abfec7c112621d
Nabs <43505437+NAB5@users.noreply.github.com>
2024-05-20 12:31:45 -0400

    fix: z-index on inventory

--- packages/client/src/components/core/Button.tsx
+++ packages/client/src/components/core/Button.tsx
@@ -129,9 +129,9 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       <Tooltip tooltipContent={tooltip} direction={tooltipDirection}>
         <button
           className={cn(
-            "cursor-pointer active:cursor-pointerDown disabled:opacity-50",
+            "relative cursor-pointer active:cursor-pointerDown disabled:opacity-50",
             buttonVariants({ variant, size, motion, modifier, shape, className }),
-            selected && "border-1 border-accent"
+            selected && "border-1 border-accent z-10"
           )}
           ref={ref}
           tabIndex={-1}
