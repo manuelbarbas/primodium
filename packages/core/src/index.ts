5434ba40d7da9e219a8f1e573d9baecd3c860409
0xhank <zeroxhank@gmail.com>
2024-06-06 10:55:33 -0400

    feat: packagize

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,9 @@ export * from "@/lib/constants";
 export * from "@/lib/mappings";
 export * from "@/lib/types";
 export { WorldAbi } from "@/lib/WorldAbi";
+
+export * from "@/sync";
+export * from "@/systems";
+export * from "@/hooks";
+export * from "@/account";
+export * from "@/network/config/chainConfigs";
---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+  },
+  outDir: "dist",
+  format: ["esm"],
+  // dts: true,
+  clean: true,
+  minify: true,
+  tsconfig: "./tsconfig.json",
+});
