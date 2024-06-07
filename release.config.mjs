c0704be45f566b7be4c61ff3f49e18df6ab2b6dd
Nabs <43505437+NAB5@users.noreply.github.com>
2024-06-07 12:57:13 -0400

    build: add build pipeline

---
+++ release.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["main", {
+    name: "release/*",
+    channel: "alpha",
+    prerelease: "alpha",
+  }],
+  extends: "semantic-release-monorepo"
+};
