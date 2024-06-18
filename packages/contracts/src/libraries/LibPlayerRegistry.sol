a842ae4a24d1ae93f34c47bbdb40cb9143a06f56
kethcode <kethic@kethic.com>
2024-06-18 14:14:30 -0700

    feat: PlayerRegistry

---
+++ packages/contracts/src/libraries/LibPlayerRegistry.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { Keys_PlayerRegistry } from "codegen/index.sol";
+
+library LibPlayerRegistry {
+  /**
+   * @dev Checks if a player entity exists in the player registry.
+   * @param entity The player's entity identifier.
+   * @return index -1 if does not exist, otherwise the index of the player entity.
+   */
+  function indexOf(bytes32 entity) internal view returns (int256 index) {
+    bytes32[] memory playerEntities = Keys_PlayerRegistry.get();
+    uint256 listSize = playerEntities.length;
+
+    index = -1;
+    for (uint256 i = 0; i < listSize; i++) {
+      if (playerEntities[i] == entity) {
+        index = int256(i);
+        break;
+      }
+    }
+  }
+
+  /**
+   * @dev Sets an asteroid for a specific entity.
+   * If the player already exists, it updates the existing one.
+   * @param entity The player's entity identifier.
+   */
+  function add(bytes32 entity) internal {
+    // if (LibPlayerRegistry.indexOf(entity) == -1) return;
+    Keys_PlayerRegistry.push(entity);
+    // bytes32[] memory playerEntities = Keys_PlayerRegistry.get();
+    // playerEntities.push(entity);
+    // Keys_PlayerRegistry.set(playerEntities);
+  }
+
+  /**
+   * @dev Retrieves all players registered in the registry.
+   * @return playerEntities array of player entity identifiers for the asteroids.
+   */
+  function getPlayerEntities() internal view returns (bytes32[] memory playerEntities) {
+    return Keys_PlayerRegistry.get();
+  }
+
+  /**
+   * @dev Removes a registered player from the registry.
+   * @param entity The player's entity identifier.
+   */
+  function remove(bytes32 entity) internal {
+    int256 index = LibPlayerRegistry.indexOf(entity);
+    if (index == -1) return;
+
+    if (LibPlayerRegistry.size() == 1) {
+      Keys_PlayerRegistry.pop();
+      return;
+    }
+
+    bytes32 replacement = Keys_PlayerRegistry.getItem(LibPlayerRegistry.size() - 1);
+
+    // copy last player to the index being removed, overwriting the player being removed
+    Keys_PlayerRegistry.update(uint256(index), replacement);
+
+    // remove the last registered player (now a duplicate) from the list
+    Keys_PlayerRegistry.pop();
+  }
+
+  /**
+   * @dev Size of player entity array
+   * @return length Number of registerted player entities.
+   */
+  function size() internal view returns (uint256 length) {
+    bytes32[] memory playerEntities = Keys_PlayerRegistry.get();
+    length = playerEntities.length;
+  }
+
+  /**
+   * @dev Clears all registered player from the registry
+   */
+  function clear() internal {
+    uint256 listSize = LibPlayerRegistry.size();
+    if (listSize == 0) return;
+
+    for (uint256 i = 0; i < listSize; i++) {
+      Keys_PlayerRegistry.pop();
+    }
+  }
+}
---
+++ packages/contracts/test/libraries/LibPlayerRegistry.t.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { console, PrimodiumTest } from "test/PrimodiumTest.t.sol";
+import { addressToEntity } from "src/utils.sol";
+
+import { Keys_PlayerRegistry } from "codegen/index.sol";
+import { LibPlayerRegistry } from "libraries/LibPlayerRegistry.sol";
+
+import { ISpawnSystem } from "codegen/world/ISpawnSystem.sol";
+
+contract LibPlayerRegistryTest is PrimodiumTest {
+  bytes32 aliceEntity;
+  bytes32 bobEntity;
+  bytes32 eveEntity;
+
+  function setUp() public override {
+    super.setUp();
+    aliceEntity = addressToEntity(alice);
+    bobEntity = addressToEntity(bob);
+    eveEntity = addressToEntity(eve);
+    vm.startPrank(creator);
+  }
+
+  function testGetEmpty() public {
+    bytes32[] memory playerEntities = LibPlayerRegistry.getPlayerEntities();
+    assertEq(playerEntities.length, 0);
+  }
+
+  function testGetLengthSingle() public {
+    LibPlayerRegistry.add(aliceEntity);
+    assertEq(LibPlayerRegistry.size(), 1);
+  }
+
+  function testGetLengthMultple() public {
+    LibPlayerRegistry.add(aliceEntity);
+    LibPlayerRegistry.add(eveEntity);
+    LibPlayerRegistry.add(bobEntity);
+    assertEq(LibPlayerRegistry.size(), 3);
+  }
+
+  function testAddAndGetSingle() public {
+    LibPlayerRegistry.add(aliceEntity);
+    bytes32[] memory playerEntities = LibPlayerRegistry.getPlayerEntities();
+    assertEq(playerEntities.length, 1);
+    assertEq(playerEntities[0], aliceEntity);
+  }
+
+  function testAddAndGetMultiple() public {
+    LibPlayerRegistry.add(eveEntity);
+    LibPlayerRegistry.add(aliceEntity);
+    LibPlayerRegistry.add(bobEntity);
+    bytes32[] memory playerEntities = LibPlayerRegistry.getPlayerEntities();
+    assertEq(playerEntities.length, 3);
+    assertEq(playerEntities[0], eveEntity);
+    assertEq(playerEntities[1], aliceEntity);
+    assertEq(playerEntities[2], bobEntity);
+  }
+
+  function testIndexOfEmpty() public {
+    int256 index = LibPlayerRegistry.indexOf(aliceEntity);
+    assertEq(index, -1);
+  }
+
+  function testIndexOfSingle() public {
+    LibPlayerRegistry.add(aliceEntity);
+    int256 index = LibPlayerRegistry.indexOf(aliceEntity);
+    assertEq(index, 0);
+  }
+
+  function testIndexOfOneOfMany() public {
+    LibPlayerRegistry.add(aliceEntity);
+    LibPlayerRegistry.add(bobEntity);
+    LibPlayerRegistry.add(eveEntity);
+    int256 index = LibPlayerRegistry.indexOf(bobEntity);
+    assertEq(index, 1);
+  }
+
+  function testIndexOfLastOfMany() public {
+    LibPlayerRegistry.add(aliceEntity);
+    LibPlayerRegistry.add(bobEntity);
+    LibPlayerRegistry.add(eveEntity);
+    int256 index = LibPlayerRegistry.indexOf(eveEntity);
+    assertEq(index, 2);
+  }
+
+  function testRemoveEmpty() public {
+    LibPlayerRegistry.remove(aliceEntity);
+    assertEq(LibPlayerRegistry.size(), 0);
+  }
+
+  function testRemoveFromSingle() public {
+    LibPlayerRegistry.add(aliceEntity);
+    assertEq(LibPlayerRegistry.size(), 1);
+    LibPlayerRegistry.remove(aliceEntity);
+    assertEq(LibPlayerRegistry.size(), 0);
+  }
+
+  function testRemoveFromMany() public {
+    LibPlayerRegistry.add(aliceEntity);
+    LibPlayerRegistry.add(bobEntity);
+    LibPlayerRegistry.add(eveEntity);
+    assertEq(LibPlayerRegistry.size(), 3);
+    LibPlayerRegistry.remove(bobEntity);
+    assertEq(LibPlayerRegistry.size(), 2);
+    bytes32[] memory playerEntities = LibPlayerRegistry.getPlayerEntities();
+    assertEq(playerEntities[0], aliceEntity);
+    assertEq(playerEntities[1], eveEntity);
+  }
+
+  function testClearEmpty() public {
+    assertEq(LibPlayerRegistry.size(), 0);
+    LibPlayerRegistry.add(aliceEntity);
+    LibPlayerRegistry.add(bobEntity);
+    LibPlayerRegistry.add(eveEntity);
+    LibPlayerRegistry.clear();
+    assertEq(LibPlayerRegistry.size(), 0);
+  }
+
+  function testAddOnSpawn() public {
+    vm.stopPrank();
+    vm.startPrank(alice);
+    world.Pri_11__spawn();
+
+    bytes32[] memory playerEntities = LibPlayerRegistry.getPlayerEntities();
+    assertEq(playerEntities.length, 1);
+    assertEq(playerEntities[0], aliceEntity);
+  }
+
+  function testFindAfterMultipleSpawn() public {
+    vm.stopPrank();
+    vm.startPrank(alice);
+    world.Pri_11__spawn();
+    vm.stopPrank();
+    vm.startPrank(bob);
+    world.Pri_11__spawn();
+    vm.stopPrank();
+    vm.startPrank(eve);
+    world.Pri_11__spawn();
+
+    bytes32[] memory playerEntities = LibPlayerRegistry.getPlayerEntities();
+    assertEq(playerEntities.length, 3);
+    assertEq(playerEntities[0], aliceEntity);
+    assertEq(playerEntities[1], bobEntity);
+    assertEq(playerEntities[2], eveEntity);
+  }
+}
