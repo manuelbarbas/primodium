b9540af00f81f79d11ab3f3a446ab5e429e48616
Nabs <43505437+NAB5@users.noreply.github.com>
2024-06-20 15:19:01 +0000

    fix:include iworld dts

--- packages/client/src/components/hud/starbelt/index.tsx
+++ packages/client/src/components/hud/starbelt/index.tsx
@@ -1,6 +1,6 @@
 import { HUD } from "@/components/core/HUD";
 import { StarmapNavigator } from "@/components/hud/starbelt/starmap-navigator/StarmapNavigator";
-import { usePersistentStore } from "@primodiumxyz/game/src/stores/persistentStore";
+import { usePersistentStore } from "@primodiumxyz/game/src/stores/PersistentStore";
 import { memo } from "react";
 import { useShallow } from "zustand/react/shallow";
 import { AsteroidMenuPopup } from "@/components/hud/starbelt/markers/AsteroidMenuPopup";
