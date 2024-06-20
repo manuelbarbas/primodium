d116369565fee0a6bb6343af53e2ace3ea50be27
0xhank <zeroxhank@gmail.com>
2024-06-20 12:42:28 -0400

    fix: make setTableValue work with keyless tables

--- packages/client/src/contractCalls/contractCalls/dev.ts
+++ packages/client/src/contractCalls/contractCalls/dev.ts
@@ -1,15 +1,8 @@
 import { ExecuteFunctions } from "@primodiumxyz/core";
-import {
-  AbiToSchema,
-  Entity,
-  ContractTable,
-  ContractTableDef,
-  Properties,
-  defaultEntity,
-} from "@primodiumxyz/reactive-tables";
+import { AbiToSchema, Entity, ContractTable, ContractTableDef, Properties } from "@primodiumxyz/reactive-tables";
 import { encodeField, entityToHexKeyTuple, StaticAbiType, uuid } from "@primodiumxyz/reactive-tables/utils";
 import { encodeKey } from "@latticexyz/protocol-parser/internal";
-import { Hex, padHex } from "viem";
+import { Hex } from "viem";
 
 export function createDevCalls({ execute }: ExecuteFunctions) {
   async function removeTable<tableDef extends ContractTableDef = ContractTableDef>(
@@ -34,11 +27,12 @@ export function createDevCalls({ execute }: ExecuteFunctions) {
     keys: Properties<AbiToSchema<ContractTable<tableDef>["metadata"]["abiKeySchema"]>>,
     newValues: Partial<Properties<ContractTable<tableDef>["propertiesSchema"]>>
   ) {
-    if (Object.entries(keys).length === 0) keys = { entity: padHex(defaultEntity, { size: 32 }) };
     const tableId = table.id as Hex;
 
     const schema = Object.keys(table.metadata.abiPropertiesSchema);
-    const key = encodeKey(table.metadata.abiKeySchema, keys);
+
+    // todo: remove this hack once reactive-tables/25 is merged
+    const key = encodeKey(Object.entries(keys).length === 0 ? {} : table.metadata.abiKeySchema, keys);
     return Object.entries(newValues).forEach(async ([name, value]) => {
       const type = table.metadata.abiPropertiesSchema[name] as StaticAbiType;
       const data = encodeField(type, value);
