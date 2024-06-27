e2fbba9a274a4a198fe4e5bce9637ecb5e8ed4e5
0xhank <zeroxhank@gmail.com>
2024-06-27 12:17:03 -0400

    fix: make delegation stay on refresh

--- packages/core/src/sync/queries/initialQueries.ts
+++ packages/core/src/sync/queries/initialQueries.ts
@@ -25,6 +25,7 @@ export const getInitialQuery = ({
       { tableId: tables.GracePeriod.tableId },
       { tableId: tables.Reserves.tableId },
       { tableId: tables.VictoryStatus.tableId },
+      { tableId: tables.UserDelegationControl.tableId },
       // main base starting coord
       { tableId: tables.Position.tableId, where: { column: "entity", operation: "eq", value: EntityType.MainBase } },
     ],
